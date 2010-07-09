0e4871d5b650830ba638842e0c9232ef9b4b534a
jbarrez <jbarrez@51b7ce9a-3489-0410-86de-dc9a27cf12c7>
2010-07-09 11:46:38 +0000

    Testcase for a persistent parallel gateway with no wait states in between

---
+++ modules/activiti-engine-test-api/src/test/java/org/activiti/test/bpmn/gateway/ParallelGatewayTest.java
@@ -0,0 +1,48 @@
+/* Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.activiti.test.bpmn.gateway;
+
+import org.activiti.ProcessInstance;
+import org.activiti.test.LogInitializer;
+import org.activiti.test.ProcessDeclared;
+import org.activiti.test.ProcessDeployer;
+import org.junit.Rule;
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+
+/**
+ * @author Joram Barrez
+ */
+public class ParallelGatewayTest {
+
+  @Rule
+  public LogInitializer logSetup = new LogInitializer();
+  @Rule
+  public ProcessDeployer deployer = new ProcessDeployer();
+
+  /**
+   * Case where there is a parallel gateway that splits into 3 paths of
+   * execution, that are immediately joined, without any wait states in between.
+   * In the end, no executions should be in the database.
+   */
+  @Test
+  @ProcessDeclared
+  public void testSplitMergeNoWaitstates() {
+    ProcessInstance processInstance = 
+      deployer.getProcessService().startProcessInstanceByKey("splitMergeNoWaitstates");
+    assertTrue(processInstance.isEnded());
+  }
+
+}
