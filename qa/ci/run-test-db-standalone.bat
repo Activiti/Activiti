7b030803eb6f8a13c32d700bad7ab0ecf924a3e0
cmuelder <cmuelder@51b7ce9a-3489-0410-86de-dc9a27cf12c7>
2010-07-23 13:48:17 +0000

    added support for ci, running from different location!

--- qa/ci/run-test-db-standalone.bat
+++ qa/ci/run-test-db-standalone.bat
@@ -1,4 +1,18 @@
-cd ../../
-mvn -Ddatabase=%1 -Pcheck clean install
+@echo off
+if ci == %2 goto execute
 
+:folder_up
+cd ../../
+
+ 
+:execute
+mvn -Ddatabase=%1 clean install
+
+if ci == %2 goto end
+
+
+:finish
 cd qa/ci
+
+:end
+
