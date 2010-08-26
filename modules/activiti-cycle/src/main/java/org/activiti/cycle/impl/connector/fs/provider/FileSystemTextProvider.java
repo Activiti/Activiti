c6cf5f6ec232502ccad334dae58d752d23df953f
nilspreusker <nilspreusker@51b7ce9a-3489-0410-86de-dc9a27cf12c7>
2010-08-26 16:22:15 +0000

    fixed line breaks for text files in file system connector

--- modules/activiti-cycle/src/main/java/org/activiti/cycle/impl/connector/fs/provider/FileSystemTextProvider.java
+++ modules/activiti-cycle/src/main/java/org/activiti/cycle/impl/connector/fs/provider/FileSystemTextProvider.java
@@ -1,10 +1,8 @@
 package org.activiti.cycle.impl.connector.fs.provider;
 
-import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.IOException;
 
 import org.activiti.cycle.Content;
 import org.activiti.cycle.ContentType;
@@ -22,32 +20,11 @@ public class FileSystemTextProvider extends FileSystemContentRepresentationProvi
   @Override
   public void addValueToContent(Content content, RepositoryArtifact artifact) {
     File file = new File(getConnector(artifact).getConfiguration().getBasePath() + artifact.getId());
-    BufferedReader br = null;
-    StringBuilder sb = null;
-
-    try {
-      br = new BufferedReader(new FileReader(file));
-      sb = new StringBuilder();
-
-      String s;
-      while ((s = br.readLine()) != null) {
-        sb.append(s);
-      }
-    } catch (FileNotFoundException fnfe) {
+    try{
+    	content.setValue(new FileInputStream(file));
+  	} catch (FileNotFoundException fnfe) {
       throw new RepositoryException("Unable to find artifact " + artifact, fnfe);
-    } catch (IOException ioe) {
-      throw new RepositoryException("Error while accessing artifact " + artifact, ioe);
-    } finally {
-      if (br != null) {
-        try {
-          br.close();
-        } catch (IOException e) {
-          // ignore
-        }
-      }
     }
-
-    content.setValue(sb.toString());
   }
 
 }
