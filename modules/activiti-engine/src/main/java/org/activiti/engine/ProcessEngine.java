9c2afe8dc6722137f3ff9e2979831a41db616727
tombaeyens <tombaeyens@51b7ce9a-3489-0410-86de-dc9a27cf12c7>
2010-08-31 13:13:45 +0000

    updating version from 5.0.beta1-SNAPSHOT to 5.0.beta2-SNAPSHOT

--- modules/activiti-engine/src/main/java/org/activiti/engine/ProcessEngine.java
+++ modules/activiti-engine/src/main/java/org/activiti/engine/ProcessEngine.java
@@ -46,7 +46,7 @@ package org.activiti.engine;
 public interface ProcessEngine {
 
   /** the version of the activiti library */
-  public static String VERSION = "5.0.beta1-SNAPSHOT";
+  public static String VERSION = "5.0.beta2-SNAPSHOT";
 
   /** the name as specified in the 'process.engine.name' property in 
    * the activiti.properties configuration file or in the 
