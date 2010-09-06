6f1ea38f4bd0d13d9bc1b5a40a26aeb520d34ab3
tombaeyens <tombaeyens@51b7ce9a-3489-0410-86de-dc9a27cf12c7>
2010-09-06 15:18:17 +0000

    ACT-127 start restructuring the modules

---
+++ modules/activiti-engine/src/test/java/org/activiti/examples/identity/IdentityTest.java
@@ -0,0 +1,192 @@
+/* Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.activiti.examples.identity;
+
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+import org.activiti.engine.identity.Group;
+import org.activiti.engine.identity.User;
+import org.activiti.engine.test.ProcessEngineTestCase;
+
+/**
+ * @author Tom Baeyens
+ */
+public class IdentityTest extends ProcessEngineTestCase {
+
+  public void testAuthentication() {
+    User user = identityService.newUser("johndoe");
+    user.setPassword("xxx");
+    identityService.saveUser(user);
+
+    assertTrue(identityService.checkPassword("johndoe", "xxx"));
+    assertFalse(identityService.checkPassword("johndoe", "invalid pwd"));
+
+    identityService.deleteUser("johndoe");
+  }
+
+  public void testFindGroupsByUserAndType() {
+    Group sales = identityService.newGroup("sales");
+    sales.setType("hierarchy");
+    identityService.saveGroup(sales);
+
+    Group development = identityService.newGroup("development");
+    development.setType("hierarchy");
+    identityService.saveGroup(development);
+
+    Group admin = identityService.newGroup("admin");
+    admin.setType("security-role");
+    identityService.saveGroup(admin);
+
+    Group user = identityService.newGroup("user");
+    user.setType("security-role");
+    identityService.saveGroup(user);
+
+    User johndoe = identityService.newUser("johndoe");
+    identityService.saveUser(johndoe);
+
+    User joesmoe = identityService.newUser("joesmoe");
+    identityService.saveUser(joesmoe);
+
+    User jackblack = identityService.newUser("jackblack");
+    identityService.saveUser(jackblack);
+
+    identityService.createMembership("johndoe", "sales");
+    identityService.createMembership("johndoe", "user");
+    identityService.createMembership("johndoe", "admin");
+
+    identityService.createMembership("joesmoe", "user");
+
+    List<Group> groups = identityService.findGroupsByUserIdAndGroupType("johndoe", "security-role");
+    Set<String> groupIds = getGroupIds(groups);
+    Set<String> expectedGroupIds = new HashSet<String>();
+    expectedGroupIds.add("sales");
+    expectedGroupIds.add("user");
+    expectedGroupIds.add("admin");
+    assertEquals(expectedGroupIds, groupIds);
+
+    groups = identityService.findGroupsByUserIdAndGroupType("joesmoe", "security-role");
+    groupIds = getGroupIds(groups);
+    expectedGroupIds = new HashSet<String>();
+    expectedGroupIds.add("user");
+    assertEquals(expectedGroupIds, groupIds);
+
+    groups = identityService.findGroupsByUserIdAndGroupType("jackblack", "security-role");
+    assertTrue(groups.isEmpty());
+
+    identityService.deleteGroup("sales");
+    identityService.deleteGroup("development");
+    identityService.deleteGroup("admin");
+    identityService.deleteGroup("user");
+    identityService.deleteUser("johndoe");
+    identityService.deleteUser("joesmoe");
+    identityService.deleteUser("jackblack");
+  }
+
+  public void testUser() {
+    User user = identityService.newUser("johndoe");
+    user.setFirstName("John");
+    user.setLastName("Doe");
+    user.setEmail("johndoe@alfresco.com");
+    identityService.saveUser(user);
+
+    user = identityService.findUser("johndoe");
+    assertEquals("johndoe", user.getId());
+    assertEquals("John", user.getFirstName());
+    assertEquals("Doe", user.getLastName());
+    assertEquals("johndoe@alfresco.com", user.getEmail());
+
+    identityService.deleteUser("johndoe");
+  }
+
+  public void testGroup() {
+    Group group = identityService.newGroup("sales");
+    group.setName("Sales division");
+    identityService.saveGroup(group);
+
+    group = identityService.findGroupById("sales");
+    assertEquals("sales", group.getId());
+    assertEquals("Sales division", group.getName());
+
+    identityService.deleteGroup("sales");
+  }
+
+  public void testMembership() {
+    Group sales = identityService.newGroup("sales");
+    identityService.saveGroup(sales);
+
+    Group development = identityService.newGroup("development");
+    identityService.saveGroup(development);
+
+    User johndoe = identityService.newUser("johndoe");
+    identityService.saveUser(johndoe);
+
+    User joesmoe = identityService.newUser("joesmoe");
+    identityService.saveUser(joesmoe);
+
+    User jackblack = identityService.newUser("jackblack");
+    identityService.saveUser(jackblack);
+
+    identityService.createMembership("johndoe", "sales");
+    identityService.createMembership("joesmoe", "sales");
+
+    identityService.createMembership("joesmoe", "development");
+    identityService.createMembership("jackblack", "development");
+
+    List<Group> groups = identityService.findGroupsByUserId("johndoe");
+    assertEquals(createStringSet("sales"), getGroupIds(groups));
+
+    groups = identityService.findGroupsByUserId("joesmoe");
+    assertEquals(createStringSet("sales", "development"), getGroupIds(groups));
+
+    groups = identityService.findGroupsByUserId("jackblack");
+    assertEquals(createStringSet("development"), getGroupIds(groups));
+
+    List<User> users = identityService.findUsersByGroupId("sales");
+    assertEquals(createStringSet("johndoe", "joesmoe"), getUserIds(users));
+
+    users = identityService.findUsersByGroupId("development");
+    assertEquals(createStringSet("joesmoe", "jackblack"), getUserIds(users));
+
+    identityService.deleteGroup("sales");
+    identityService.deleteGroup("development");
+
+    identityService.deleteUser("jackblack");
+    identityService.deleteUser("joesmoe");
+    identityService.deleteUser("johndoe");
+  }
+
+  private Object createStringSet(String... strings) {
+    Set<String> stringSet = new HashSet<String>();
+    for (String string : strings) {
+      stringSet.add(string);
+    }
+    return stringSet;
+  }
+
+  public Set<String> getGroupIds(List<Group> groups) {
+    Set<String> groupIds = new HashSet<String>();
+    for (Group group : groups) {
+      groupIds.add(group.getId());
+    }
+    return groupIds;
+  }
+  public Set<String> getUserIds(List<User> users) {
+    Set<String> userIds = new HashSet<String>();
+    for (User user : users) {
+      userIds.add(user.getId());
+    }
+    return userIds;
+  }
+}
---
+++ modules/activiti-engine/src/test/java/org/activiti/examples/task/StandaloneTaskTest.java
@@ -0,0 +1,74 @@
+/* Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.activiti.examples.task;
+
+import java.util.List;
+
+import org.activiti.engine.task.Task;
+import org.activiti.engine.test.ProcessEngineTestCase;
+
+/**
+ * @author Joram Barrez
+ */
+public class StandaloneTaskTest extends ProcessEngineTestCase {
+
+  public void setUp() throws Exception {
+    super.setUp();
+    identityService.saveUser(identityService.newUser("kermit"));
+    identityService.saveUser(identityService.newUser("gonzo"));
+  }
+
+  public void tearDown() throws Exception {
+    identityService.deleteUser("kermit");
+    identityService.deleteUser("gonzo");
+    super.tearDown();
+  }
+
+  public void testCreateToComplete() {
+
+    // Create and save task
+    Task task = taskService.newTask();
+    task.setName("testTask");
+    taskService.saveTask(task);
+    String taskId = task.getId();
+
+    // Add user as candidate user
+    taskService.addCandidateUser(taskId, "kermit");
+    taskService.addCandidateUser(taskId, "gonzo");
+
+    // Retrieve task list for jbarrez
+    List<Task> tasks = taskService.findUnassignedTasks("kermit");
+    assertEquals(1, tasks.size());
+    assertEquals("testTask", tasks.get(0).getName());
+
+    // Retrieve task list for tbaeyens
+    tasks = taskService.findUnassignedTasks("gonzo");
+    assertEquals(1, tasks.size());
+    assertEquals("testTask", tasks.get(0).getName());
+
+    // Claim task
+    taskService.claim(taskId, "kermit");
+
+    // Tasks shouldn't appear in the candidate tasklists anymore
+    assertTrue(taskService.findUnassignedTasks("kermit").isEmpty());
+    assertTrue(taskService.findUnassignedTasks("gonzo").isEmpty());
+
+    // Complete task
+    taskService.complete(taskId);
+
+    // Task should be removed from runtime data
+    // TODO: check for historic data when implemented!
+    assertNull(taskService.findTask(taskId));
+  }
+
+}
