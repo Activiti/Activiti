3666f36e36ff3b79e0c59d94ac82942e27406c47
kristin.polenz <kristin.polenz@51b7ce9a-3489-0410-86de-dc9a27cf12c7>
2010-09-21 15:38:48 +0000

    Uncommented test methods.

--- modules/activiti-cycle/src/test/java/org/activiti/cycle/impl/conf/RepositoryConnectorConfigurationManagerImplTest.java
+++ modules/activiti-cycle/src/test/java/org/activiti/cycle/impl/conf/RepositoryConnectorConfigurationManagerImplTest.java
@@ -79,75 +79,75 @@ public class RepositoryConnectorConfigurationManagerImplTest {
     }
   }
   
-//  @Test
-//  public void testInsertCycleConfiguration() {
-//    String id = "kristin";
-//    String configXML = "<org.activiti.cycle.impl.conf.ConfigurationContainer>"
-//                       + " <name>kristin</name>"
-//                       + " <linkedConnectors>"
-//                       + "<org.activiti.cycle.impl.connector.signavio.SignavioConnectorConfiguration>"
-//                       + "<name>Activiti Modeler</name>"
-//                       + "<credentialsSaved>false</credentialsSaved>"
-//                       + "<signavioBaseUrl>http://localhost:8080/activiti-modeler/</signavioBaseUrl>"
-//                       + "<loginRequired>false</loginRequired>"
-//                       + "</org.activiti.cycle.impl.connector.signavio.SignavioConnectorConfiguration>"
-//                       + "<org.activiti.cycle.impl.connector.fs.FileSystemConnectorConfiguration>"
-//                       + "<name>Eclipse Workspace (File System)</name>"
-//                       + "<baseFilePath>C:/Dokumente+und+Einstellungen/polenz/workspace/activiti/distro/target/activiti-5.0.beta2-SNAPSHOT/apps/eclipse-workspace</baseFilePath>"
-//                       + "</org.activiti.cycle.impl.connector.fs.FileSystemConnectorConfiguration>"
-//                       + "<org.activiti.cycle.impl.connector.signavio.OryxConnectorConfiguration>"
-//                       + "<name>oryx-project.org</name>"
-//                       + "<credentialsSaved>false</credentialsSaved>"
-//                       + "<signavioBaseUrl>http://oryx-project.org/</signavioBaseUrl>"
-//                       + "<loginRequired>false</loginRequired>"
-//                       + "</org.activiti.cycle.impl.connector.signavio.OryxConnectorConfiguration>"
-//                       + "</linkedConnectors>"
-//                       + "<parentContainers/>"
-//                       + "</org.activiti.cycle.impl.conf.ConfigurationContainer>";
-//    configurationService.createAndInsert(configXML, id);
-//    
-//  }
-//  
-//  @Test
-//  public void testSelectById() {
-//    CycleConfigEntity cycleConfig = configurationService.selectById("kristin");
-//    if (cycleConfig != null)
-//        System.out.println("Cycle Config XML: " + cycleConfig.getConfigXML());
-//    
-//    String configXML = "<org.activiti.cycle.impl.conf.ConfigurationContainer>"
-//      + " <name>kristinPolenz</name>"
-//      + " <linkedConnectors>"
-//      + "<org.activiti.cycle.impl.connector.signavio.SignavioConnectorConfiguration>"
-//      + "<name>Activiti Modeler</name>"
-//      + "<credentialsSaved>false</credentialsSaved>"
-//      + "<signavioBaseUrl>http://localhost:8080/activiti-modeler/</signavioBaseUrl>"
-//      + "<loginRequired>false</loginRequired>"
-//      + "</org.activiti.cycle.impl.connector.signavio.SignavioConnectorConfiguration>"
-//      + "<org.activiti.cycle.impl.connector.fs.FileSystemConnectorConfiguration>"
-//      + "<name>Eclipse Workspace (File System)</name>"
-//      + "<baseFilePath>C:/Dokumente+und+Einstellungen/polenz/workspace/activiti/distro/target/activiti-5.0.beta2-SNAPSHOT/apps/eclipse-workspace</baseFilePath>"
-//      + "</org.activiti.cycle.impl.connector.fs.FileSystemConnectorConfiguration>"
-//      + "<org.activiti.cycle.impl.connector.signavio.OryxConnectorConfiguration>"
-//      + "<name>oryx-project.org</name>"
-//      + "<credentialsSaved>false</credentialsSaved>"
-//      + "<signavioBaseUrl>http://oryx-project.org/</signavioBaseUrl>"
-//      + "<loginRequired>false</loginRequired>"
-//      + "</org.activiti.cycle.impl.connector.signavio.OryxConnectorConfiguration>"
-//      + "</linkedConnectors>"
-//      + "<parentContainers/>"
-//      + "</org.activiti.cycle.impl.conf.ConfigurationContainer>";
-//    
-//    //update by id
-//    cycleConfig.setConfigXML(configXML);
-//    cycleConfig.setRevision(cycleConfig.getRevision()+1);
-//    
-//    configurationService.updateById(cycleConfig);
-//  }
-//  
-//  @Test
-//  public void testDeleteById() {
-//    configurationService.deleteById("kristin");
-//  }
+  @Test
+  public void testInsertCycleConfiguration() {
+    String id = "kristin";
+    String configXML = "<org.activiti.cycle.impl.conf.ConfigurationContainer>"
+                       + " <name>kristin</name>"
+                       + " <linkedConnectors>"
+                       + "<org.activiti.cycle.impl.connector.signavio.SignavioConnectorConfiguration>"
+                       + "<name>Activiti Modeler</name>"
+                       + "<credentialsSaved>false</credentialsSaved>"
+                       + "<signavioBaseUrl>http://localhost:8080/activiti-modeler/</signavioBaseUrl>"
+                       + "<loginRequired>false</loginRequired>"
+                       + "</org.activiti.cycle.impl.connector.signavio.SignavioConnectorConfiguration>"
+                       + "<org.activiti.cycle.impl.connector.fs.FileSystemConnectorConfiguration>"
+                       + "<name>Eclipse Workspace (File System)</name>"
+                       + "<baseFilePath>C:/Dokumente+und+Einstellungen/polenz/workspace/activiti/distro/target/activiti-5.0.beta2-SNAPSHOT/apps/eclipse-workspace</baseFilePath>"
+                       + "</org.activiti.cycle.impl.connector.fs.FileSystemConnectorConfiguration>"
+                       + "<org.activiti.cycle.impl.connector.signavio.OryxConnectorConfiguration>"
+                       + "<name>oryx-project.org</name>"
+                       + "<credentialsSaved>false</credentialsSaved>"
+                       + "<signavioBaseUrl>http://oryx-project.org/</signavioBaseUrl>"
+                       + "<loginRequired>false</loginRequired>"
+                       + "</org.activiti.cycle.impl.connector.signavio.OryxConnectorConfiguration>"
+                       + "</linkedConnectors>"
+                       + "<parentContainers/>"
+                       + "</org.activiti.cycle.impl.conf.ConfigurationContainer>";
+    configurationService.createAndInsert(configXML, id);
+    
+  }
+  
+  @Test
+  public void testSelectById() {
+    CycleConfigEntity cycleConfig = configurationService.selectById("kristin");
+    if (cycleConfig != null)
+        System.out.println("Cycle Config XML: " + cycleConfig.getConfigXML());
+    
+    String configXML = "<org.activiti.cycle.impl.conf.ConfigurationContainer>"
+      + " <name>kristinPolenz</name>"
+      + " <linkedConnectors>"
+      + "<org.activiti.cycle.impl.connector.signavio.SignavioConnectorConfiguration>"
+      + "<name>Activiti Modeler</name>"
+      + "<credentialsSaved>false</credentialsSaved>"
+      + "<signavioBaseUrl>http://localhost:8080/activiti-modeler/</signavioBaseUrl>"
+      + "<loginRequired>false</loginRequired>"
+      + "</org.activiti.cycle.impl.connector.signavio.SignavioConnectorConfiguration>"
+      + "<org.activiti.cycle.impl.connector.fs.FileSystemConnectorConfiguration>"
+      + "<name>Eclipse Workspace (File System)</name>"
+      + "<baseFilePath>C:/Dokumente+und+Einstellungen/polenz/workspace/activiti/distro/target/activiti-5.0.beta2-SNAPSHOT/apps/eclipse-workspace</baseFilePath>"
+      + "</org.activiti.cycle.impl.connector.fs.FileSystemConnectorConfiguration>"
+      + "<org.activiti.cycle.impl.connector.signavio.OryxConnectorConfiguration>"
+      + "<name>oryx-project.org</name>"
+      + "<credentialsSaved>false</credentialsSaved>"
+      + "<signavioBaseUrl>http://oryx-project.org/</signavioBaseUrl>"
+      + "<loginRequired>false</loginRequired>"
+      + "</org.activiti.cycle.impl.connector.signavio.OryxConnectorConfiguration>"
+      + "</linkedConnectors>"
+      + "<parentContainers/>"
+      + "</org.activiti.cycle.impl.conf.ConfigurationContainer>";
+    
+    //update by id
+    cycleConfig.setConfigXML(configXML);
+    cycleConfig.setRevision(cycleConfig.getRevision()+1);
+    
+    configurationService.updateById(cycleConfig);
+  }
+  
+  @Test
+  public void testDeleteById() {
+    configurationService.deleteById("kristin");
+  }
   
   //  
   //
