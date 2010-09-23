85de33f4d05593e6d39026a95be7d608eed51f91
kristin.polenz <kristin.polenz@51b7ce9a-3489-0410-86de-dc9a27cf12c7>
2010-09-23 14:04:25 +0000

    Added create and drop sql file for mysql database.

---
+++ modules/activiti-cycle/src/main/resources/org/activiti/db/cycle/create/activiti.mysql.create.sql
@@ -0,0 +1,6 @@
+create table ACT_CY_CONFIG (
+	ID_ varchar(64),
+    VALUE_ varchar(300),
+    REV_ integer,
+    primary key (ID_)
+) TYPE=InnoDB;
---
+++ modules/activiti-cycle/src/main/resources/org/activiti/db/cycle/drop/activiti.mysql.drop.sql
@@ -0,0 +1,1 @@
+drop table if exists ACT_CY_CONFIG;
