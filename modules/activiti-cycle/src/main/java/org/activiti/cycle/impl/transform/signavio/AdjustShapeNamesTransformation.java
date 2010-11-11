2561cb1849d500aa063f1e884396146bb2f2c6b9
falko.menge <falko.menge@51b7ce9a-3489-0410-86de-dc9a27cf12c7>
2010-11-11 18:49:09 +0000

    Introduced abstract base class for transformations of shape names, which stores original names in BPMN's documentation element

--- modules/activiti-cycle/src/main/java/org/activiti/cycle/impl/transform/signavio/AdjustShapeNamesTransformation.java
+++ modules/activiti-cycle/src/main/java/org/activiti/cycle/impl/transform/signavio/AdjustShapeNamesTransformation.java
@@ -1,27 +1,29 @@
+/* Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package org.activiti.cycle.impl.transform.signavio;
 
-import org.oryxeditor.server.diagram.Diagram;
-import org.oryxeditor.server.diagram.Shape;
-
 /**
- * adjust name from Signavio (remove new lines, ' and maybe add more in future)
+ * Adjusts names from Signavio (remove new lines, ' and maybe add more in future)
  * See https://app.camunda.com/jira/browse/HEMERA-164.
  * 
  * @author bernd.ruecker@camunda.com
  */
-public class AdjustShapeNamesTransformation extends OryxTransformation {
+public class AdjustShapeNamesTransformation extends OryxShapeNameTransformation {
 
   @Override
-	public Diagram transform(Diagram diagram) {
-    for (Shape shape : diagram.getShapes()) {
-      if (shape.getProperties().containsKey("name")) {
-        String name = shape.getProperty("name");
-        String newName = name.replaceAll("\n", " ").replaceAll("'", "").replaceAll("\"", "");
-        shape.getProperties().put("name", newName);
-			}
-		}
-    
-		return diagram;
+  public String transformName(String name) {
+    return name.replaceAll("\n", " ").replaceAll("'", "").replaceAll("\"", "");
   }
 
 }
