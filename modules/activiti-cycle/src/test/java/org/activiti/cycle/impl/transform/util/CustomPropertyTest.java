cc14820e77f55e2262f5cc859dc6cf6024b46c28
falko.menge <falko.menge@51b7ce9a-3489-0410-86de-dc9a27cf12c7>
2010-11-19 11:02:08 +0000

    Added test case for custom properties

---
+++ modules/activiti-cycle/src/test/java/org/activiti/cycle/impl/transform/util/CustomPropertyTest.java
@@ -0,0 +1,23 @@
+package org.activiti.cycle.impl.transform.util;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+
+public class CustomPropertyTest {
+
+  @Test
+  public void testGetValue() {
+    String expected = "Do\n\"Something\"";
+
+    String propertyContainer = "Original Name: \"Do\\n\"\"Something\"\"\" Original ID: \"sid-910BF305-8A75-4319-A3A9-23C5B2828944\"";
+    String actual = CustomProperty.ORIGINAL_NAME.getValue(propertyContainer);
+    assertEquals(expected, actual);
+
+    propertyContainer = "Original Name: \"Do\\n\"\"Something\"\"\"\nOriginal ID: \"sid-910BF305-8A75-4319-A3A9-23C5B2828944\"";
+    actual = CustomProperty.ORIGINAL_NAME.getValue(propertyContainer);
+    // FIXME assertEquals(expected, actual);
+  }
+
+}
