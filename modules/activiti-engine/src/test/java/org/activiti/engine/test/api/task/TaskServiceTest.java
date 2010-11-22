496c1d893d35b47d9eea52581965c29f33ed83ca
jbarrez <jbarrez@51b7ce9a-3489-0410-86de-dc9a27cf12c7>
2010-11-22 08:51:59 +0000

    Committed failing test to verify Hudson email sending

--- modules/activiti-engine/src/test/java/org/activiti/engine/test/api/task/TaskServiceTest.java
+++ modules/activiti-engine/src/test/java/org/activiti/engine/test/api/task/TaskServiceTest.java
@@ -34,6 +34,10 @@ import org.activiti.engine.test.Deployment;
  * @author Joram Barrez
  */
 public class TaskServiceTest extends ActivitiInternalTestCase {
+  
+  public void testMustFail() {
+    assertTrue(false);
+  }
 
   public void testSaveTaskUpdate() {
     Task task = taskService.newTask();
