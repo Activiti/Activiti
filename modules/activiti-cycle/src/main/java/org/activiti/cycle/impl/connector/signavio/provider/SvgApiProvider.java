0fa101c941f1b1288ff398801fb9cf691950ec72
meyerd <meyerd@51b7ce9a-3489-0410-86de-dc9a27cf12c7>
2010-11-29 14:48:47 +0000

    Activiti Cycle removed unused import (breaks build)

--- modules/activiti-cycle/src/main/java/org/activiti/cycle/impl/connector/signavio/provider/SvgApiProvider.java
+++ modules/activiti-cycle/src/main/java/org/activiti/cycle/impl/connector/signavio/provider/SvgApiProvider.java
@@ -3,7 +3,6 @@ package org.activiti.cycle.impl.connector.signavio.provider;
 import org.activiti.cycle.Content;
 import org.activiti.cycle.RepositoryArtifact;
 import org.activiti.cycle.impl.connector.signavio.SignavioConnector;
-import org.activiti.cycle.impl.connector.signavio.util.NewSignavioSvgApiBuilder;
 import org.activiti.cycle.impl.connector.signavio.util.SignavioSvgApiBuilder;
 
 public class SvgApiProvider extends SignavioContentRepresentationProvider {
