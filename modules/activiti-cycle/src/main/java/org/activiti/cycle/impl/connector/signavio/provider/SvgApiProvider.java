babfec219f6f6ab48076cd882a2b1b615c0208d0
meyerd <meyerd@51b7ce9a-3489-0410-86de-dc9a27cf12c7>
2010-11-30 08:58:23 +0000

    Activiti Cycle removed unused import (breaks build, again...)

--- modules/activiti-cycle/src/main/java/org/activiti/cycle/impl/connector/signavio/provider/SvgApiProvider.java
+++ modules/activiti-cycle/src/main/java/org/activiti/cycle/impl/connector/signavio/provider/SvgApiProvider.java
@@ -3,14 +3,13 @@ package org.activiti.cycle.impl.connector.signavio.provider;
 import org.activiti.cycle.Content;
 import org.activiti.cycle.RepositoryArtifact;
 import org.activiti.cycle.impl.connector.signavio.SignavioConnector;
-import org.activiti.cycle.impl.connector.signavio.util.NewSignavioSvgApiBuilder;
 import org.activiti.cycle.impl.connector.signavio.util.SignavioSvgApiBuilder;
 
 public class SvgApiProvider extends SignavioContentRepresentationProvider {
 
   @Override
   public void addValueToContent(Content content, SignavioConnector connector, RepositoryArtifact artifact) {
-      String text = new NewSignavioSvgApiBuilder(connector, artifact).buildHtml();
+      String text = new SignavioSvgApiBuilder(connector, artifact).buildHtml();
       content.setValue(text);
   }
 
