aa49b62d6c84c99c474b097b967a2563ae3db5d5
tombaeyens <tombaeyens@51b7ce9a-3489-0410-86de-dc9a27cf12c7>
2010-12-08 13:56:55 +0000

    ACT-421 adding test for task-local variables

--- modules/activiti-engine/src/test/java/org/activiti/engine/test/api/task/TaskVariablesTest.java
+++ modules/activiti-engine/src/test/java/org/activiti/engine/test/api/task/TaskVariablesTest.java
@@ -13,8 +13,12 @@
 
 package org.activiti.engine.test.api.task;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.activiti.engine.impl.test.PluggableActivitiTestCase;
 import org.activiti.engine.task.Task;
+import org.activiti.engine.test.Deployment;
 
 
 /**
@@ -32,5 +36,51 @@ public class TaskVariablesTest extends PluggableActivitiTestCase {
     assertEquals("trumpet", taskService.getVariable(taskId, "instrument"));
     
     taskService.deleteTask(taskId);
+  }
+  
+  @Deployment
+  public void testTaskExecutionVariables() {
+    String processInstanceId = runtimeService.startProcessInstanceByKey("oneTaskProcess").getId();
+    String taskId = taskService.createTaskQuery().singleResult().getId();
+    
+    Map<String, Object> expectedVariables = new HashMap<String, Object>();
+    assertEquals(expectedVariables, runtimeService.getVariables(processInstanceId));
+    assertEquals(expectedVariables, taskService.getVariables(taskId));
+    assertEquals(expectedVariables, runtimeService.getVariablesLocal(processInstanceId));
+    assertEquals(expectedVariables, taskService.getVariablesLocal(taskId));
+    
+    runtimeService.setVariable(processInstanceId, "instrument", "trumpet");
+    
+    expectedVariables = new HashMap<String, Object>();
+    assertEquals(expectedVariables, taskService.getVariablesLocal(taskId));
+    expectedVariables.put("instrument", "trumpet");
+    assertEquals(expectedVariables, runtimeService.getVariables(processInstanceId));
+    assertEquals(expectedVariables, taskService.getVariables(taskId));
+    assertEquals(expectedVariables, runtimeService.getVariablesLocal(processInstanceId));
+    
+    taskService.setVariable(taskId, "player", "gonzo");
+    
+    expectedVariables = new HashMap<String, Object>();
+    assertEquals(expectedVariables, taskService.getVariablesLocal(taskId));
+    expectedVariables.put("player", "gonzo");
+    expectedVariables.put("instrument", "trumpet");
+    assertEquals(expectedVariables, runtimeService.getVariables(processInstanceId));
+    assertEquals(expectedVariables, taskService.getVariables(taskId));
+    assertEquals(expectedVariables, runtimeService.getVariablesLocal(processInstanceId));
+    
+    taskService.setVariableLocal(taskId, "budget", "unlimited");
+    
+    expectedVariables = new HashMap<String, Object>();
+    expectedVariables.put("budget", "unlimited");
+    assertEquals(expectedVariables, taskService.getVariablesLocal(taskId));
+    expectedVariables.put("player", "gonzo");
+    expectedVariables.put("instrument", "trumpet");
+    assertEquals(expectedVariables, taskService.getVariables(taskId));
+
+    expectedVariables = new HashMap<String, Object>();
+    expectedVariables.put("player", "gonzo");
+    expectedVariables.put("instrument", "trumpet");
+    assertEquals(expectedVariables, runtimeService.getVariables(processInstanceId));
+    assertEquals(expectedVariables, runtimeService.getVariablesLocal(processInstanceId));
   }
 }
