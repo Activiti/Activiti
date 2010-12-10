c1c1520d377cc37e3aebea01207b9127b12dfe66
tombaeyens <tombaeyens@51b7ce9a-3489-0410-86de-dc9a27cf12c7>
2010-12-10 16:46:09 +0000

    ACT-420 Automatic upgrade

---
+++ activiti-5.0/modules/activiti-upgrade/src/main/java/org/activiti/upgrade/test/UserTaskAfterTest.java
@@ -0,0 +1,75 @@
+/* Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.activiti.upgrade.test;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.activiti.engine.task.Task;
+import org.activiti.engine.test.Deployment;
+
+
+/**
+ * @author Tom Baeyens
+ */
+public class UserTaskAfterTest extends UpgradeTestCase {
+
+  @Deployment
+  public void testSimplestTask() {
+    Task task = taskService
+      .createTaskQuery()
+      .taskName("simpleTask")
+      .singleResult();
+    
+    assertNotNull(task);
+    
+    String processInstanceId = task.getProcessInstanceId();
+    
+    taskService.complete(task.getId());
+    
+    assertEquals(0, runtimeService
+      .createExecutionQuery()
+      .processInstanceId(processInstanceId)
+      .list()
+      .size());
+  }
+
+  @Deployment
+  public void testTaskWithProcessVariables() {
+    Task task = taskService
+      .createTaskQuery()
+      .taskName("taskWithExecutionVariables")
+      .singleResult();
+
+    String taskId = task.getId();
+
+    assertNotNull(task);
+
+    String processInstanceId = task.getProcessInstanceId();
+    
+    Map<String, Object> expectedVariables = new HashMap<String, Object>();
+    expectedVariables.put("instrument", "trumpet");
+    expectedVariables.put("player", "gonzo");
+
+//    taskService.getVariables(taskId);
+
+    taskService.complete(taskId);
+
+    assertEquals(0, runtimeService
+      .createExecutionQuery()
+      .processInstanceId(processInstanceId)
+      .list()
+      .size());
+  }
+}
