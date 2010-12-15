57f979182df2aa2f3c5fb6712ccc4a87d52f2e0c
bernd.ruecker <bernd.ruecker@51b7ce9a-3489-0410-86de-dc9a27cf12c7>
2010-12-15 20:07:32 +0000

    HEMERA-474: Moved incubating cycle connectors

--- modules/activiti-cycle/src/test/java/org/activiti/cycle/impl/conf/RepositoryConfigurationHandlerTest.java
+++ modules/activiti-cycle/src/test/java/org/activiti/cycle/impl/conf/RepositoryConfigurationHandlerTest.java
@@ -6,8 +6,6 @@ import java.util.UUID;
 
 import org.activiti.cycle.impl.connector.fs.FileSystemConnectorConfiguration;
 import org.activiti.cycle.impl.connector.signavio.SignavioConnectorConfiguration;
-import org.activiti.cycle.incubator.connector.svn.SvnConnectorConfiguration;
-import org.activiti.cycle.incubator.connector.vfs.VfsConnectorConfiguration;
 import org.junit.Test;
 
 public class RepositoryConfigurationHandlerTest {
@@ -15,21 +13,19 @@ public class RepositoryConfigurationHandlerTest {
   @Test
   public void testGetConfigurationFields() {
     RepositoryConfigurationHandler handler = new RepositoryConfigurationHandler();
-    System.out.println(handler.getConfigurationFields(SvnConnectorConfiguration.class.getCanonicalName()));
     System.out.println(handler.getConfigurationFields(SignavioConnectorConfiguration.class.getCanonicalName()));
     System.out.println(handler.getConfigurationFields(FileSystemConnectorConfiguration.class.getCanonicalName()));
-    System.out.println(handler.getConfigurationFields(VfsConnectorConfiguration.class.getCanonicalName()));
   }
 
   @Test
   public void setConfigurationFields() {
     RepositoryConfigurationHandler handler = new RepositoryConfigurationHandler();
     Map<String, String> keyValueMap = new HashMap<String, String>();
-    for (String fieldname : handler.getConfigurationFields(SvnConnectorConfiguration.class.getCanonicalName()).keySet()) {
+    for (String fieldname : handler.getConfigurationFields(SignavioConnectorConfiguration.class.getCanonicalName()).keySet()) {
       keyValueMap.put(fieldname, UUID.randomUUID().toString());
     }
     
-    SvnConnectorConfiguration config = new SvnConnectorConfiguration();
+    SignavioConnectorConfiguration config = new SignavioConnectorConfiguration();
     handler.setConfigurationfields(keyValueMap, config);
     
     System.out.println(config);
