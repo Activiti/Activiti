dcae18d7c7f81ae9ff216cb9bbbc9dd170d89641
jbarrez <jbarrez@51b7ce9a-3489-0410-86de-dc9a27cf12c7>
2011-01-07 15:34:50 +0000

    Added support for debugging KickStart with in memory database

--- modules/activiti-webapp-kickstart/src/main/java/org/activiti/kickstart/engine/ProcessEnginesServletContextListener.java
+++ modules/activiti-webapp-kickstart/src/main/java/org/activiti/kickstart/engine/ProcessEnginesServletContextListener.java
@@ -12,18 +12,37 @@
  */
 package org.activiti.kickstart.engine;
 
+import java.util.logging.Logger;
+
 import javax.servlet.ServletContextEvent;
 import javax.servlet.ServletContextListener;
 
+import org.activiti.engine.ProcessEngine;
 import org.activiti.engine.ProcessEngines;
+import org.activiti.engine.impl.cfg.StandaloneInMemProcessEngineConfiguration;
 
 /**
  * @author Joram Barrez
  */
 public class ProcessEnginesServletContextListener implements ServletContextListener {
+  
+  protected static final Logger LOGGER = Logger.getLogger(ProcessEnginesServletContextListener.class.getName());
 
   public void contextInitialized(ServletContextEvent servletContextEvent) {
     ProcessEngines.init();
+    
+    ProcessEngine processEngine = ProcessEngines.getDefaultProcessEngine();
+    if (processEngine == null) {
+      LOGGER.warning("Could not construct a process engine for KickStart. " 
+              + "Please verify if your activiti.cfg.xml configuration is correct.");
+      
+      if ("true".equals(System.getProperty("KickStartDebugInMem"))) {
+        LOGGER.info("KickStartDebugInMem system property found. Switching to in memory Activiti configuratuon");
+        processEngine = StandaloneInMemProcessEngineConfiguration
+          .createStandaloneInMemProcessEngineConfiguration().buildProcessEngine();
+        ProcessEngines.registerProcessEngine(processEngine);
+      }
+    }
   }
 
   public void contextDestroyed(ServletContextEvent servletContextEvent) {
