5debbbfb4d9226d91ee1324563c6b3dd179b7e03
meyerd <meyerd@51b7ce9a-3489-0410-86de-dc9a27cf12c7>
2011-02-07 13:10:58 +0000

    First try to reuse engine-provided schema generation

--- modules/activiti-cycle/src/test/java/org/activiti/cycle/impl/ActivitiCycleDbAwareTest.java
+++ modules/activiti-cycle/src/test/java/org/activiti/cycle/impl/ActivitiCycleDbAwareTest.java
@@ -4,50 +4,18 @@ import org.activiti.cycle.context.CycleApplicationContext;
 import org.activiti.cycle.context.CycleMapContext;
 import org.activiti.cycle.context.CycleRequestContext;
 import org.activiti.cycle.context.CycleSessionContext;
-import org.activiti.engine.impl.ProcessEngineImpl;
-import org.activiti.engine.impl.db.DbSqlSession;
-import org.activiti.engine.impl.interceptor.Command;
-import org.activiti.engine.impl.interceptor.CommandContext;
-import org.activiti.engine.impl.interceptor.CommandExecutor;
 import org.activiti.engine.impl.test.PluggableActivitiTestCase;
 
 public abstract class ActivitiCycleDbAwareTest extends PluggableActivitiTestCase {
 
   @Override
   protected void setUp() throws Exception {
-    ensureCycleDbCreated();
     setupCycleContextInfrastructure();
   }
 
   @Override
   protected void tearDown() throws Exception {
     tearDownCycleContextInfrastructure();
-    ensureCycleDbDropped();
-  }
-
-  protected void ensureCycleDbCreated() {
-    CommandExecutor commandExecutor = ((ProcessEngineImpl) processEngine).getProcessEngineConfiguration().getCommandExecutorTxRequired();
-    commandExecutor.execute(new Command<Object>() {
-
-      public Object execute(CommandContext commandContext) {
-        DbSqlSession session = commandContext.getSession(DbSqlSession.class);
-//        session.executeSchemaResourceOperation("cycle/drop", "drop");
-//        session.executeSchemaResourceOperation("cycle/create", "create");
-        return null;
-      }
-    });
-  }
-
-  protected void ensureCycleDbDropped() {
-    CommandExecutor commandExecutor = ((ProcessEngineImpl) processEngine).getProcessEngineConfiguration().getCommandExecutorTxRequired();
-    commandExecutor.execute(new Command<Object>() {
-
-      public Object execute(CommandContext commandContext) {
-        DbSqlSession session = commandContext.getSession(DbSqlSession.class);
-//        session.executeSchemaResourceOperation("cycle/drop", "drop");
-        return null;
-      }
-    });
   }
 
   protected void setupCycleContextInfrastructure() {
