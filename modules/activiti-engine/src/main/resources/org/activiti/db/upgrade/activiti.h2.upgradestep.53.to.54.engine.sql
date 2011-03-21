d2a43732a0fb496b85b710708c25511aee00d742
tombaeyens <tombaeyens@51b7ce9a-3489-0410-86de-dc9a27cf12c7>
2011-03-21 16:44:23 +0000

    splitting multiple column adds into separate statements in h2 schema upgrade from 5.3 to 5.4

--- modules/activiti-engine/src/main/resources/org/activiti/db/upgrade/activiti.h2.upgradestep.53.to.54.engine.sql
+++ modules/activiti-engine/src/main/resources/org/activiti/db/upgrade/activiti.h2.upgradestep.53.to.54.engine.sql
@@ -1,4 +1,8 @@
 alter table ACT_RU_TASK 
-add OWNER_ varchar(64),
-    DELEGATION_ varchar(64),
-    DUE_DATE_ timestamp;
+add OWNER_ varchar(64);
+
+alter table ACT_RU_TASK 
+add DELEGATION_ varchar(64);
+
+alter table ACT_RU_TASK 
+add DUE_DATE_ timestamp;
