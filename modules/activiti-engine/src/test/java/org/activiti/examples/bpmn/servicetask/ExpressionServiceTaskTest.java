f6efe17fd908ef9fa38d5fbd4740a0c8446d2441
tombaeyens <tombaeyens@51b7ce9a-3489-0410-86de-dc9a27cf12c7>
2011-03-24 09:19:16 +0000

    ACT-628 re-enabled commented test in ExpressionServiceTaskTest

--- modules/activiti-engine/src/test/java/org/activiti/examples/bpmn/servicetask/ExpressionServiceTaskTest.java
+++ modules/activiti-engine/src/test/java/org/activiti/examples/bpmn/servicetask/ExpressionServiceTaskTest.java
@@ -12,13 +12,13 @@ import org.activiti.engine.test.Deployment;
  */
 public class ExpressionServiceTaskTest extends PluggableActivitiTestCase {
 
-//  @Deployment
-//  public void testSetServiceResultToProcessVariables() {
-//    Map<String,Object> variables = new HashMap<String, Object>();
-//    variables.put("bean", new ValueBean("ok"));
-//    ProcessInstance pi = runtimeService.startProcessInstanceByKey("setServiceResultToProcessVariables", variables);
-//    assertEquals("ok", runtimeService.getVariable(pi.getId(), "result"));
-//  }
+  @Deployment
+  public void testSetServiceResultToProcessVariables() {
+    Map<String,Object> variables = new HashMap<String, Object>();
+    variables.put("bean", new ValueBean("ok"));
+    ProcessInstance pi = runtimeService.startProcessInstanceByKey("setServiceResultToProcessVariables", variables);
+    assertEquals("ok", runtimeService.getVariable(pi.getId(), "result"));
+  }
 
   @Deployment
   public void testBackwardsCompatibleExpression() {
