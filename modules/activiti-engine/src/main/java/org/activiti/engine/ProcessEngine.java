5ecfa78373d9656c9a244eca70442d7b051b6fe8
tombaeyens <tombaeyens@51b7ce9a-3489-0410-86de-dc9a27cf12c7>
2011-03-30 09:08:44 +0000

    updating version to 5.5-SNAPSHOT

--- modules/activiti-engine/src/main/java/org/activiti/engine/ProcessEngine.java
+++ modules/activiti-engine/src/main/java/org/activiti/engine/ProcessEngine.java
@@ -49,7 +49,7 @@ package org.activiti.engine;
 public interface ProcessEngine {
 
   /** the version of the activiti library */
-  public static String VERSION = "5.4-SNAPSHOT";
+  public static String VERSION = "5.5-SNAPSHOT";
 
   /** The name as specified in 'process-engine-name' in 
    * the activiti.cfg.xml configuration file.
