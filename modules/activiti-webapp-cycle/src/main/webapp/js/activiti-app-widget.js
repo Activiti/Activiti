1a95070c7206cda217b4796d2e3c24f1f762f88d
nilspreusker <nilspreusker@51b7ce9a-3489-0410-86de-dc9a27cf12c7>
2011-04-08 14:29:02 +0000

    made file chooser dialog open in the top left corner in order to avoid scrollbar issue

--- modules/activiti-webapp-cycle/src/main/webapp/js/activiti-app-widget.js
+++ modules/activiti-webapp-cycle/src/main/webapp/js/activiti-app-widget.js
@@ -148,7 +148,7 @@
       content.innerHTML = '<div class="bd"><h1>Select ' + (this._highlightFolders ? 'folder' : 'file') + '</h1><div id="fileChooserTree" class="ygtv-checkbox"/></div>';
 
       this._dialog = new YAHOO.widget.Dialog(content, {
-        fixedcenter: true,
+        fixedcenter: false,
         visible: false,
         constraintoviewport: true,
         modal: true,
