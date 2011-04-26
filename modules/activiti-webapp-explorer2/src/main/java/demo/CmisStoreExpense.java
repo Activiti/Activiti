da9612d96dafa6822bb53b542b8a67100a89a58c
jbarrez <jbarrez@51b7ce9a-3489-0410-86de-dc9a27cf12c7>
2011-04-26 15:32:55 +0000

    UI: added CMIS demo delegate

--- modules/activiti-webapp-explorer2/src/main/java/demo/CmisStoreExpense.java
+++ modules/activiti-webapp-explorer2/src/main/java/demo/CmisStoreExpense.java
@@ -13,8 +13,22 @@
 
 package demo;
 
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 import org.activiti.engine.delegate.DelegateExecution;
 import org.activiti.engine.delegate.JavaDelegate;
+import org.apache.chemistry.opencmis.client.api.Folder;
+import org.apache.chemistry.opencmis.client.api.Session;
+import org.apache.pdfbox.exceptions.COSVisitorException;
+import org.apache.pdfbox.pdmodel.PDDocument;
+import org.apache.pdfbox.pdmodel.PDPage;
+import org.apache.pdfbox.pdmodel.edit.PDPageContentStream;
+import org.apache.pdfbox.pdmodel.font.PDFont;
+import org.apache.pdfbox.pdmodel.font.PDType1Font;
 
 
 /**
@@ -22,8 +36,49 @@ import org.activiti.engine.delegate.JavaDelegate;
  */
 public class CmisStoreExpense implements JavaDelegate {
   
+  private static final String FOLDER = "activiti-demo";
+  private static final DateFormat DATE_FORMATTER = new SimpleDateFormat("yyyy-MM-dd-HH-mm-ss");
+  
   public void execute(DelegateExecution execution) throws Exception {
-    System.out.println("Hello World");
-  }
+    Session session = CmisUtil.createCmisSession("admin", "admin", "http://cmis.alfresco.com:80/service/cmis");
 
+    // Get or create folder
+    Folder folder = CmisUtil.getFolder(session, FOLDER);
+    if (folder == null) {
+      folder = CmisUtil.createFolder(session, session.getRootFolder(), FOLDER); 
+    }
+    
+    // Create document
+    byte[] pdfDoc = createPdfDocument(execution);
+    
+    // Push document to CMIS
+    CmisUtil.createDocument(session, folder, "expense_" + DATE_FORMATTER.format(new Date()) + ".pdf", pdfDoc, "application/pdf");
+  }
+  
+  protected byte[] createPdfDocument(DelegateExecution execution) throws IOException, COSVisitorException {
+    PDDocument document = new PDDocument();
+    PDPage page = new PDPage();
+    document.addPage(page);
+    PDFont font = PDType1Font.HELVETICA_BOLD;
+    PDPageContentStream contentStream = new PDPageContentStream(document, page);
+    contentStream.beginText();
+    contentStream.setFont(font, 14);
+    contentStream.moveTextPositionByAmount(100, 700);
+    contentStream.drawString("Refund request by " + execution.getVariable("initiator"));
+    contentStream.endText();
+    contentStream.close();
+    
+    contentStream.beginText();
+    contentStream.setFont(font, 12);
+    contentStream.moveTextPositionByAmount(100, 650);
+    contentStream.drawString("amount: " + execution.getVariable("amount") + " EUR");
+    contentStream.endText();
+    contentStream.close();
+    
+    ByteArrayOutputStream bos = new ByteArrayOutputStream();
+    document.save(bos);
+    document.close();
+    return bos.toByteArray();
+  }
+  
 }
