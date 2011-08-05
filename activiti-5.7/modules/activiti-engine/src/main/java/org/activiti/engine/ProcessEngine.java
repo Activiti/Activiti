ff83aa54a5ec90f1bad830689e3d15698cf06702
tombaeyens <tombaeyens@51b7ce9a-3489-0410-86de-dc9a27cf12c7>
2011-08-05 15:14:45 +0000

    set version to 5.7

--- activiti-5.7/modules/activiti-engine/src/main/java/org/activiti/engine/ProcessEngine.java
+++ activiti-5.7/modules/activiti-engine/src/main/java/org/activiti/engine/ProcessEngine.java
@@ -49,7 +49,7 @@ package org.activiti.engine;
 public interface ProcessEngine {
 
   /** the version of the activiti library */
-  public static String VERSION = "5.7-SNAPSHOT";
+  public static String VERSION = "5.7";
 
   /** The name as specified in 'process-engine-name' in 
    * the activiti.cfg.xml configuration file.
