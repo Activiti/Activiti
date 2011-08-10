254a67b457c5de00659ec7c892535c4611b53df5
jbarrez <jbarrez@51b7ce9a-3489-0410-86de-dc9a27cf12c7>
2011-08-10 07:16:08 +0000

    Failing test with repeating service task

---
+++ modules/activiti-engine/src/test/java/org/activiti/engine/test/bpmn/servicetask/RepeatingServiceTaskTest.java
@@ -0,0 +1,39 @@
+/* Copyright (C) 2005-2011 of Alfresco. All rights reserved.
+ *
+ * This file is part of Alfresco Pangu.
+ * 
+ * Alfresco Pangu is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * Alfresco Pangu is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with Alfresco Pangu.  If not, see <http://www.gnu.org/licenses/>. 
+ */
+
+package org.activiti.engine.test.bpmn.servicetask;
+
+import org.activiti.engine.impl.test.PluggableActivitiTestCase;
+import org.activiti.engine.impl.util.CollectionUtil;
+import org.activiti.engine.runtime.ProcessInstance;
+import org.activiti.engine.test.Deployment;
+
+
+/**
+ * @author Joram Barrez
+ */
+public class RepeatingServiceTaskTest extends PluggableActivitiTestCase {
+  
+//  @Deployment
+//  public void testMultipleInvocationsInSameTransation() {
+//    ProcessInstance processInstance = 
+//        runtimeService.startProcessInstanceByKey("repeating", CollectionUtil.singletonMap("count", 0));
+//    assertTrue(processInstance.isEnded());
+//  }
+
+}
