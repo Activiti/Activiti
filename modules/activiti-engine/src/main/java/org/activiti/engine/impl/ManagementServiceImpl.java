ae3be4f7b651fbb42b0997651c235cb3dd1d447b
frederikheremans <frederikheremans@51b7ce9a-3489-0410-86de-dc9a27cf12c7>
2011-09-12 08:16:09 +0000

    Using DBSQLSession.class instead of factory class to lookup (fixes NPE when method is called)

--- modules/activiti-engine/src/main/java/org/activiti/engine/impl/ManagementServiceImpl.java
+++ modules/activiti-engine/src/main/java/org/activiti/engine/impl/ManagementServiceImpl.java
@@ -67,7 +67,7 @@ public class ManagementServiceImpl extends ServiceImpl implements ManagementServ
   public String databaseSchemaUpgrade(final Connection connection, final String catalog, final String schema) {
     return commandExecutor.execute(new Command<String>(){
       public String execute(CommandContext commandContext) {
-        DbSqlSessionFactory dbSqlSessionFactory = (DbSqlSessionFactory) commandContext.getSessionFactories().get(DbSqlSessionFactory.class);
+        DbSqlSessionFactory dbSqlSessionFactory = (DbSqlSessionFactory) commandContext.getSessionFactories().get(DbSqlSession.class);
         DbSqlSession dbSqlSession = new DbSqlSession(dbSqlSessionFactory, connection, catalog, schema);
         commandContext.getSessions().put(DbSqlSession.class, dbSqlSession);
         return dbSqlSession.dbSchemaUpdate();
