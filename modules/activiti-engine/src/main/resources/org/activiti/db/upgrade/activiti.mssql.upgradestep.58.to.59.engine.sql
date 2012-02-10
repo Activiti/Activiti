5269de2116b380077899c7a7eeafd7d6816d7a8c
trademak <trademak@51b7ce9a-3489-0410-86de-dc9a27cf12c7>
2012-02-10 12:30:12 +0000

    Database upgrade 5.9 fix

--- modules/activiti-engine/src/main/resources/org/activiti/db/upgrade/activiti.mssql.upgradestep.58.to.59.engine.sql
+++ modules/activiti-engine/src/main/resources/org/activiti/db/upgrade/activiti.mssql.upgradestep.58.to.59.engine.sql
@@ -1,4 +1,4 @@
-alter table AT_RU_EXECUTION 
+alter table ACT_RU_EXECUTION 
 add SUSPENSION_STATE_ int;
 
 alter table ACT_RE_PROCDEF
@@ -9,4 +9,4 @@ add REV_ int;
 
 update ACT_RE_PROCDEF set REV_ = 1;
 update ACT_RE_PROCDEF set SUSPENSION_STATE_ = 1;
-update AT_RU_EXECUTION set SUSPENSION_STATE_ = 1;
+update ACT_RU_EXECUTION set SUSPENSION_STATE_ = 1;
