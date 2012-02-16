fac03863336d5bd26d9ad612749e9737e39b4a8d
meyerd <meyerd@51b7ce9a-3489-0410-86de-dc9a27cf12c7>
2012-02-16 12:14:40 +0000

    ACT-1103 adding testcase verifying that the PVM is able to start a process instance at an alternative initial

--- modules/activiti-engine/src/test/java/org/activiti/engine/test/pvm/PvmScopeAndEventsTest.java
+++ modules/activiti-engine/src/test/java/org/activiti/engine/test/pvm/PvmScopeAndEventsTest.java
@@ -18,9 +18,12 @@ import java.util.List;
 
 import org.activiti.engine.delegate.ExecutionListener;
 import org.activiti.engine.impl.pvm.ProcessDefinitionBuilder;
+import org.activiti.engine.impl.pvm.PvmActivity;
 import org.activiti.engine.impl.pvm.PvmExecution;
 import org.activiti.engine.impl.pvm.PvmProcessDefinition;
 import org.activiti.engine.impl.pvm.PvmProcessInstance;
+import org.activiti.engine.impl.pvm.process.ActivityImpl;
+import org.activiti.engine.impl.pvm.process.ProcessDefinitionImpl;
 import org.activiti.engine.impl.test.PvmTestCase;
 import org.activiti.engine.test.pvm.activities.Automatic;
 import org.activiti.engine.test.pvm.activities.End;
@@ -117,6 +120,128 @@ public class PvmScopeAndEventsTest extends PvmTestCase {
     expectedEvents.add("start on Activity(firstMostInnerNestedActivity)");
     expectedEvents.add("start on Activity(start)");
     expectedEvents.add("end on Activity(start)");
+    expectedEvents.add("start on Activity(waitInFirst)");
+
+    assertEquals("expected "+expectedEvents+", but was \n"+eventCollector+"\n", expectedEvents, eventCollector.events); 
+    eventCollector.events.clear();
+    
+    PvmExecution execution = processInstance.findExecution("waitInFirst");
+    execution.signal(null, null);
+    
+    expectedEvents = new ArrayList<String>();
+    expectedEvents.add("end on Activity(waitInFirst)");
+    expectedEvents.add("end on Activity(firstMostInnerNestedActivity)");
+    expectedEvents.add("end on Activity(firstInnerScope)");
+    expectedEvents.add("start on Activity(secondInnerScope)");
+    expectedEvents.add("start on Activity(secondMostInnerNestedActivity)");
+    expectedEvents.add("start on Activity(waitInSecond)");
+
+    assertEquals("expected "+expectedEvents+", but was \n"+eventCollector+"\n", expectedEvents, eventCollector.events); 
+    eventCollector.events.clear();
+    
+    execution = processInstance.findExecution("waitInSecond");
+    execution.signal(null, null);
+    
+    expectedEvents = new ArrayList<String>();
+    expectedEvents.add("end on Activity(waitInSecond)");
+    expectedEvents.add("start on Activity(end)");
+    expectedEvents.add("end on Activity(end)");
+    expectedEvents.add("end on Activity(secondMostInnerNestedActivity)");
+    expectedEvents.add("end on Activity(secondInnerScope)");
+    expectedEvents.add("end on Activity(outerScope)");
+    expectedEvents.add("end on Activity(mostOuterNestedActivity)");
+    expectedEvents.add("end on ProcessDefinition(scopes and events)");
+    
+    assertEquals("expected "+expectedEvents+", but was \n"+eventCollector+"\n", expectedEvents, eventCollector.events); 
+    eventCollector.events.clear();
+  }
+  
+  /**
+   * +--------------------------------------------------------------------------------+
+   * | mostOuterNestedActivity                                                        |                  
+   * | +----------------------------------------------------------------------------+ |
+   * | | outerScope (scope)                                                         | |
+   * | | +----------------------------------+ +-----------------------------------+ | |
+   * | | | firstInnerScope (scope)          | | secondInnerScope (scope)          | | |
+   * | | | +------------------------------+ | | +-------------------------------+ | | |
+   * | | | | firstMostInnerNestedActivity | | | | secondMostInnerNestedActivity | | | |
+   * | | | | +-------+   +-------------+  | | | | +--------------+    +-----+   | | | |
+   * | | | | | start |-->| waitInFirst |--------->| waitInSecond |--> | end |   | | | |
+   * | | | | +-------+   +-------------+  | | | | +--------------+    +-----+   | | | |
+   * | | | +------------------------------+ | | +-------------------------------+ | | |
+   * | | +----------------------------------+ +-----------------------------------+ | |
+   * | +----------------------------------------------------------------------------+ |
+   * +--------------------------------------------------------------------------------+
+   * 
+   */
+  // this test does not start the process at the initial (the activity with id 'start'), but at 
+  // 'waitInFirst'
+  public void testStartEndWithScopesAndNestedActivitiesNotAtInitial() {
+    EventCollector eventCollector = new EventCollector();
+    
+    PvmProcessDefinition processDefinition = new ProcessDefinitionBuilder("scopes and events")
+      .executionListener(org.activiti.engine.impl.pvm.PvmEvent.EVENTNAME_START, eventCollector)
+      .executionListener(org.activiti.engine.impl.pvm.PvmEvent.EVENTNAME_END, eventCollector)
+      .createActivity("mostOuterNestedActivity")
+        .executionListener(org.activiti.engine.impl.pvm.PvmEvent.EVENTNAME_START, eventCollector)
+        .executionListener(org.activiti.engine.impl.pvm.PvmEvent.EVENTNAME_END, eventCollector)
+        .createActivity("outerScope")
+          .executionListener(org.activiti.engine.impl.pvm.PvmEvent.EVENTNAME_START, eventCollector)
+          .executionListener(org.activiti.engine.impl.pvm.PvmEvent.EVENTNAME_END, eventCollector)
+          .createActivity("firstInnerScope")
+            .executionListener(org.activiti.engine.impl.pvm.PvmEvent.EVENTNAME_START, eventCollector)
+            .executionListener(org.activiti.engine.impl.pvm.PvmEvent.EVENTNAME_END, eventCollector)
+            .createActivity("firstMostInnerNestedActivity")
+              .executionListener(org.activiti.engine.impl.pvm.PvmEvent.EVENTNAME_START, eventCollector)
+              .executionListener(org.activiti.engine.impl.pvm.PvmEvent.EVENTNAME_END, eventCollector)
+              .createActivity("start")
+                .initial()
+                .behavior(new Automatic())
+                .executionListener(org.activiti.engine.impl.pvm.PvmEvent.EVENTNAME_START, eventCollector)
+                .executionListener(org.activiti.engine.impl.pvm.PvmEvent.EVENTNAME_END, eventCollector)
+                .transition("waitInFirst")
+              .endActivity()
+              .createActivity("waitInFirst")
+                .behavior(new WaitState())
+                .executionListener(org.activiti.engine.impl.pvm.PvmEvent.EVENTNAME_START, eventCollector)
+                .executionListener(org.activiti.engine.impl.pvm.PvmEvent.EVENTNAME_END, eventCollector)
+                .transition("waitInSecond")
+              .endActivity()
+            .endActivity()
+          .endActivity()
+          .createActivity("secondInnerScope")
+            .executionListener(org.activiti.engine.impl.pvm.PvmEvent.EVENTNAME_START, eventCollector)
+            .executionListener(org.activiti.engine.impl.pvm.PvmEvent.EVENTNAME_END, eventCollector)
+            .createActivity("secondMostInnerNestedActivity")
+              .executionListener(org.activiti.engine.impl.pvm.PvmEvent.EVENTNAME_START, eventCollector)
+              .executionListener(org.activiti.engine.impl.pvm.PvmEvent.EVENTNAME_END, eventCollector)
+              .createActivity("waitInSecond")
+                .behavior(new WaitState())
+                .executionListener(org.activiti.engine.impl.pvm.PvmEvent.EVENTNAME_START, eventCollector)
+                .executionListener(org.activiti.engine.impl.pvm.PvmEvent.EVENTNAME_END, eventCollector)
+                .transition("end")
+              .endActivity()
+              .createActivity("end")
+                .behavior(new End())
+                .executionListener(org.activiti.engine.impl.pvm.PvmEvent.EVENTNAME_START, eventCollector)
+                .executionListener(org.activiti.engine.impl.pvm.PvmEvent.EVENTNAME_END, eventCollector)
+              .endActivity()
+            .endActivity()
+          .endActivity()
+        .endActivity()
+      .endActivity()
+    .buildProcessDefinition();
+    
+    ActivityImpl alternativeInitial = (ActivityImpl) processDefinition.findActivity("waitInFirst");
+    PvmProcessInstance processInstance = ((ProcessDefinitionImpl)processDefinition).createProcessInstanceForInitial(alternativeInitial);
+    processInstance.start();
+    
+    List<String> expectedEvents = new ArrayList<String>();
+    expectedEvents.add("start on ProcessDefinition(scopes and events)");
+    expectedEvents.add("start on Activity(mostOuterNestedActivity)");
+    expectedEvents.add("start on Activity(outerScope)");
+    expectedEvents.add("start on Activity(firstInnerScope)");
+    expectedEvents.add("start on Activity(firstMostInnerNestedActivity)");
     expectedEvents.add("start on Activity(waitInFirst)");
 
     assertEquals("expected "+expectedEvents+", but was \n"+eventCollector+"\n", expectedEvents, eventCollector.events); 
