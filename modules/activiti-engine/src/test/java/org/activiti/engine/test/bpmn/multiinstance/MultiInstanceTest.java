48330cc37f356f0bbdae03886dc91a043bcbbaff
meyerd <meyerd@51b7ce9a-3489-0410-86de-dc9a27cf12c7>
2012-02-17 11:48:27 +0000

    Fixing testcase which fails because string comparison != number comparison

--- modules/activiti-engine/src/test/java/org/activiti/engine/test/bpmn/multiinstance/MultiInstanceTest.java
+++ modules/activiti-engine/src/test/java/org/activiti/engine/test/bpmn/multiinstance/MultiInstanceTest.java
@@ -813,12 +813,17 @@ public class MultiInstanceTest extends PluggableActivitiTestCase {
   public void testNestedParallelCallActivityCompletionCondition() {
     String procId = runtimeService.startProcessInstanceByKey("miNestedParallelCallActivityCompletionCondition").getId();
 
-    List<Task> tasks = taskService.createTaskQuery().orderByTaskId().asc().list();
-    assertEquals(8, tasks.size());
-    for (int i = 0; i < 4; i++) {
-      taskService.complete(tasks.get(i).getId());
-    }
+    assertEquals(8, taskService.createTaskQuery().count());
     
+    for (int i = 0; i < 2; i++) {
+      ProcessInstance nextSubProcessInstance = runtimeService.createProcessInstanceQuery().processDefinitionKey("externalSubProcess").listPage(0, 1).get(0);
+      List<Task> tasks = taskService.createTaskQuery().processInstanceId(nextSubProcessInstance.getId()).list();
+      assertEquals(2, tasks.size());
+      for (Task task : tasks) {
+        taskService.complete(task.getId());
+      }
+    }
+        
     assertProcessEnded(procId);
   }
   
