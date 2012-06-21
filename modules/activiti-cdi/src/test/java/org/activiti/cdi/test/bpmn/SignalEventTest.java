2a3f3b13bbe52040d23cd8734db3b05ce6c1b578
bernd.ruecker <bernd.ruecker@51b7ce9a-3489-0410-86de-dc9a27cf12c7>
2012-06-21 10:01:15 +0000

    ACT-1257: Enhanced test case to fail because of bug

--- modules/activiti-cdi/src/test/java/org/activiti/cdi/test/bpmn/SignalEventTest.java
+++ modules/activiti-cdi/src/test/java/org/activiti/cdi/test/bpmn/SignalEventTest.java
@@ -12,16 +12,61 @@
  */
 package org.activiti.cdi.test.bpmn;
 
+import static org.junit.Assert.*;
+
 import java.util.HashMap;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import javax.inject.Inject;
+import javax.inject.Named;
 
 import junit.framework.Assert;
 
+import org.activiti.cdi.BusinessProcess;
 import org.activiti.cdi.test.CdiActivitiTestCase;
+import org.activiti.engine.RuntimeService;
+import org.activiti.engine.delegate.DelegateExecution;
+import org.activiti.engine.delegate.JavaDelegate;
 import org.activiti.engine.runtime.ProcessInstance;
 import org.activiti.engine.test.Deployment;
 import org.junit.Test;
 
 public class SignalEventTest extends CdiActivitiTestCase {
+  
+//  private static Logger log = Logger.getLogger(SignalEventTest.class.getName());
+  
+  @Named
+  public static class SignalReceivedDelegate implements JavaDelegate {    
+    
+    @Inject
+    private BusinessProcess businessProcess;
+    
+    public void execute(DelegateExecution execution) {
+      businessProcess.setVariable("processName", "catchSignal-visited (was " + businessProcess.getVariable("processName")  + ")");
+//      log.log(Level.INFO, "");
+    }
+  }
+
+  @Named
+  public static class SendSignalDelegate implements JavaDelegate {
+
+    @Inject
+    private RuntimeService runtimeService;  
+
+    @Inject
+    private BusinessProcess businessProcess;   
+
+    public void execute(DelegateExecution execution) throws Exception {
+      businessProcess.setVariable("processName", "throwSignal-visited (was " + businessProcess.getVariable("processName")  + ")");
+
+      String signalProcessInstanceId = (String) execution.getVariable("signalProcessInstanceId");      
+      String executionId = runtimeService.createExecutionQuery().processInstanceId(signalProcessInstanceId).signalEventSubscriptionName("alert").singleResult().getId();      
+      
+      runtimeService.signalEventReceived("alert", executionId);
+    }
+
+  }
 
   @Test
   @Deployment(resources = {"org/activiti/cdi/test/bpmn/SignalEventTests.catchAlertSignalBoundaryWithReceiveTask.bpmn20.xml", 
@@ -29,14 +74,23 @@ public class SignalEventTest extends CdiActivitiTestCase {
   public void testSignalCatchBoundaryWithVariables() {
     HashMap<String, Object> variables1 = new HashMap<String, Object>();
     variables1.put("processName", "catchSignal");
-    ProcessInstance pi1 = runtimeService.startProcessInstanceByKey("catchSignal", variables1);
+    ProcessInstance piCatchSignal = runtimeService.startProcessInstanceByKey("catchSignal", variables1);
         
     HashMap<String, Object> variables2 = new HashMap<String, Object>();
     variables2.put("processName", "throwSignal");
-    ProcessInstance pi2 = runtimeService.startProcessInstanceByKey("throwSignal", variables2);
+    variables2.put("signalProcessInstanceId", piCatchSignal.getProcessInstanceId());
+    ProcessInstance piThrowSignal = runtimeService.startProcessInstanceByKey("throwSignal", variables2);
     
-    Assert.assertEquals("catchSignal", runtimeService.getVariable(pi1.getId(), "processName"));
-    Assert.assertEquals("throwSignal", runtimeService.getVariable(pi2.getId(), "processName"));
+    assertEquals(1, runtimeService.createExecutionQuery().processInstanceId(piCatchSignal.getProcessInstanceId()).activityId("receiveTask").count());
+    assertEquals(1, runtimeService.createExecutionQuery().processInstanceId(piThrowSignal.getProcessInstanceId()).activityId("receiveTask").count());
+    
+    //TODO: THis fails because of http://jira.codehaus.org/browse/ACT-1257, should be fixed and re-enabled :-)
+    //assertEquals("catchSignal-visited (was catchSignal)", runtimeService.getVariable(piCatchSignal.getId(), "processName"));
+    //assertEquals("throwSignal-visited (was throwSignal)", runtimeService.getVariable(piThrowSignal.getId(), "processName"));
+
+    // clean up
+    runtimeService.signal(piCatchSignal.getId());
+    runtimeService.signal(piThrowSignal.getId());
   }
 
 }
