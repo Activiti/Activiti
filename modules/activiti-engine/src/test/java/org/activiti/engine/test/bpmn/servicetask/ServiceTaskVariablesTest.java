cf783f141a741ce1e24c472e16cbfeff41b6a3e7
meyerd <meyerd@51b7ce9a-3489-0410-86de-dc9a27cf12c7>
2012-06-25 13:10:37 +0000

    making test class with all tests disabled "abstract" to fix CI build

--- modules/activiti-engine/src/test/java/org/activiti/engine/test/bpmn/servicetask/ServiceTaskVariablesTest.java
+++ modules/activiti-engine/src/test/java/org/activiti/engine/test/bpmn/servicetask/ServiceTaskVariablesTest.java
@@ -25,7 +25,7 @@ import org.activiti.engine.test.Deployment;
  *
  * @author Daniel Meyer
  */
-public class ServiceTaskVariablesTest extends PluggableActivitiTestCase {
+public abstract class ServiceTaskVariablesTest extends PluggableActivitiTestCase {
   
   static boolean isNullInDelegate2;
   static boolean isNullInDelegate3;
@@ -112,6 +112,6 @@ public class ServiceTaskVariablesTest extends PluggableActivitiTestCase {
     }
     
   }
-  
+
 }
 
