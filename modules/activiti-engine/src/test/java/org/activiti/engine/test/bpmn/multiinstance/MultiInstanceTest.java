8328702bec3dc21090bb2491fe0e8de015d6f97b
bernd.ruecker <bernd.ruecker@51b7ce9a-3489-0410-86de-dc9a27cf12c7>
2012-07-03 15:32:31 +0000

    ACT-955: Tries to reproduce FK violation with MI-Sub Process, but test case runs green

--- modules/activiti-engine/src/test/java/org/activiti/engine/test/bpmn/multiinstance/MultiInstanceTest.java
+++ modules/activiti-engine/src/test/java/org/activiti/engine/test/bpmn/multiinstance/MultiInstanceTest.java
@@ -13,6 +13,7 @@
 
 package org.activiti.engine.test.bpmn.multiinstance;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collection;
 import java.util.Date;
@@ -656,7 +657,41 @@ public class MultiInstanceTest extends PluggableActivitiTestCase {
     
     assertProcessEnded(procId);
   }
-  
+
+  @Deployment(resources = "org/activiti/engine/test/bpmn/multiinstance/MultiInstanceTest.testSequentialCallActivityWithList.bpmn20.xml")
+  public void testSequentialCallActivityWithList() {
+    ArrayList<String> list = new ArrayList<String>();
+    list.add("one");
+    list.add("two");
+    
+    HashMap<String, Object> variables = new HashMap<String, Object>();
+    variables.put("list", list);
+    
+    String procId = runtimeService.startProcessInstanceByKey("parentProcess", variables).getId();
+
+    Task task1 = taskService.createTaskQuery().processVariableValueEquals("element", "one").singleResult();      
+    Task task2 = taskService.createTaskQuery().processVariableValueEquals("element", "two").singleResult();
+    
+    assertNotNull(task1);
+    assertNotNull(task2);
+    
+    HashMap<String, Object> subVariables = new HashMap<String, Object>();
+    subVariables.put("x", "y");
+
+    taskService.complete(task1.getId(), subVariables);
+    taskService.complete(task2.getId(), subVariables);
+
+    Task task3 = taskService.createTaskQuery().processDefinitionKey("midProcess").singleResult();
+    assertNotNull(task3);
+    taskService.complete(task3.getId());
+
+    Task task4 = taskService.createTaskQuery().processDefinitionKey("parentProcess").singleResult();
+    assertNotNull(task4);
+    taskService.complete(task4.getId());
+
+    assertProcessEnded(procId);
+  }
+
   @Deployment(resources = { "org/activiti/engine/test/bpmn/multiinstance/MultiInstanceTest.testSequentialCallActivityWithTimer.bpmn20.xml",
       "org/activiti/engine/test/bpmn/multiinstance/MultiInstanceTest.externalSubProcess.bpmn20.xml" })
   public void testSequentialCallActivityWithTimer() {
