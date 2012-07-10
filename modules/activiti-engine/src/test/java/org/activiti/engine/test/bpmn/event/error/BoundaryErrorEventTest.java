d39ca94c5608f6a018db71aa4f6e83c32cebf029
bernd.ruecker <bernd.ruecker@51b7ce9a-3489-0410-86de-dc9a27cf12c7>
2012-07-10 14:45:34 +0000

    ACT-832: Added test case no longer failing in current codebase

--- modules/activiti-engine/src/test/java/org/activiti/engine/test/bpmn/event/error/BoundaryErrorEventTest.java
+++ modules/activiti-engine/src/test/java/org/activiti/engine/test/bpmn/event/error/BoundaryErrorEventTest.java
@@ -259,6 +259,27 @@ public class BoundaryErrorEventTest extends PluggableActivitiTestCase {
     assertProcessEnded(procId);
   }
   
+  @Deployment(resources = {
+          "org/activiti/engine/test/bpmn/event/error/BoundaryErrorEventTest.testCatchErrorThrownByCallActivityOnCallActivity.bpmn20.xml",
+          "org/activiti/engine/test/bpmn/event/error/BoundaryErrorEventTest.subprocess2ndLevel.bpmn20.xml",
+          "org/activiti/engine/test/bpmn/event/error/BoundaryErrorEventTest.subprocess.bpmn20.xml"
+  })
+  public void testCatchErrorThrownByCallActivityOnCallActivity() throws InterruptedException {
+      String procId = runtimeService.startProcessInstanceByKey("catchErrorOnCallActivity2ndLevel").getId();
+      
+      Task task = taskService.createTaskQuery().singleResult();
+      assertEquals("Task in subprocess", task.getName());
+      
+      taskService.complete(task.getId());
+
+      task = taskService.createTaskQuery().singleResult();
+      assertEquals("Escalated Task", task.getName());
+      
+      // Completing the task will end the process instance
+      taskService.complete(task.getId());
+      assertProcessEnded(procId);
+  }  
+  
   @Deployment
   public void testCatchErrorOnParallelMultiInstance() {
     String procId = runtimeService.startProcessInstanceByKey("catchErrorOnParallelMi").getId();
