31c8ea16ccf8399bd2c9092414f11c0e7672980e
trademak <trademak@51b7ce9a-3489-0410-86de-dc9a27cf12c7>
2012-07-30 13:22:01 +0000

    removed duplicate ACT_IDX_EVENT_SUBSCR drop

--- modules/activiti-engine/src/main/resources/org/activiti/db/drop/activiti.oracle.drop.engine.sql
+++ modules/activiti-engine/src/main/resources/org/activiti/db/drop/activiti.oracle.drop.engine.sql
@@ -15,7 +15,6 @@ drop index ACT_IDX_EXEC_BUSKEY;
 drop index ACT_IDX_TASK_CREATE;
 drop index ACT_IDX_IDENT_LNK_USER;
 drop index ACT_IDX_IDENT_LNK_GROUP;
-drop index ACT_IDX_EVENT_SUBSCR;
 drop index ACT_IDX_VARIABLE_TASK_ID;
 
 alter table ACT_GE_BYTEARRAY 
