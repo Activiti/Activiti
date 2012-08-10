328de068c64c49f04f8e79c0fd500af4be6adf32
hawky4s <hawky4s@51b7ce9a-3489-0410-86de-dc9a27cf12c7>
2012-08-10 09:17:39 +0000

    ACT-1300 New history level
    Added migration to upgrade scripts

--- modules/activiti-engine/src/main/resources/org/activiti/db/upgrade/activiti.mssql.upgradestep.510.to.511.history.sql
+++ modules/activiti-engine/src/main/resources/org/activiti/db/upgrade/activiti.mssql.upgradestep.510.to.511.history.sql
@@ -19,5 +19,27 @@ create table ACT_HI_PROCVARIABLE (
 create index ACT_IDX_HI_PROCVAR_PROC_INST on ACT_HI_PROCVARIABLE(PROC_INST_ID_);
 create index ACT_IDX_HI_PROCVAR_NAME_TYPE on ACT_HI_PROCVARIABLE(NAME_, VAR_TYPE_);
 
+insert into ACT_HI_PROCVARIABLE
+  (ID_,PROC_INST_ID_,EXECUTION_ID_,TASK_ID_,ACT_INST_ID_,NAME_,VAR_TYPE_,REV_,TIME_,BYTEARRAY_ID_,DOUBLE_,LONG_,TEXT_,TEXT2_)
+  select d.ID_,d.PROC_INST_ID_,d.EXECUTION_ID_,d.TASK_ID_,d.ACT_INST_ID_,d.NAME_,d.VAR_TYPE_,d.REV_,d.TIME_,d.BYTEARRAY_ID_,d.DOUBLE_,d.LONG_,d.TEXT_,d.TEXT2_
+  from ACT_HI_DETAIL d
+  inner join
+    (
+      select de.PROC_INST_ID_, de.NAME_, MAX(de.TIME_) as MAXTIME
+      from ACT_HI_DETAIL de
+      inner join ACT_HI_PROCINST p on de.PROC_INST_ID_ = p.ID_
+      where p.END_TIME_ is not NULL
+      group by de.PROC_INST_ID_, de.NAME_
+    )
+  groupeddetail on d.PROC_INST_ID_ = groupeddetail.PROC_INST_ID_
+  and d.NAME_ = groupeddetail.NAME_
+  and d.TIME_ = groupeddetail.MAXTIME
+  and (select prop.VALUE_ from ACT_GE_PROPERTY prop where prop.NAME_ = 'historyLevel') = 3;
+  
+update ACT_GE_PROPERTY
+  set VALUE_ = VALUE_ + 1,
+      REV_ = REV_ + 1
+  where NAME_ = 'historyLevel' and VALUE_ >= 2;
+
 alter table ACT_HI_ACTINST
 add TASK_ID_ nvarchar(64), CALL_PROC_INST_ID_ nvarchar(64);
