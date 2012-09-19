25a7b17be0d685213eb04b6abc331157ca59f7ff
meyerd <meyerd@51b7ce9a-3489-0410-86de-dc9a27cf12c7>
2012-09-19 14:52:47 +0000

    ACT-1381 removing accidentally committed import

--- modules/activiti-engine/src/main/java/org/activiti/engine/impl/AbstractQuery.java
+++ modules/activiti-engine/src/main/java/org/activiti/engine/impl/AbstractQuery.java
@@ -23,7 +23,6 @@ import org.activiti.engine.impl.interceptor.CommandContext;
 import org.activiti.engine.impl.interceptor.CommandExecutor;
 import org.activiti.engine.query.Query;
 import org.activiti.engine.query.QueryProperty;
-import org.junit.runners.ParentRunner;
 
 
 /**
