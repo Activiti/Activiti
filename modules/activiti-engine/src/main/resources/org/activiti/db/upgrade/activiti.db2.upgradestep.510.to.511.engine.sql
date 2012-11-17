fa97d11d5f02a3bf7f4c82390935f19aa4911556
Paweł Mączka <pawel.maczka@gmail.com>
2012-11-17 11:51:13 +0100

    makes DB2 scripts working for upgrades: 5.9->5.10->5.11

---
+++ modules/activiti-engine/src/main/resources/org/activiti/db/upgrade/activiti.db2.upgradestep.510.to.511.engine.sql
@@ -0,0 +1,31 @@
+alter table ACT_RE_DEPLOYMENT 
+    add CATEGORY_ varchar(255);
+    
+alter table ACT_RE_PROCDEF
+    add DESCRIPTION_ varchar(4000);
+    
+create table ACT_RE_MODEL (
+    ID_ varchar(64) not null,
+    NAME_ varchar(255),
+    CATEGORY_ varchar(255),
+    CREATE_TIME_ timestamp,
+    VERSION_ integer,
+    META_INFO_ varchar(4000),
+    EDITOR_SOURCE_VALUE_ID_ varchar(64),
+    EDITOR_SOURCE_EXTRA_VALUE_ID_ varchar(64),
+    primary key (ID_)
+);
+
+alter table ACT_RE_MODEL 
+    add constraint ACT_FK_MODEL_SOURCE 
+    foreign key (EDITOR_SOURCE_VALUE_ID_) 
+    references ACT_GE_BYTEARRAY (ID_);
+
+alter table ACT_RE_MODEL 
+    add constraint ACT_FK_MODEL_SOURCE_EXTRA 
+    foreign key (EDITOR_SOURCE_EXTRA_VALUE_ID_) 
+    references ACT_GE_BYTEARRAY (ID_);
+
+delete from ACT_GE_PROPERTY where NAME_ = 'historyLevel';
+
+update ACT_GE_PROPERTY set VALUE_ = '5.11' where NAME_ = 'schema.version';
--- modules/activiti-engine/src/main/resources/org/activiti/db/upgrade/activiti.db2.upgradestep.510.to.511.history.sql
+++ modules/activiti-engine/src/main/resources/org/activiti/db/upgrade/activiti.db2.upgradestep.510.to.511.history.sql
@@ -37,3 +37,5 @@ alter table ACT_HI_DETAIL
 	alter column EXECUTION_ID_ DROP NOT NULL;
 
 Call Sysproc.admin_cmd ('REORG TABLE ACT_HI_DETAIL');
+
+create index ACT_IDX_HI_ACT_INST_PROCINST on ACT_HI_ACTINST(PROC_INST_ID_, ACT_ID_);
