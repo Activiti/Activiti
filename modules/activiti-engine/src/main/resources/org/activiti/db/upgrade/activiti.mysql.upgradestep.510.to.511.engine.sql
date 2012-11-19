959246834a514d75377199275def16e28b79ec5e
Tom Baeyens <mail@tombaeyens.be>
2012-11-19 16:59:46 +0100

    ACT-1333 fixing upgrade script: adding missing constraint in upgrade

--- modules/activiti-engine/src/main/resources/org/activiti/db/upgrade/activiti.mysql.upgradestep.510.to.511.engine.sql
+++ modules/activiti-engine/src/main/resources/org/activiti/db/upgrade/activiti.mysql.upgradestep.510.to.511.engine.sql
@@ -13,3 +13,8 @@ alter table ACT_RE_DEPLOYMENT
     
 alter table ACT_RE_PROCDEF
     add DESCRIPTION_ varchar(4000);
+
+alter table ACT_RU_EXECUTION
+    add constraint ACT_FK_EXE_PROCDEF 
+    foreign key (PROC_DEF_ID_) 
+    references ACT_RE_PROCDEF (ID_);
