b42e21b0569726d84c0137f67bebed52c7bff5dc
Tom Baeyens <mail@tombaeyens.be>
2012-11-20 12:50:34 +0100

    ACT-1333 fixing upgrade script

--- modules/activiti-upgrade/src/test/resources/org/activiti/db/clean/clean.mysql.sql
+++ modules/activiti-upgrade/src/test/resources/org/activiti/db/clean/clean.mysql.sql
@@ -1,3 +1,2 @@
-drop schema activiti;
-create database activiti character set utf8 collate utf8_bin;
-grant all on activiti.* to 'activiti'@'localhost' identified by 'activiti' with grant option;
+drop schema activitiupgrade;
+create database activitiupgrade character set utf8 collate utf8_bin;
