73ab6d9fb0183237913326950a6a904b104d5e2e
Joram Barrez <joram.barrez@gmail.com>
2012-11-21 14:50:40 +0100

    Updated and moven Explorer startup script. Also added rebel.xml generation to any of the web app dependencies such that we now have sweet, lovely and shiny reloading of everything

---
+++ scripts/start-explorer.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+export MAVEN_OPTS="-Xms521M -Xmx1024M -noverify -javaagent:/Applications/ZeroTurnaround/JRebel/jrebel.jar -Xdebug -Djava.compiler=NONE -Xrunjdwp:transport=dt_socket,address=8000,server=y,suspend=n"
+cd ..
+mvn -T 1C -PbuildWebappDependencies clean install
+STATUS=$?
+if [ $STATUS -eq 0 ] 
+then
+    cd modules/activiti-webapp-explorer2
+    mvn clean package jetty:run
+else
+    echo "Build failure in dependent project. Cannot boot Activiti Explorer."
+fi    
