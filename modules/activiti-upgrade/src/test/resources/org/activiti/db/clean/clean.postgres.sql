decb05282b49fe8444e909f615d28c224dcb26bf
Tom Baeyens <mail@tombaeyens.be>
2012-11-22 16:20:37 +0100

    ACT-1333 fixing upgrade script

--- modules/activiti-upgrade/src/test/resources/org/activiti/db/clean/clean.postgres.sql
+++ modules/activiti-upgrade/src/test/resources/org/activiti/db/clean/clean.postgres.sql
@@ -1,25 +1,22 @@
-drop table act_ge_property cascade;
-
-drop table ACT_GE_BYTEARRAY cascade;
-drop table ACT_RE_MODEL cascade;
-drop table ACT_RE_DEPLOYMENT cascade;
-drop table ACT_RE_PROCDEF cascade;
-drop table ACT_RU_IDENTITYLINK cascade;
-drop table ACT_RU_VARIABLE cascade;
-drop table ACT_RU_TASK cascade;
-drop table ACT_RU_EXECUTION cascade;
-drop table ACT_RU_JOB cascade;
-drop table ACT_RU_EVENT_SUBSCR cascade;
-
-drop table ACT_HI_PROCINST cascade;
-drop table ACT_HI_ACTINST cascade;
-drop table ACT_HI_VARINST cascade;
-drop table ACT_HI_TASKINST cascade;
-drop table ACT_HI_DETAIL cascade;
-drop table ACT_HI_COMMENT cascade;
-drop table ACT_HI_ATTACHMENT cascade;
-
-drop table ACT_ID_INFO cascade;
-drop table ACT_ID_MEMBERSHIP cascade;
-drop table ACT_ID_GROUP cascade;
-drop table ACT_ID_USER cascade;
+drop table if exists act_ge_property cascade;
+drop table if exists ACT_GE_BYTEARRAY cascade;
+drop table if exists ACT_RE_MODEL cascade;
+drop table if exists ACT_RE_DEPLOYMENT cascade;
+drop table if exists ACT_RE_PROCDEF cascade;
+drop table if exists ACT_RU_IDENTITYLINK cascade;
+drop table if exists ACT_RU_VARIABLE cascade;
+drop table if exists ACT_RU_TASK cascade;
+drop table if exists ACT_RU_EXECUTION cascade;
+drop table if exists ACT_RU_JOB cascade;
+drop table if exists ACT_RU_EVENT_SUBSCR cascade;
+drop table if exists ACT_HI_PROCINST cascade;
+drop table if exists ACT_HI_ACTINST cascade;
+drop table if exists ACT_HI_VARINST cascade;
+drop table if exists ACT_HI_TASKINST cascade;
+drop table if exists ACT_HI_DETAIL cascade;
+drop table if exists ACT_HI_COMMENT cascade;
+drop table if exists ACT_HI_ATTACHMENT cascade;
+drop table if exists ACT_ID_INFO cascade;
+drop table if exists ACT_ID_MEMBERSHIP cascade;
+drop table if exists ACT_ID_GROUP cascade;
+drop table if exists ACT_ID_USER cascade;
