25b284a42fb6654a3db3089864e23e3fd2c45848
tijsrademakers <tijs.rademakers@alfresco.com>
2012-11-29 13:49:20 +0100

    Upgraded version to 5.11

--- scripts/start-explorer.sh
+++ scripts/start-explorer.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
-export MAVEN_OPTS="-Xms521M -Xmx1024M -noverify -javaagent:/Applications/ZeroTurnaround/JRebel/jrebel.jar -Xdebug -Djava.compiler=NONE -Xrunjdwp:transport=dt_socket,address=8000,server=y,suspend=n"
+export MAVEN_OPTS="-Xms521M -Xmx1024M -noverify -javaagent:/Users/trademakers/Downloads/jrebel/jrebel.jar -Xdebug -Djava.compiler=NONE -Xrunjdwp:transport=dt_socket,address=8000,server=y,suspend=n"
 cd ..
-mvn -T 1C -PbuildWebappDependencies clean install
+mvn3 -T 1C -PbuildWebappDependencies clean install
 STATUS=$?
 if [ $STATUS -eq 0 ] 
 then
     cd modules/activiti-webapp-explorer2
-    mvn clean package jetty:run
+    mvn3 clean package jetty:run
 else
     echo "Build failure in dependent project. Cannot boot Activiti Explorer."
 fi    
