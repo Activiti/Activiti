37cbe28bfe70f514c858582a7a28e584501a7ae6
tijsrademakers <tijs.rademakers@alfresco.com>
2013-01-24 20:00:10 +0100

    Fix for Oracle NVARCHAR2 fields bigger than 2000

--- modules/activiti-engine/src/main/java/org/activiti/engine/impl/db/ActivitiOracleTypeConverter.java
+++ modules/activiti-engine/src/main/java/org/activiti/engine/impl/db/ActivitiOracleTypeConverter.java
@@ -13,80 +13,79 @@ import liquibase.database.structure.type.IntType;
 import liquibase.database.structure.type.SmallIntType;
 import liquibase.database.typeconversion.core.OracleTypeConverter;
 
-
 public class ActivitiOracleTypeConverter extends OracleTypeConverter {
 
-    @Override
-    public int getPriority() {
-        return super.getPriority() + 1;
+  @Override
+  public int getPriority() {
+    return super.getPriority() + 1;
+  }
+
+  @Override
+  public boolean supports(final Database database) {
+    return database instanceof OracleDatabase;
+  }
+
+  @Override
+  protected DataType getDataType(final String columnTypeString,
+      final Boolean autoIncrement, final String dataTypeName,
+      final String precision, final String additionalInformation) {
+
+    DataType dataType = super.getDataType(columnTypeString, autoIncrement,
+        dataTypeName, precision, additionalInformation);
+    if (dataType instanceof CustomType
+        && columnTypeString.toUpperCase().equals("REAL")) {
+      dataType = getFloatType();
     }
+    return dataType;
+  }
 
-    @Override
-    public boolean supports(final Database database) {
-        return database instanceof OracleDatabase;
+  // See http://liquibase.org/forum/index.php?topic=715.0
+  @Override
+  public String convertToDatabaseTypeString(final Column referenceColumn, final Database database) {
+    String translatedTypeName = referenceColumn.getTypeName();
+    if ("NVARCHAR2".equals(translatedTypeName)) {
+      int columnSize = referenceColumn.getColumnSize();
+      if (columnSize > 2000) {
+        columnSize = 2000;
+      }
+      translatedTypeName = translatedTypeName + "(" + columnSize + ")";
+    } else if ("BINARY_FLOAT".equals(translatedTypeName)
+        || "BINARY_DOUBLE".equals(translatedTypeName)) {
+      // nothing to do
+    } else {
+      translatedTypeName = super.convertToDatabaseTypeString(referenceColumn,
+          database);
     }
+    return translatedTypeName;
+  }
 
-    @Override
-    protected DataType getDataType(final String columnTypeString,
-            final Boolean autoIncrement, final String dataTypeName,
-            final String precision, final String additionalInformation) {
-      
-        DataType dataType = super.getDataType(columnTypeString, autoIncrement,
-                dataTypeName, precision, additionalInformation);
-        if (dataType instanceof CustomType
-                && columnTypeString.toUpperCase().equals("REAL")) {
-            dataType = getFloatType();
-        }
-        return dataType;
-    }
+  @Override
+  public SmallIntType getSmallIntType() {
+    return new SmallIntType("NUMBER(5)");
+  }
 
-    // See http://liquibase.org/forum/index.php?topic=715.0
-    @Override
-    public String convertToDatabaseTypeString(final Column referenceColumn,
-            final Database database) {
-        String translatedTypeName = referenceColumn.getTypeName();
-        if ("NVARCHAR2".equals(translatedTypeName)) {
-            translatedTypeName = translatedTypeName
-                + "(" + referenceColumn.getColumnSize() + ")";
-        } else if ("BINARY_FLOAT".equals(translatedTypeName)
-                || "BINARY_DOUBLE".equals(translatedTypeName)) {
-            // nothing to do
-        } else {
-            translatedTypeName = super.convertToDatabaseTypeString(
-                    referenceColumn, database);
-        }
-        return translatedTypeName;
-    }
+  @Override
+  public DateTimeType getDateTimeType() {
+    return new DateTimeType("TIMESTAMP(9)");
+  }
 
-    @Override
-    public SmallIntType getSmallIntType() {
-        return new SmallIntType("NUMBER(5)");
-    }
+  @Override
+  public IntType getIntType() {
+    return new IntType("NUMBER(10)");
+  }
 
-    @Override
-    public DateTimeType getDateTimeType() {
-        return new DateTimeType("TIMESTAMP(9)");
-    }
+  @Override
+  public BigIntType getBigIntType() {
+    return new BigIntType("NUMBER(19)");
+  }
 
-    @Override
-    public IntType getIntType() {
-        return new IntType("NUMBER(10)");
-    }
+  @Override
+  public DoubleType getDoubleType() {
+    return new DoubleType("BINARY_DOUBLE");
+  }
 
-    @Override
-    public BigIntType getBigIntType() {
-        return new BigIntType("NUMBER(19)");
-    }
-
-    @Override
-    public DoubleType getDoubleType() {
-        return new DoubleType("BINARY_DOUBLE");
-    }
-
-    @Override
-    public FloatType getFloatType() {
-        return new FloatType("BINARY_FLOAT");
-    }
-
-
+  @Override
+  public FloatType getFloatType() {
+    return new FloatType("BINARY_FLOAT");
+  }
 }
