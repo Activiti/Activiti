daed9b70174b040aa8f17294be7bf6736552b132
tijsrademakers <tijs.rademakers@alfresco.com>
2013-02-08 14:45:38 +0100

    Added if exists to mssql drop scripts

--- modules/activiti-engine/src/main/resources/org/activiti/db/drop/activiti.mssql.drop.identity.sql
+++ modules/activiti-engine/src/main/resources/org/activiti/db/drop/activiti.mssql.drop.identity.sql
@@ -1,8 +1,5 @@
-alter table ACT_ID_MEMBERSHIP 
-    drop constraint ACT_FK_MEMB_GROUP;
-    
-alter table ACT_ID_MEMBERSHIP 
-    drop constraint ACT_FK_MEMB_USER;
+if exists (select TABLE_NAME from INFORMATION_SCHEMA.TABLES where TABLE_NAME = 'ACT_ID_MEMBERSHIP') alter table ACT_ID_MEMBERSHIP drop constraint ACT_FK_MEMB_GROUP;
+if exists (select TABLE_NAME from INFORMATION_SCHEMA.TABLES where TABLE_NAME = 'ACT_ID_MEMBERSHIP') alter table ACT_ID_MEMBERSHIP drop constraint ACT_FK_MEMB_USER;
     
 if exists (select TABLE_NAME from INFORMATION_SCHEMA.TABLES where TABLE_NAME = 'ACT_ID_INFO') drop table ACT_ID_INFO;
 if exists (select TABLE_NAME from INFORMATION_SCHEMA.TABLES where TABLE_NAME = 'ACT_ID_MEMBERSHIP') drop table ACT_ID_MEMBERSHIP;
