ee7b213c4cbeb8d38fac857652409b902fd9c956
Jamison Dulude <jamie.dulude@gmail.com>
2013-02-16 11:29:07 -0500

    Added support for filtering tasks by process instance as well as assignee, owner, involved, candidate, candidate-group, etc… Added support for filtering tasks by process instance / task list by candidate-groups (a list of groups delimited by a pipe) Added support for filtering tasks by process instance / task list by candidate-group-user-id (does an internal search for all possible groups the user id is associated with)

--- modules/activiti-rest/src/main/java/org/activiti/rest/api/process/ProcessInstanceTaskResource.java
+++ modules/activiti-rest/src/main/java/org/activiti/rest/api/process/ProcessInstanceTaskResource.java
@@ -13,9 +13,13 @@
 
 package org.activiti.rest.api.process;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
+import org.activiti.engine.ActivitiException;
 import org.activiti.engine.ActivitiIllegalArgumentException;
 import org.activiti.engine.impl.TaskQueryProperty;
 import org.activiti.engine.query.QueryProperty;
@@ -23,6 +27,8 @@ import org.activiti.engine.task.TaskQuery;
 import org.activiti.rest.api.ActivitiUtil;
 import org.activiti.rest.api.DataResponse;
 import org.activiti.rest.api.SecuredResource;
+import org.activiti.rest.api.identity.GroupInfo;
+import org.activiti.rest.api.identity.UserGroupsPaginateList;
 import org.activiti.rest.api.task.TasksPaginateList;
 import org.restlet.resource.Get;
 
@@ -50,9 +56,49 @@ public class ProcessInstanceTaskResource extends SecuredResource {
 		if (processInstanceId == null) {
 			throw new ActivitiIllegalArgumentException("No process instance id provided");
 		}
-
+		
+		String personalTaskUserId = getQuery().getValues("assignee");
+	    String ownerTaskUserId = getQuery().getValues("owner");
+	    String involvedTaskUserId = getQuery().getValues("involved");
+	    String candidateTaskUserId = getQuery().getValues("candidate");
+	    String candidateGroupId = getQuery().getValues("candidate-group");
+	    String candidateGroupIds = getQuery().getValues("candidate-groups");
+	    String candidateGroupUserId = getQuery().getValues("candidate-group-user-id");
+	    
 		TaskQuery taskQuery = ActivitiUtil.getTaskService().createTaskQuery().processInstanceId(processInstanceId);
-
+		
+		if (personalTaskUserId != null) {
+	    	taskQuery.taskAssignee(personalTaskUserId);
+	    } else if (ownerTaskUserId != null) {
+	    	taskQuery.taskOwner(ownerTaskUserId);
+	    } else if (involvedTaskUserId != null) {
+	    	taskQuery.taskInvolvedUser(involvedTaskUserId);
+	    } else if (candidateTaskUserId != null) {
+	    	taskQuery.taskCandidateUser(candidateTaskUserId);
+	    } else if (candidateGroupId != null) {
+	    	taskQuery.taskCandidateGroup(candidateGroupId);
+	    } else if (candidateGroupIds != null){
+	    	taskQuery.taskCandidateGroupIn(Arrays.asList(candidateGroupIds.split("\\|")));
+	    } else if (candidateGroupUserId != null){
+	    	DataResponse userGroupsDataResponse = new UserGroupsPaginateList().paginateList(
+	    	        getQuery(), ActivitiUtil.getIdentityService().createGroupQuery()
+	    	            .groupMember(candidateGroupUserId), "id", properties);
+	    	ArrayList<GroupInfo> userGroupsInfo = (userGroupsDataResponse.getData() instanceof ArrayList<?>) ? (ArrayList<GroupInfo>)userGroupsDataResponse.getData() : null;
+	    	
+	    	if(userGroupsInfo != null && userGroupsInfo.size() > 0){
+		    	List<String> candidateGroups = new ArrayList<String>();
+		    	for(GroupInfo groupInfo : userGroupsInfo){
+		    		candidateGroups.add(groupInfo.getId());
+		    	}
+		    	
+		    	taskQuery.taskCandidateGroupIn(candidateGroups);
+	    	}
+	    	else
+	    	{
+	    		throw new ActivitiException("User Id " + candidateGroupUserId + " does not exist, or there are no groups associated with it.");
+	    	}
+	    }
+		
 		// Return also processDefinitionName for each task
 		DataResponse dataResponse = new TasksPaginateList().paginateList(getQuery(), taskQuery, "id", properties);
 		return dataResponse;
