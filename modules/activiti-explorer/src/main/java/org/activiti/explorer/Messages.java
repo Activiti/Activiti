5201f0f057f47c5d18e5fc1e1921ec918ac7c194
Joram Barrez <joram.barrez@gmail.com>
2013-03-01 18:35:03 +0100

    Merge branch 'master' of github.com:Activiti/Activiti
    Conflicts:
    	modules/activiti-explorer/src/main/java/org/activiti/explorer/Messages.java

--- modules/activiti-explorer/src/main/java/org/activiti/explorer/Messages.java
+++ modules/activiti-explorer/src/main/java/org/activiti/explorer/Messages.java
@@ -289,7 +289,6 @@ public interface Messages {
   String REPORTING_SAVE_POPUP_NAME_EMPTY = "reporting.save.popup.name.empty";
   String REPORTING_SAVE_POPUP_NAME_EXISTS = "reporting.save.popup.name.exists";
   String REPORTING_SAVE_POPUP_NAME_TOO_LONG = "reporting.save.popup.name.too.long";
-  String REPORTING_SAVE_POPUP_NAME_TOO_LONG = "reporting.save.popup..name.too.long";
   String REPORTING_CREATE_TIME = "reporting.report.created";
   
   // Management menu
