dadcff691179882b94298a316a9ed84600f041ab
Frederik Heremans <frederik.heremans@gmail.com>
2013-04-17 11:02:29 +0200

    Added task-update and task-delete

--- modules/activiti-rest/src/main/java/org/activiti/rest/api/task/TaskResource.java
+++ modules/activiti-rest/src/main/java/org/activiti/rest/api/task/TaskResource.java
@@ -15,11 +15,16 @@ package org.activiti.rest.api.task;
 
 import org.activiti.engine.ActivitiIllegalArgumentException;
 import org.activiti.engine.ActivitiObjectNotFoundException;
+import org.activiti.engine.task.DelegationState;
 import org.activiti.engine.task.Task;
 import org.activiti.rest.api.ActivitiUtil;
 import org.activiti.rest.api.SecuredResource;
 import org.activiti.rest.application.ActivitiRestServicesApplication;
+import org.restlet.data.Form;
+import org.restlet.data.Status;
+import org.restlet.resource.Delete;
 import org.restlet.resource.Get;
+import org.restlet.resource.Put;
 
 /**
  * @author Frederik Heremans
@@ -28,6 +33,97 @@ public class TaskResource extends SecuredResource {
 
   @Get
   public TaskResponse getTask() {
+    return getApplication(ActivitiRestServicesApplication.class).getRestResponseFactory()
+            .createTaskReponse(this, getTaskFromRequest());
+  }
+  
+//  TODO: move to collection-resource
+//  @Post
+//  public TaskResponse createTask(TaskRequest taskRequest) {
+//    Task task = ActivitiUtil.getTaskService().newTask();
+//
+//    // Populate the task
+//    task.setName(taskRequest.getName());
+//    task.setAssignee(taskRequest.getAssignee());
+//    task.setDescription(taskRequest.getDescription());
+//    task.setDueDate(taskRequest.getDueDate());
+//    task.setOwner(taskRequest.getOwner());
+//    task.setParentTaskId(taskRequest.getParentTaskId());
+//    task.setPriority(taskRequest.getPriority());
+//
+//    DelegationState delegationState = getDelegationState(taskRequest.getDelegationState());
+//    task.setDelegationState(delegationState);
+//    
+//    return getApplication(ActivitiRestServicesApplication.class).getRestResponseFactory()
+//            .createTaskReponse(this, task);
+//  }
+  
+  
+  @Put
+  public TaskResponse updateTask(TaskRequest taskRequest) {
+    Task task = getTaskFromRequest();
+
+    // Populate the task properties based on the request
+    if(taskRequest.isNameSet()) {
+      task.setName(taskRequest.getName());
+    }
+    if(taskRequest.isAssigneeSet()) {
+      task.setAssignee(taskRequest.getAssignee());
+    }
+    if(taskRequest.isDescriptionSet()) {
+      task.setDescription(taskRequest.getDescription());
+    }
+    if(taskRequest.isDuedateSet()) {
+      task.setDueDate(taskRequest.getDueDate());
+    }
+    if(taskRequest.isOwnerSet()) {
+      task.setOwner(taskRequest.getOwner());
+    }
+    if(taskRequest.isParentTaskIdSet()) {
+      task.setParentTaskId(taskRequest.getParentTaskId());
+    }
+    if(taskRequest.isPrioritySet()) {
+      task.setPriority(taskRequest.getPriority());
+    }
+
+    if(taskRequest.isDelegationStateSet()) {
+      DelegationState delegationState = getDelegationState(taskRequest.getDelegationState());
+      task.setDelegationState(delegationState);
+    }
+
+    // Save the task and fetch agian, it's possible that an assignment-listener has updated
+    // fields after it was saved so we can't use the in-memory task
+    ActivitiUtil.getTaskService().saveTask(task);
+    task = ActivitiUtil.getTaskService().createTaskQuery().taskId(task.getId()).singleResult();
+    
+    return getApplication(ActivitiRestServicesApplication.class).getRestResponseFactory()
+            .createTaskReponse(this, task);
+  }
+  
+  @Delete
+  public void deleteTask() {
+    Form query = getQuery();
+    Boolean cascadeHistory = getQueryParameterAsBoolean("cascadeHistory", query);
+    String deleteReason = getQueryParameter("deleteReason", query);
+    
+    Task taskToDelete = getTaskFromRequest();
+    
+    if(cascadeHistory != null) {
+      // Ignore delete-reason since the task-history (where the reason is recorded) will be deleted anyway 
+      ActivitiUtil.getTaskService().deleteTask(taskToDelete.getId(), cascadeHistory);
+    } else {
+      // Delete with delete-reason
+      ActivitiUtil.getTaskService().deleteTask(taskToDelete.getId(), deleteReason);
+    }
+    
+    getResponse().setStatus(Status.SUCCESS_NO_CONTENT);
+  }
+  
+
+  /**
+   * Get valid task from request. Throws exception if task doen't exist or if task id is not provided.
+   */
+  protected Task getTaskFromRequest() {
     String taskId = getAttribute("taskId");
 
     if (taskId == null) {
@@ -38,7 +134,21 @@ public class TaskResource extends SecuredResource {
     if (task == null) {
       throw new ActivitiObjectNotFoundException("Could not find a task with id '" + taskId + "'.", Task.class);
     }
-    return getApplication(ActivitiRestServicesApplication.class).getRestResponseFactory().createTaskReponse(this, task);
+    return task;
+  }
+
+  protected DelegationState getDelegationState(String delegationState) {
+    DelegationState state = null;
+    if(delegationState != null) {
+      if(DelegationState.RESOLVED.name().toLowerCase().equals(delegationState)) {
+        return DelegationState.RESOLVED;
+      } else if(DelegationState.PENDING.name().toLowerCase().equals(delegationState)) {
+        return DelegationState.PENDING;
+      } else {
+        throw new ActivitiIllegalArgumentException("Illegal value for delegationState: " + delegationState);
+      }
+    }
+    return state;
   }
 
 }
