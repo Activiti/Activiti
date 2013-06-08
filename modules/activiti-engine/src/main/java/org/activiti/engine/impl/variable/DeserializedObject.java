ba773b69680df62a5f3461d508d928ba6b8cf7e9
Marcus Klimstra <mgb.klimstra@gmail.com>
2013-06-08 22:57:18 +0200

    DeserializedObject: use the new VariableInstanceEntity.setBytes

--- modules/activiti-engine/src/main/java/org/activiti/engine/impl/variable/DeserializedObject.java
+++ modules/activiti-engine/src/main/java/org/activiti/engine/impl/variable/DeserializedObject.java
@@ -37,9 +37,7 @@ public class DeserializedObject {
     if (deserializedObject==variableInstanceEntity.getCachedValue()) {
       byte[] bytes = SerializableType.serialize(deserializedObject, variableInstanceEntity);
       if (!Arrays.equals(originalBytes, bytes)) {
-        variableInstanceEntity
-          .getByteArrayValue()
-          .setBytes(bytes);
+        variableInstanceEntity.setBytes(bytes);
       }
     }
   }
