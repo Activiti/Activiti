9ee02213539e4551f52fe4ca7eeb897dab9b68f7
fheremans <frederik.heremans@gmail.com>
2013-10-02 15:08:45 +0200

    ACT-1777: non-null timestamps are best initialized with default on mysql

--- modules/activiti-engine/src/main/resources/org/activiti/db/create/activiti.mysql.create.engine.sql
+++ modules/activiti-engine/src/main/resources/org/activiti/db/create/activiti.mysql.create.engine.sql
@@ -160,7 +160,7 @@ create table ACT_RU_EVENT_SUBSCR (
     PROC_INST_ID_ varchar(64),
     ACTIVITY_ID_ varchar(64),
     CONFIGURATION_ varchar(255),
-    CREATED_ timestamp not null,
+    CREATED_ timestamp not null DEFAULT CURRENT_TIMESTAMP,
     primary key (ID_)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE utf8_bin;
 
