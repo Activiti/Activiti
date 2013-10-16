970fed72df2c19c5794679fa501157ffe3cd129e
martin.grofcik <martin.grofcik@edorasware.com>
2013-10-16 14:15:18 +0200

    ACT-1479 fix
    ACT-1749 Impossible to remove identity-links from create-listener
    http://jira.codehaus.org/browse/ACT-1749

--- modules/activiti-engine/src/test/java/org/activiti/engine/test/api/task/TaskIdentityLinksTest.java
+++ modules/activiti-engine/src/test/java/org/activiti/engine/test/api/task/TaskIdentityLinksTest.java
@@ -213,5 +213,32 @@ public class TaskIdentityLinksTest extends PluggableActivitiTestCase {
 
     assertEquals("user", identityLink.getUserId());
   }
-  
+
+  @Deployment(resources = "org/activiti/engine/test/api/task/IdentityLinksProcess.bpmn20.xml")
+  public void testEmptyCandidateUserLink() {
+    runtimeService.startProcessInstanceByKey("IdentityLinksProcess");
+
+    String taskId = taskService
+        .createTaskQuery()
+        .singleResult()
+        .getId();
+
+    taskService.addCandidateGroup(taskId, "muppets");
+    taskService.deleteCandidateUser(taskId, "kermit");
+
+    List<IdentityLink> identityLinks = taskService.getIdentityLinksForTask(taskId);
+    assertNotNull(identityLinks);
+    assertEquals( 1, identityLinks.size());
+
+    IdentityLink identityLink = identityLinks.get(0);
+    assertEquals("muppets", identityLink.getGroupId());
+    assertEquals(null, identityLink.getUserId());
+    assertEquals(IdentityLinkType.CANDIDATE, identityLink.getType());
+    assertEquals(taskId, identityLink.getTaskId());
+
+    taskService.deleteCandidateGroup(taskId, "muppets");
+
+    assertEquals(0, taskService.getIdentityLinksForTask(taskId).size());
+  }
+
 }
