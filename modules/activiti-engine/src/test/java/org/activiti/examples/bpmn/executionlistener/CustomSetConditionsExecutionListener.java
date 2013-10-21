d4c154e89cdb1366ede1762517adde0740ebf9ef
tijsrademakers <tijs.rademakers@alfresco.com>
2013-10-21 11:45:33 +0200

    Moved version to 5.14

---
+++ modules/activiti-engine/src/test/java/org/activiti/examples/bpmn/executionlistener/CustomSetConditionsExecutionListener.java
@@ -0,0 +1,41 @@
+package org.activiti.examples.bpmn.executionlistener;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.activiti.engine.delegate.DelegateExecution;
+import org.activiti.engine.delegate.ExecutionListener;
+
+public class CustomSetConditionsExecutionListener implements ExecutionListener {
+
+  private static final long serialVersionUID = 1L;
+  
+  protected List<String> conditions = new ArrayList<String>();
+  protected String flowId;
+
+  @Override
+  public void notify(DelegateExecution execution) throws Exception {
+    execution.setVariable(flowId + "_activiti_conditions", conditions);
+  }
+
+  public List<String> getConditions() {
+    return conditions;
+  }
+
+  public void setConditions(List<String> conditions) {
+    this.conditions = conditions;
+  }
+  
+  public void addCondition(String condition) {
+    this.conditions.add(condition);
+  }
+
+  public String getFlowId() {
+    return flowId;
+  }
+
+  public void setFlowId(String flowId) {
+    this.flowId = flowId;
+  }
+  
+}
