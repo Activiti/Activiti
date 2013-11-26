468436dd22c91fdd266a739abe37079ff9ff4d3d
Joram Barrez <joram.barrez@gmail.com>
2013-11-26 14:27:44 +0000

    ACT-1860: fix for postgres (wrong index name)

--- modules/activiti-engine/src/main/resources/org/activiti/db/upgrade/activiti.postgres.upgradestep.514.to.515.history.sql
+++ modules/activiti-engine/src/main/resources/org/activiti/db/upgrade/activiti.postgres.upgradestep.514.to.515.history.sql
@@ -1,5 +1,5 @@
 alter table ACT_HI_TASKINST
     add CATEGORY_ varchar(255);
     
-alter table ACT_HI_PROCINST drop constraint act_ru_execution_proc_def_id__business_key__key;    
+alter table ACT_HI_PROCINST drop constraint act_hi_procinst_proc_def_id__business_key__key;    
     
