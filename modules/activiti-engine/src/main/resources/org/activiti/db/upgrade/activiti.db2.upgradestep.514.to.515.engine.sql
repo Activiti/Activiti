4e2e6ee0bf9b30b78650c3967eb5556e663932d5
Joram Barrez <joram.barrez@gmail.com>
2013-11-26 17:42:19 +0000

    ACT-1860: fix for DB2

--- modules/activiti-engine/src/main/resources/org/activiti/db/upgrade/activiti.db2.upgradestep.514.to.515.engine.sql
+++ modules/activiti-engine/src/main/resources/org/activiti/db/upgrade/activiti.db2.upgradestep.514.to.515.engine.sql
@@ -1,8 +1,12 @@
 alter table ACT_RU_TASK 
     add CATEGORY_ varchar(255);
+
+Call Sysproc.admin_cmd ('REORG TABLE ACT_RU_TASK');
         
 drop index ACT_UNIQ_RU_BUS_KEY;
 alter table ACT_RU_EXECUTION drop colum UNI_BUSINESS_KEY;
 alter table ACT_RU_EXECUTION drop colum UNI_PROC_DEF_ID;
+
+Call Sysproc.admin_cmd ('REORG TABLE ACT_RU_EXECUTION');
 
 update ACT_GE_PROPERTY set VALUE_ = '5.15-SNAPSHOT' where NAME_ = 'schema.version';
--- modules/activiti-engine/src/main/resources/org/activiti/db/upgrade/activiti.db2.upgradestep.514.to.515.history.sql
+++ modules/activiti-engine/src/main/resources/org/activiti/db/upgrade/activiti.db2.upgradestep.514.to.515.history.sql
@@ -1,6 +1,13 @@
 alter table ACT_HI_TASKINST
     add CATEGORY_ varchar(255);
     
+Call Sysproc.admin_cmd ('REORG TABLE ACT_HI_TASKINST');    
+    
 drop index ACT_UNIQ_HI_BUS_KEY;
-alter table ACT_HI_PROCINST drop colum UNI_BUSINESS_KEY;
-alter table ACT_HI_PROCINST drop colum UNI_PROC_DEF_ID;    
+
+-- DB2 *cannot* drop columns. Yes, this is 2013.
+-- This means that for DB2 the columns will remain as they are (they won't be used)
+-- alter table ACT_HI_PROCINST drop colum UNI_BUSINESS_KEY;
+-- alter table ACT_HI_PROCINST drop colum UNI_PROC_DEF_ID;
+
+Call Sysproc.admin_cmd ('REORG TABLE ACT_HI_PROCINST');    
