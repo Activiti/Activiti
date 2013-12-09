6c8a11f3e8fe800e0a591a6afe9781ca01260ce9
tijsrademakers <tijs.rademakers@alfresco.com>
2013-12-09 17:13:12 +0100

    Fix for ACT-1854

--- modules/activiti-bpmn-model/src/main/java/org/activiti/bpmn/model/BpmnModel.java
+++ modules/activiti-bpmn-model/src/main/java/org/activiti/bpmn/model/BpmnModel.java
@@ -41,6 +41,7 @@ public class BpmnModel {
 	protected List<Pool> pools = new ArrayList<Pool>();
 	protected List<Import> imports = new ArrayList<Import>();
 	protected List<Interface> interfaces = new ArrayList<Interface>();
+	protected List<Artifact> globalArtifacts = new ArrayList<Artifact>();
 	protected List<Problem> problems = new ArrayList<Problem>();
 	protected List<Warning> warnings = new ArrayList<Warning>();
 	protected Map<String, String> namespaceMap = new LinkedHashMap<String, String>();
@@ -362,6 +363,14 @@ public class BpmnModel {
 
   public void setInterfaces(List<Interface> interfaces) {
     this.interfaces = interfaces;
+  }
+
+  public List<Artifact> getGlobalArtifacts() {
+    return globalArtifacts;
+  }
+
+  public void setGlobalArtifacts(List<Artifact> globalArtifacts) {
+    this.globalArtifacts = globalArtifacts;
   }
 
   public void addNamespace(String prefix, String uri) {
