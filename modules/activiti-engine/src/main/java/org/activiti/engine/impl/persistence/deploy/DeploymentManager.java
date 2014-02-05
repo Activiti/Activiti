01a77b37e58d136d3834272e2aefe226031b2098
Joram Barrez <joram.barrez@gmail.com>
2014-02-05 16:43:52 +0100

    Small tweak to exception, add quotes to see difference between empty and space string

--- modules/activiti-engine/src/main/java/org/activiti/engine/impl/persistence/deploy/DeploymentManager.java
+++ modules/activiti-engine/src/main/java/org/activiti/engine/impl/persistence/deploy/DeploymentManager.java
@@ -79,7 +79,7 @@ public class DeploymentManager {
       .getProcessDefinitionEntityManager()
       .findLatestProcessDefinitionByKeyAndTenantId(processDefinitionKey, tenantId);
     if (processDefinition==null) {
-      throw new ActivitiObjectNotFoundException("no processes deployed with key '"+processDefinitionKey+"' for tenant identifier " + tenantId, ProcessDefinition.class);
+      throw new ActivitiObjectNotFoundException("no processes deployed with key '"+processDefinitionKey+"' for tenant identifier '" + tenantId + "'", ProcessDefinition.class);
     }
     processDefinition = resolveProcessDefinition(processDefinition);
     return processDefinition;
