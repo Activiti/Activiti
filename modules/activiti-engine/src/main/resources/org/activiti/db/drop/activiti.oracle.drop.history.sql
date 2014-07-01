449641eb31c9e30fe6ae41ff261d9a1cd2cf0f5c
Joram Barrez <joram.barrez@gmail.com>
2014-07-01 11:27:54 +0200

    Database event logger: fix Oracle qa (switch to using sequence instead of trigger) - part2

--- modules/activiti-engine/src/main/resources/org/activiti/db/drop/activiti.oracle.drop.history.sql
+++ modules/activiti-engine/src/main/resources/org/activiti/db/drop/activiti.oracle.drop.history.sql
@@ -23,6 +23,5 @@ drop table ACT_HI_COMMENT;
 drop table ACT_HI_ATTACHMENT;
 drop table ACT_HI_IDENTITYLINK;
 
-drop trigger act_hi_evt_log_nr;
 drop sequence act_hi_evt_log_seq;
 drop table ACT_HI_EVT_LOG;
