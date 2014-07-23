27be7c65fe215f4451e0a14c479f171d0263768b
Henry Yan <yanhonglei@gmail.com>
2014-07-23 17:29:33 +0800

    Add setTenantId to Task + handle tenantId in rest resource.

--- modules/activiti-rest/src/main/java/org/activiti/rest/service/api/runtime/task/TaskBaseResource.java
+++ modules/activiti-rest/src/main/java/org/activiti/rest/service/api/runtime/task/TaskBaseResource.java
@@ -97,6 +97,9 @@ public class TaskBaseResource extends SecuredResource {
     if(taskRequest.isCategorySet()) {
     	task.setCategory(taskRequest.getCategory());
     }
+    if(taskRequest.isTenantIdSet()) {
+      task.setTenantId(taskRequest.getTenantId());
+    }
     if(taskRequest.isFormKeySet()) {
       task.setFormKey(taskRequest.getFormKey());
     }
