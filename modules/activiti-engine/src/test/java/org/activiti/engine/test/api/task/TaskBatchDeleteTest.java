a349dcfaf03269fee259929d9c1b171f5a326ada
fheremans <frederik.heremans@gmail.com>
2014-07-30 16:24:02 +0200

    Added additional test to confirm proper multi-instance task deletion behaviour

--- modules/activiti-engine/src/test/java/org/activiti/engine/test/api/task/TaskBatchDeleteTest.java
+++ modules/activiti-engine/src/test/java/org/activiti/engine/test/api/task/TaskBatchDeleteTest.java
@@ -47,4 +47,29 @@ public class TaskBatchDeleteTest extends PluggableActivitiTestCase {
         
         
     }
+    
+    @Deployment
+    public void testDeleteCancelledMultiInstanceTasks() throws Exception {
+        
+        ProcessInstance processInstance = runtimeService.startProcessInstanceByKey("testBatchDeleteOfTask");
+        assertNotNull(processInstance);
+        assertFalse(processInstance.isEnded());
+        
+        Task lastTask = taskService.createTaskQuery().processInstanceId(processInstance.getId())
+                .taskDefinitionKey("multiInstance").listPage(4, 1).get(0);
+        
+        taskService.addCandidateGroup(lastTask.getId(), "sales");
+        
+        Task firstTask = taskService.createTaskQuery().processInstanceId(processInstance.getId())
+                .taskDefinitionKey("multiInstance").listPage(0, 1).get(0);
+        assertNotNull(firstTask);
+        
+        taskService.complete(firstTask.getId());
+        
+        // Process should have ended fine
+        processInstance = runtimeService.createProcessInstanceQuery()
+                .processInstanceId(processInstance.getId())
+                .singleResult();
+        assertNull(processInstance);
+    }
 }
