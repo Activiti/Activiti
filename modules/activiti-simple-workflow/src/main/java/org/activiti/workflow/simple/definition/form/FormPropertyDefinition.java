6bddf067becce1c34ef21160a1954d9984452478
fheremans <frederik.heremans@gmail.com>
2014-08-19 16:47:32 +0200

    Fixed issue with serialisation of super-type properties only (skipping ListPropertyDefinition 'entries')

--- modules/activiti-simple-workflow/src/main/java/org/activiti/workflow/simple/definition/form/FormPropertyDefinition.java
+++ modules/activiti-simple-workflow/src/main/java/org/activiti/workflow/simple/definition/form/FormPropertyDefinition.java
@@ -15,11 +15,11 @@ package org.activiti.workflow.simple.definition.form;
 import java.util.HashMap;
 import java.util.Map;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonInclude.Include;
 import com.fasterxml.jackson.annotation.JsonTypeInfo;
 import com.fasterxml.jackson.annotation.JsonTypeInfo.As;
 import com.fasterxml.jackson.annotation.JsonTypeInfo.Id;
-import com.fasterxml.jackson.databind.annotation.JsonSerialize;
-import com.fasterxml.jackson.databind.annotation.JsonSerialize.Inclusion;
 
 /**
  * Defines one property in a {@link FormDefinition}.
@@ -92,7 +92,7 @@ public void setParameters(Map<String, Object> parameters) {
 	  this.parameters = parameters;
   }
   
-  @JsonSerialize(include=Inclusion.NON_EMPTY)
+  @JsonInclude(Include.NON_EMPTY)
   public Map<String, Object> getParameters() {
 	  return parameters;
   }
