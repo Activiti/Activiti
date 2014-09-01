672464e7c33fcf8727efb4a221f065480a94e794
Joram Barrez <joram.barrez@gmail.com>
2014-09-01 20:24:38 +0200

    Fix for previous commit on Mssql

--- modules/activiti-engine/src/main/java/org/activiti/engine/impl/AbstractQuery.java
+++ modules/activiti-engine/src/main/java/org/activiti/engine/impl/AbstractQuery.java
@@ -205,7 +205,7 @@ public abstract class AbstractQuery<T extends Query<?,?>, U> extends ListQueryPa
     			orderBy = orderBy + "isnull(" + column +") desc," + defaultOrderByClause;
     		} else if (ProcessEngineConfigurationImpl.DATABASE_TYPE_DB2.equals(databaseType)
     				|| ProcessEngineConfigurationImpl.DATABASE_TYPE_MSSQL.equals(databaseType)) {
-    			orderBy = orderBy + "case when " + column + " is null then 1 else 0 end," + defaultOrderByClause;
+    			orderBy = orderBy + "case when " + column + " is null then 0 else 1 end," + defaultOrderByClause;
     		} else {
     			orderBy = orderBy + defaultOrderByClause;
     		}
@@ -221,7 +221,7 @@ public abstract class AbstractQuery<T extends Query<?,?>, U> extends ListQueryPa
     			orderBy = orderBy + "isnull(" + column +") asc," + defaultOrderByClause;
     		} else if (ProcessEngineConfigurationImpl.DATABASE_TYPE_DB2.equals(databaseType)
     				|| ProcessEngineConfigurationImpl.DATABASE_TYPE_MSSQL.equals(databaseType)) {
-    			orderBy = orderBy + "case when " + column + " is null then 0 else 1 end," + defaultOrderByClause;
+    			orderBy = orderBy + "case when " + column + " is null then 1 else 0 end," + defaultOrderByClause;
     		} else {
     			orderBy = orderBy + defaultOrderByClause;
     		}
