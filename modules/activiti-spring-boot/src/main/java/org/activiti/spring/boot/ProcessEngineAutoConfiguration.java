ee15a9c0b79c59967f318b31cf0189efe5f34d8b
Joram Barrez <joram.barrez@gmail.com>
2014-09-18 01:20:28 +0200

    Move to properties

--- modules/activiti-spring-boot/src/main/java/org/activiti/spring/boot/ProcessEngineAutoConfiguration.java
+++ modules/activiti-spring-boot/src/main/java/org/activiti/spring/boot/ProcessEngineAutoConfiguration.java
@@ -15,10 +15,17 @@
  */
 package org.activiti.spring.boot;
 
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
+
+import javax.sql.DataSource;
+
 import org.activiti.engine.ProcessEngine;
 import org.activiti.spring.SpringProcessEngineConfiguration;
 import org.activiti.spring.annotations.ActivitiConfigurer;
 import org.activiti.spring.annotations.EnableActiviti;
+import org.activiti.spring.boot.ProcessEngineAutoConfiguration.ActivitiProperties;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -28,117 +35,197 @@ import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.bind.RelaxedPropertyResolver;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.EnvironmentAware;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.env.Environment;
 import org.springframework.core.io.Resource;
 import org.springframework.core.io.support.ResourcePatternResolver;
 import org.springframework.util.Assert;
-
-import javax.sql.DataSource;
-import java.io.IOException;
-import java.util.Arrays;
-import java.util.List;
+import org.springframework.util.StringUtils;
 
 /**
  * <p>
- * Auto configuration for using Activiti from a <A href="http://spring.io/projects/spring-boot">Spring Boot application</a>. 
- * Provides a configured {@link org.activiti.engine.ProcessEngine} if none other is detected. 
+ * Auto configuration for using Activiti from a <A
+ * href="http://spring.io/projects/spring-boot">Spring Boot application</a>.
+ * Provides a configured {@link org.activiti.engine.ProcessEngine} if none other
+ * is detected.
  * <p>
- * Discovers any process definitions deployed in the {@literal src/main/resources/process} folder, and 
- * uses the single {@link javax.sql.DataSource} bean discovered in the Spring application context.. 
+ * Discovers any process definitions deployed in the
+ * {@literal src/main/resources/process} folder, and uses the single
+ * {@link javax.sql.DataSource} bean discovered in the Spring application
+ * context..
  * 
  *
  * @author Josh Long
  */
 @Configuration
-@ConditionalOnClass({ProcessEngine.class, EnableActiviti.class})
+@ConditionalOnClass({ ProcessEngine.class, EnableActiviti.class })
 @ConditionalOnBean(DataSource.class)
+@EnableConfigurationProperties(ActivitiProperties.class)
 @AutoConfigureAfter(DataSourceAutoConfiguration.class)
 @ConditionalOnMissingBean(ProcessEngine.class)
 public class ProcessEngineAutoConfiguration {
 
+	@ConfigurationProperties("spring.activiti")
+	public static class ActivitiProperties {
+		private boolean checkProcessDefinitions;
 
-    @Configuration
-    @EnableActiviti
-    public static class DefaultActivitiConfiguration implements ActivitiConfigurer, EnvironmentAware {
+		private String deploymentName;
 
-        private Log log = LogFactory.getLog(getClass());
+		private String databaseSchemaUpdate;
 
-        public static final String PROCESS_DEFINITIONS_DEFAULT_PREFIX =
-                "classpath:/processes/";
+		private String databaseSchema;
+		
+		private String processDefinitionLocationPrefix;
+		
+		private String processDefinitionLocationSuffix;
 
-        public static final String PROCESS_DEFINITIONS_DEFAULT_SUFFIX =
-                "**.bpmn20.xml";
+		public boolean isCheckProcessDefinitions() {
+			return checkProcessDefinitions;
+		}
 
-        private RelaxedPropertyResolver environment;
+		public void setCheckProcessDefinitions(boolean checkProcessDefinitions) {
+			this.checkProcessDefinitions = checkProcessDefinitions;
+		}
 
-        @Autowired
-        private DataSource[] dataSources;
+		public String getDeploymentName() {
+			return deploymentName;
+		}
 
-        @Autowired
-        private ResourcePatternResolver applicationContext;
+		public void setDeploymentName(String deploymentName) {
+			this.deploymentName = deploymentName;
+		}
 
-        @Override
-        public void processDefinitionResources(List<Resource> resourceList) {
-            List<Resource> resources;
-            String prefix, suffix;
-            try {
-                prefix = this.environment.getProperty("prefix", PROCESS_DEFINITIONS_DEFAULT_PREFIX);
-                suffix = this.environment.getProperty("suffix", PROCESS_DEFINITIONS_DEFAULT_SUFFIX);
-                String path = prefix + suffix;
+		public String getDatabaseSchemaUpdate() {
+			return databaseSchemaUpdate;
+		}
 
-                if (this.environment.getProperty("checkProcessDefinitions", Boolean.class, true)) {
-                    Assert.state(this.applicationContext.getResource(prefix).exists(), String.format("No process definitions were found deployed at %s. Are you actually using Activiti? ", path));
-                }
+		public void setDatabaseSchemaUpdate(String databaseSchemaUpdate) {
+			this.databaseSchemaUpdate = databaseSchemaUpdate;
+		}
 
-                // loop through the process definitions discovered.
-                resources = Arrays.asList(this.applicationContext.getResources(path));
+		public String getDatabaseSchema() {
+			return databaseSchema;
+		}
 
-                if (log.isInfoEnabled()) {
-                    log.info(String.format(
-                            "found %s process definitions in %s.", resources.size(), prefix));
-                    for (Resource resource : resources) {
-                        log.info(String.format("found process definition: %s", resource.getURI().toString()));
-                    }
-                }
+		public void setDatabaseSchema(String databaseSchema) {
+			this.databaseSchema = databaseSchema;
+		}
 
-            } catch (IOException e) {
-                throw new RuntimeException(e);
-            }
+		public String getProcessDefinitionLocationPrefix() {
+			return processDefinitionLocationPrefix;
+		}
 
+		public void setProcessDefinitionLocationPrefix(
+		        String processDefinitionLocationPrefix) {
+			this.processDefinitionLocationPrefix = processDefinitionLocationPrefix;
+		}
 
-            resourceList.addAll(resources);
-        }
+		public String getProcessDefinitionLocationSuffix() {
+			return processDefinitionLocationSuffix;
+		}
 
+		public void setProcessDefinitionLocationSuffix(
+		        String processDefinitionLocationSuffix) {
+			this.processDefinitionLocationSuffix = processDefinitionLocationSuffix;
+		}
+		
+	}
 
-        @Override
-        public void postProcessSpringProcessEngineConfiguration(
-                SpringProcessEngineConfiguration config) {
+	@Configuration
+	@EnableActiviti
+	public static class DefaultActivitiConfiguration implements ActivitiConfigurer {
 
-            config.setDeploymentName(
-                    this.environment.getProperty("deploymentName", config.getDeploymentName()));
+		private Log log = LogFactory.getLog(getClass());
 
-            config.setDatabaseSchemaUpdate(
-                    this.environment.getProperty("databaseSchemaUpdate", config.getDatabaseSchemaUpdate()));
+		public static final String PROCESS_DEFINITIONS_DEFAULT_PREFIX = "classpath:/processes/";
 
-            config.setDatabaseSchema(
-                    this.environment.getProperty("databaseSchema", config.getDatabaseSchema()));
+		public static final String PROCESS_DEFINITIONS_DEFAULT_SUFFIX = "**.bpmn20.xml";
 
-        }
+ 
+		@Autowired
+		private DataSource[] dataSources;
 
-        @Override
-        public DataSource dataSource() {
-            Assert.isTrue(this.dataSources.length > 0, "you must have configured at least " +
-                    "one javax.sql.DataSource bean in your Spring application context.");
-            return this.dataSources[0];
-        }
+		@Autowired
+		private ResourcePatternResolver applicationContext;
 
-        @Override
-        public void setEnvironment(Environment environment) {
-            this.environment = new RelaxedPropertyResolver(environment,
-                    "spring.activiti.");
+		@Autowired
+		private ActivitiProperties activitiProperties;
 
-        }
-    }
+		@Override
+		public void processDefinitionResources(List<Resource> resourceList) {
+			List<Resource> resources;
+			String prefix, suffix;
+			try {
+				prefix = defaultText(activitiProperties.getProcessDefinitionLocationPrefix(), PROCESS_DEFINITIONS_DEFAULT_PREFIX);
+				suffix = defaultText(activitiProperties.getProcessDefinitionLocationSuffix(), PROCESS_DEFINITIONS_DEFAULT_SUFFIX);
+				String path = prefix + suffix;
+
+				boolean checkProcDefs = activitiProperties
+				        .isCheckProcessDefinitions();
+				if (checkProcDefs) {
+					Assert.state(
+					        this.applicationContext.getResource(prefix)
+					                .exists(),
+					        String.format(
+					                "No process definitions were found deployed at %s. Are you actually using Activiti? ",
+					                path));
+				}
+
+				// loop through the process definitions discovered.
+				resources = Arrays.asList(this.applicationContext
+				        .getResources(path));
+
+				if (log.isInfoEnabled()) {
+					log.info(String.format(
+					        "found %s process definitions in %s.",
+					        resources.size(), prefix));
+					for (Resource resource : resources) {
+						log.info(String.format("found process definition: %s",
+						        resource.getURI().toString()));
+					}
+				}
+
+			} catch (IOException e) {
+				throw new RuntimeException(e);
+			}
+
+			resourceList.addAll(resources);
+		}
+
+		private final String defaultText(String i, String o) {
+			return (StringUtils.hasText(i)) ? i : o;
+		}
+
+		@Override
+		public void postProcessSpringProcessEngineConfiguration(
+		        SpringProcessEngineConfiguration config) {
+
+			config.setDeploymentName(defaultText(
+			        activitiProperties.getDeploymentName(),
+			        config.getDeploymentName()));
+
+			config.setDeploymentName(defaultText(
+			        activitiProperties.getDatabaseSchema(),
+			        config.getDatabaseSchema()));
+
+			config.setDeploymentName(defaultText(
+			        activitiProperties.getDatabaseSchemaUpdate(),
+			        config.getDatabaseSchemaUpdate()));
+
+		}
+
+		@Override
+		public DataSource dataSource() {
+			Assert.isTrue(
+			        this.dataSources.length > 0,
+			        "you must have configured at least "
+			                + "one javax.sql.DataSource bean in your Spring application context.");
+			return this.dataSources[0];
+		}
+
+		 
+	}
 }
