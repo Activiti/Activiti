b5ab7ad697d21c2722e26b2a65774db5356415b0
martin.grofcik <martin.grofcik@edorasware.com>
2014-10-07 07:19:10 +0200

    Oracle short string support

---
+++ modules/activiti-engine/src/main/resources/org/activiti/db/upgrade/activiti.oracle.upgradestep.515.to.51640.history.sql
@@ -0,0 +1,11 @@
+alter table ACT_HI_DETAIL
+  modify (
+    TEXT_ NVARCHAR2(4000),
+    TEXT2_ NVARCHAR2(4000)
+  );
+
+alter table ACT_HI_VARINST
+  modify (
+    TEXT_ NVARCHAR2(4000),
+    TEXT2_ NVARCHAR2(4000)
+  );
--- modules/activiti-engine/src/main/resources/org/activiti/db/upgrade/activiti.oracle.upgradestep.51630.to.51640.engine.sql
+++ modules/activiti-engine/src/main/resources/org/activiti/db/upgrade/activiti.oracle.upgradestep.51630.to.51640.engine.sql
@@ -1,1 +1,7 @@
+alter table ACT_RU_VARIABLE
+  modify (
+    TEXT_ NVARCHAR2(4000),
+    TEXT2_ NVARCHAR2(4000)
+  );
+
 update ACT_GE_PROPERTY set VALUE_ = '5.16.4.0' where NAME_ = 'schema.version';
