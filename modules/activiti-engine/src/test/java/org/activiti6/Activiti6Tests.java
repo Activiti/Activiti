686c12421def350c52405fcacd7251a11f905eee
Joram Barrez <joram.barrez@gmail.com>
2015-01-02 22:23:52 +0100

    Added simple test for nested parallelism

--- modules/activiti-engine/src/test/java/org/activiti6/Activiti6Tests.java
+++ modules/activiti-engine/src/test/java/org/activiti6/Activiti6Tests.java
@@ -90,5 +90,26 @@ public class Activiti6Tests extends AbstractActvitiTest {
 		
 		assertEquals(0, runtimeService.createProcessInstanceQuery().count());
 	}
+	
+	@Test
+	@org.activiti.engine.test.Deployment
+	public void testSimpleNestedParallelGateway() {
+		ProcessInstance processInstance = runtimeService.startProcessInstanceByKey("simpleParallelGateway");
+		assertNotNull(processInstance);
+		assertFalse(processInstance.isEnded());
+		
+		List<Task> tasks = taskService.createTaskQuery().processDefinitionKey("simpleParallelGateway").orderByTaskName().asc().list();
+		assertEquals(4, tasks.size());
+		assertEquals("Task a", tasks.get(0).getName());
+		assertEquals("Task b1", tasks.get(1).getName());
+		assertEquals("Task b2", tasks.get(2).getName());
+		assertEquals("Task c", tasks.get(3).getName());
+		
+		for (Task task : tasks) {
+			taskService.complete(task.getId());
+		}
+		
+		assertEquals(0, runtimeService.createProcessInstanceQuery().count());
+	}
 
 }
