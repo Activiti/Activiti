d19320825944f2fa25552660ae7b4e9c9785e18b
Tijs Rademakers <tijs.rademakers@gmail.com>
2015-02-04 08:58:25 +0100

    Small changes to pull request 510

--- modules/activiti-osgi/src/main/java/org/activiti/osgi/blueprint/BlueprintELResolver.java
+++ modules/activiti-osgi/src/main/java/org/activiti/osgi/blueprint/BlueprintELResolver.java
@@ -19,27 +19,27 @@ public class BlueprintELResolver extends ELResolver {
 	
   private static final Logger LOGGER = LoggerFactory.getLogger(BlueprintELResolver.class);
 	private Map<String, JavaDelegate> delegateMap = new HashMap<String, JavaDelegate>();
-    private Map<String, ActivityBehavior> activityBehaviourMap = new HashMap<String, ActivityBehavior>();
+  private Map<String, ActivityBehavior> activityBehaviourMap = new HashMap<String, ActivityBehavior>();
 
-    public Object getValue(ELContext context, Object base, Object property) {
-        if (base == null) {
-            // according to javadoc, can only be a String
-            String key = (String) property;
-            for (String name : delegateMap.keySet()) {
-                if (name.equalsIgnoreCase(key)) {
-                    context.setPropertyResolved(true);
-                    return delegateMap.get(name);
-                }
-            }
-            for (String name : activityBehaviourMap.keySet()) {
-                if (name.equalsIgnoreCase(key)) {
-                    context.setPropertyResolved(true);
-                    return activityBehaviourMap.get(name);
-                }
-            }
+  public Object getValue(ELContext context, Object base, Object property) {
+    if (base == null) {
+      // according to javadoc, can only be a String
+      String key = (String) property;
+      for (String name : delegateMap.keySet()) {
+        if (name.equalsIgnoreCase(key)) {
+          context.setPropertyResolved(true);
+          return delegateMap.get(name);
         }
+      }
+      for (String name : activityBehaviourMap.keySet()) {
+        if (name.equalsIgnoreCase(key)) {
+          context.setPropertyResolved(true);
+          return activityBehaviourMap.get(name);
+        }
+      }
+    }
 
-		return null;
+    return null;
 	}
 	
 	@SuppressWarnings("rawtypes")
@@ -58,36 +58,34 @@ public class BlueprintELResolver extends ELResolver {
     LOGGER.info("removed Activiti service from delegate cache {}", name);
 	}
 
-    @SuppressWarnings("rawtypes")
-    public void bindActivityBehaviourService(ActivityBehavior delegate, Map props) {
-        String name = (String) props.get("osgi.service.blueprint.compname");
-        activityBehaviourMap.put(name, delegate);
-        LOGGER.info("added Activiti service to activity behaviour cache {}", name);
-    }
+	@SuppressWarnings("rawtypes")
+	public void bindActivityBehaviourService(ActivityBehavior delegate, Map props) {
+    String name = (String) props.get("osgi.service.blueprint.compname");
+    activityBehaviourMap.put(name, delegate);
+    LOGGER.info("added Activiti service to activity behaviour cache {}", name);
+  }
 
-    @SuppressWarnings("rawtypes")
-    public void unbindActivityBehaviourService(ActivityBehavior delegate, Map props) {
-        String name = (String) props.get("osgi.service.blueprint.compname");
-        if(activityBehaviourMap.containsKey(name)) {
-            activityBehaviourMap.remove(name);
-        }
-        LOGGER.info("removed Activiti service from activity behaviour cache {}", name);
+  @SuppressWarnings("rawtypes")
+  public void unbindActivityBehaviourService(ActivityBehavior delegate, Map props) {
+    String name = (String) props.get("osgi.service.blueprint.compname");
+    if (activityBehaviourMap.containsKey(name)) {
+      activityBehaviourMap.remove(name);
     }
+    LOGGER.info("removed Activiti service from activity behaviour cache {}", name);
+  }
 
 	public boolean isReadOnly(ELContext context, Object base, Object property) {
 		return true;
 	}
 
-	public void setValue(ELContext context, Object base, Object property,
-	    Object value) {
+	public void setValue(ELContext context, Object base, Object property, Object value) {
 	}
 
 	public Class<?> getCommonPropertyType(ELContext context, Object arg) {
 		return Object.class;
 	}
 
-	public Iterator<FeatureDescriptor> getFeatureDescriptors(ELContext context,
-	    Object arg) {
+	public Iterator<FeatureDescriptor> getFeatureDescriptors(ELContext context, Object arg) {
 		return null;
 	}
 
--- modules/activiti-osgi/src/test/java/org/activiti/osgi/blueprint/bean/ActivityBehaviourBean.java
+++ modules/activiti-osgi/src/test/java/org/activiti/osgi/blueprint/bean/ActivityBehaviourBean.java
@@ -4,9 +4,12 @@ import org.activiti.engine.impl.pvm.delegate.ActivityBehavior;
 import org.activiti.engine.impl.pvm.delegate.ActivityExecution;
 
 public class ActivityBehaviourBean implements ActivityBehavior {
-    @Override
-    public void execute(ActivityExecution execution) throws Exception {
-        execution.setVariable("visitedActivityBehaviour", true);
-        execution.end();
-    }
+  
+  private static final long serialVersionUID = 1L;
+
+  @Override
+  public void execute(ActivityExecution execution) throws Exception {
+    execution.setVariable("visitedActivityBehaviour", true);
+    execution.end();
+  }
 }
