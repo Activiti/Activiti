de95b7caef2763ddae83b3c9e508818a009d7a44
Robert Hafner <robert.hafner@gmail.com>
2015-04-21 13:27:00 -0400

    Add support for filtering historic variables by execution id. ACT:4000

--- modules/activiti-engine/src/test/java/org/activiti/engine/test/history/HistoricVariableInstanceTest.java
+++ modules/activiti-engine/src/test/java/org/activiti/engine/test/history/HistoricVariableInstanceTest.java
@@ -434,5 +434,26 @@ public class HistoricVariableInstanceTest extends PluggableActivitiTestCase {
      return historyService.createHistoricVariableInstanceQuery().variableName(variableName).singleResult();
    }
    
-  
+   @Deployment
+   public void testRestrictByExecutionId() {
+     if(processEngineConfiguration.getHistoryLevel().isAtLeast(HistoryLevel.FULL)) {
+       ProcessInstance processInstance = runtimeService.startProcessInstanceByKey("myProc");
+       TaskQuery taskQuery = taskService.createTaskQuery();
+       Task userTask = taskQuery.singleResult();
+       assertEquals("userTask1", userTask.getName());
+
+       taskService.complete(userTask.getId(), CollectionUtil.singletonMap("myVar", "test789"));
+
+       assertProcessEnded(processInstance.getId());
+
+       List<HistoricVariableInstance> variables = historyService.createHistoricVariableInstanceQuery().executionId(processInstance.getId()).list();
+       assertEquals(1, variables.size());
+
+       HistoricVariableInstanceEntity historicVariable = (HistoricVariableInstanceEntity) variables.get(0);
+       assertEquals("test456", historicVariable.getTextValue());
+
+       assertEquals(5, historyService.createHistoricActivityInstanceQuery().count());
+       assertEquals(3, historyService.createHistoricDetailQuery().count());
+     }
+   }
 }
