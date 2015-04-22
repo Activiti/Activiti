8f73d9de1abb35d8e64217be4155d0489a5c901e
Vasile Dirla <vasile@dirla.ro>
2015-04-22 15:26:26 +0300

    Reverted the process definition file. adjusted the Test.

--- modules/activiti-bpmn-converter/src/test/java/org/activiti/editor/language/xml/MessageConverterTest.java
+++ modules/activiti-bpmn-converter/src/test/java/org/activiti/editor/language/xml/MessageConverterTest.java
@@ -23,17 +23,17 @@ public class MessageConverterTest extends AbstractConverterTest {
   }
   
   private void validateModel(BpmnModel model) {
-    Message message = model.getMessage("writeReportId");
+    Message message = model.getMessage("writeReport");
     assertNotNull(message);
-    assertEquals("writeReportItem", message.getItemRef());
-    assertEquals("writeReportName", message.getName());
-    assertEquals("writeReportId", message.getId());
+    assertEquals("Examples:writeReportItem", message.getItemRef());
+    assertEquals("newWriteReport", message.getName());
+    assertEquals("writeReport", message.getId());
 
-    Message message2 = model.getMessage("writeReport2Id");
+    Message message2 = model.getMessage("writeReport2");
     assertNotNull(message2);
-    assertEquals("writeReportItem2", message2.getItemRef());
-    assertEquals("writeReport2Name", message2.getName());
-    assertEquals("writeReport2Id", message2.getId());
+    assertEquals("http://foo.bar.com/Examples:writeReportItem2", message2.getItemRef());
+    assertEquals("newWriteReport2", message2.getName());
+    assertEquals("writeReport2", message2.getId());
   }
   
   protected String getResource() {
