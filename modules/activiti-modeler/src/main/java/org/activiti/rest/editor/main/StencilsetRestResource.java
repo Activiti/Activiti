c015d11303339f50254a10be7335fd33546911ab
Zhiguo.Chen <chenzhiguo@live.com>
2015-04-30 10:51:33 +0800

    Fix read stencilset.json gibberish when in other languages

--- modules/activiti-modeler/src/main/java/org/activiti/rest/editor/main/StencilsetRestResource.java
+++ modules/activiti-modeler/src/main/java/org/activiti/rest/editor/main/StencilsetRestResource.java
@@ -27,11 +27,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class StencilsetRestResource {
   
-  @RequestMapping(value="/editor/stencilset", method = RequestMethod.GET, produces = "application/json")
+  @RequestMapping(value="/editor/stencilset", method = RequestMethod.GET, produces = "application/json;charset=utf-8")
   public @ResponseBody String getStencilset() {
     InputStream stencilsetStream = this.getClass().getClassLoader().getResourceAsStream("stencilset.json");
     try {
-      return IOUtils.toString(stencilsetStream);
+      return IOUtils.toString(stencilsetStream, "utf-8");
     } catch (Exception e) {
       throw new ActivitiException("Error while loading stencil set", e);
     }
