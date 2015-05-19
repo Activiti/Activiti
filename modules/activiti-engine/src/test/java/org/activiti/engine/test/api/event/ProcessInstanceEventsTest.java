05e13bfcb9775e4d62b48b2092b6179ac2f89ff4
Joram Barrez <joram.barrez@gmail.com>
2015-05-19 17:23:15 +0100

    Fix events test

--- modules/activiti-engine/src/test/java/org/activiti/engine/test/api/event/ProcessInstanceEventsTest.java
+++ modules/activiti-engine/src/test/java/org/activiti/engine/test/api/event/ProcessInstanceEventsTest.java
@@ -387,13 +387,19 @@ public class ProcessInstanceEventsTest extends PluggableActivitiTestCase {
     List<ActivitiEvent> activityTerminatedEvents = listener.filterEvents(ActivitiEventType.ACTIVITY_CANCELLED);
     assertThat("There should be exactly two ActivitiEventType.ACTIVITY_CANCELLED event after the task complete.", activityTerminatedEvents.size(), is(2));
 
-    ActivitiActivityCancelledEventImpl activityEvent = (ActivitiActivityCancelledEventImpl) activityTerminatedEvents.get(0);
-    assertThat("The user task must be terminated", activityEvent.getActivityId(), is("preNormalTerminateTask"));
-    assertThat("The cause must be terminate end event", ((FlowNode) activityEvent.getCause()).getId(), is("EndEvent_2"));
+    for (ActivitiEvent event : activityTerminatedEvents) {
+      
+      ActivitiActivityCancelledEventImpl activityEvent = (ActivitiActivityCancelledEventImpl) event;
+      if (activityEvent.getActivityId().equals("preNormalTerminateTask")) {
+        assertThat("The user task must be terminated", activityEvent.getActivityId(), is("preNormalTerminateTask"));
+        assertThat("The cause must be terminate end event", ((FlowNode) activityEvent.getCause()).getId(), is("EndEvent_2"));
+      } else if (activityEvent.getActivityId().equals("EndEvent_2")) {
+        assertThat("The end event must be terminated", activityEvent.getActivityId(), is("EndEvent_2"));
+        assertThat("The cause must be terminate end event", ((FlowNode) activityEvent.getCause()).getId(), is("EndEvent_2"));
+      }
+      
+    }
     
-    activityEvent = (ActivitiActivityCancelledEventImpl) activityTerminatedEvents.get(1);
-    assertThat("The end event must be terminated", activityEvent.getActivityId(), is("EndEvent_2"));
-    assertThat("The cause must be terminate end event", ((FlowNode) activityEvent.getCause()).getId(), is("EndEvent_2"));
   }
 
   @Deployment(resources = { "org/activiti/engine/test/bpmn/event/end/TerminateEndEventTest.testTerminateInCallActivity.bpmn",
