f1d48140cd88fd03bee1dadfe9353a57db38ce02
Vasile Dirla <vasile@dirla.ro>
2015-07-05 01:25:36 +0300

    Allow WebResult response mapping with default name "result" or allow the users to use any resrved keywrd as response name. ie:    @WebResult(name = "static") or just @WebResult

--- modules/activiti-cxf/src/test/java/org/activiti/engine/impl/webservice/WSDLImporterTest.java
+++ modules/activiti-cxf/src/test/java/org/activiti/engine/impl/webservice/WSDLImporterTest.java
@@ -51,29 +51,35 @@ public class WSDLImporterTest {
     WSService service = services.get(0);
     
     Assert.assertEquals("Counter", service.getName());
-    Assert.assertEquals("http://localhost:63081/counter", service.getLocation());
+    Assert.assertEquals("http://localhost:63081/webservicemock", service.getLocation());
     
     List<StructureDefinition> structures = sortStructures();
     List<WSOperation> operations = sortOperations();
 
-    Assert.assertEquals(5, operations.size());
+    Assert.assertEquals(7, operations.size());
     this.assertOperation(operations.get(0), "getCount", service);
     this.assertOperation(operations.get(1), "inc", service);
-    this.assertOperation(operations.get(2), "prettyPrintCount", service);
-    this.assertOperation(operations.get(3), "reset", service);
-    this.assertOperation(operations.get(4), "setTo", service);
+    this.assertOperation(operations.get(2), "noNameResult", service);
+    this.assertOperation(operations.get(3), "prettyPrintCount", service);
+    this.assertOperation(operations.get(4), "reservedWordAsName", service);
+    this.assertOperation(operations.get(5), "reset", service);
+    this.assertOperation(operations.get(6), "setTo", service);
     
-    Assert.assertEquals(10, structures.size());
+    Assert.assertEquals(14, structures.size());
     this.assertStructure(structures.get(0), "getCount", new String[] {}, new Class<?>[] {});
-    this.assertStructure(structures.get(1), "getCountResponse", new String[] {"count"}, new Class<?>[] {Integer.class});
+    this.assertStructure(structures.get(1), "getCountResponse", new String[] { "count" }, new Class<?>[] { Integer.class });
     this.assertStructure(structures.get(2), "inc", new String[] {}, new Class<?>[] {});
     this.assertStructure(structures.get(3), "incResponse", new String[] {}, new Class<?>[] {});
-    this.assertStructure(structures.get(4), "prettyPrintCount", new String[] {"prefix", "suffix"}, new Class<?>[] {String.class, String.class});
-    this.assertStructure(structures.get(5), "prettyPrintCountResponse", new String[] {"prettyPrint"}, new Class<?>[] {String.class});
-    this.assertStructure(structures.get(6), "reset", new String[] {}, new Class<?>[] {});
-    this.assertStructure(structures.get(7), "resetResponse", new String[] {}, new Class<?>[] {});
-    this.assertStructure(structures.get(8), "setTo", new String[] {"value"}, new Class<?>[] {Integer.class});
-    this.assertStructure(structures.get(9), "setToResponse", new String[] {}, new Class<?>[] {});
+    this.assertStructure(structures.get(4), "noNameResult", new String[] {"prefix", "suffix"}, new Class<?>[] {String.class, String.class});
+    this.assertStructure(structures.get(5), "noNameResultResponse", new String[] {"return"}, new Class<?>[] {String.class});
+    this.assertStructure(structures.get(6), "prettyPrintCount", new String[] {"prefix", "suffix"}, new Class<?>[] {String.class, String.class});
+    this.assertStructure(structures.get(7), "prettyPrintCountResponse", new String[] {"prettyPrint"}, new Class<?>[] {String.class});
+    this.assertStructure(structures.get(8), "reservedWordAsName", new String[] {"prefix","suffix"}, new Class<?>[] {String.class, String.class});
+    this.assertStructure(structures.get(9), "reservedWordAsNameResponse", new String[] {"static"}, new Class<?>[] {String.class});
+    this.assertStructure(structures.get(10), "reset", new String[] {}, new Class<?>[] {});
+    this.assertStructure(structures.get(11), "resetResponse", new String[] {}, new Class<?>[] {});
+    this.assertStructure(structures.get(12), "setTo", new String[] {"value"}, new Class<?>[] {Integer.class});
+    this.assertStructure(structures.get(13), "setToResponse", new String[] {}, new Class<?>[] {});
   }
   
   @Test
@@ -91,24 +97,30 @@ public class WSDLImporterTest {
     List<StructureDefinition> structures = sortStructures();
     List<WSOperation> operations = sortOperations();
 
-    Assert.assertEquals(5, operations.size());
+    Assert.assertEquals(7, operations.size());
     this.assertOperation(operations.get(0), "getCount", service);
     this.assertOperation(operations.get(1), "inc", service);
-    this.assertOperation(operations.get(2), "prettyPrintCount", service);
-    this.assertOperation(operations.get(3), "reset", service);
-    this.assertOperation(operations.get(4), "setTo", service);
+    this.assertOperation(operations.get(2), "noNameResult", service);
+    this.assertOperation(operations.get(3), "prettyPrintCount", service);
+    this.assertOperation(operations.get(4), "reservedWordAsName", service);
+    this.assertOperation(operations.get(5), "reset", service);
+    this.assertOperation(operations.get(6), "setTo", service);
     
-    Assert.assertEquals(10, structures.size());
+    Assert.assertEquals(14, structures.size());
     this.assertStructure(structures.get(0), "getCount", new String[] {}, new Class<?>[] {});
-    this.assertStructure(structures.get(1), "getCountResponse", new String[] {"count"}, new Class<?>[] {Integer.class});
+    this.assertStructure(structures.get(1), "getCountResponse", new String[] { "count" }, new Class<?>[] { Integer.class });
     this.assertStructure(structures.get(2), "inc", new String[] {}, new Class<?>[] {});
     this.assertStructure(structures.get(3), "incResponse", new String[] {}, new Class<?>[] {});
-    this.assertStructure(structures.get(4), "prettyPrintCount", new String[] {"prefix", "suffix"}, new Class<?>[] {String.class, String.class});
-    this.assertStructure(structures.get(5), "prettyPrintCountResponse", new String[] {"prettyPrint"}, new Class<?>[] {String.class});
-    this.assertStructure(structures.get(6), "reset", new String[] {}, new Class<?>[] {});
-    this.assertStructure(structures.get(7), "resetResponse", new String[] {}, new Class<?>[] {});
-    this.assertStructure(structures.get(8), "setTo", new String[] {"value"}, new Class<?>[] {Integer.class});
-    this.assertStructure(structures.get(9), "setToResponse", new String[] {}, new Class<?>[] {});
+    this.assertStructure(structures.get(4), "noNameResult", new String[] {"prefix", "suffix"}, new Class<?>[] {String.class, String.class});
+    this.assertStructure(structures.get(5), "noNameResultResponse", new String[] {"return"}, new Class<?>[] {String.class});
+    this.assertStructure(structures.get(6), "prettyPrintCount", new String[] {"prefix", "suffix"}, new Class<?>[] {String.class, String.class});
+    this.assertStructure(structures.get(7), "prettyPrintCountResponse", new String[] {"prettyPrint"}, new Class<?>[] {String.class});
+    this.assertStructure(structures.get(8), "reservedWordAsName", new String[] {"prefix", "suffix"}, new Class<?>[] {String.class, String.class});
+    this.assertStructure(structures.get(9), "reservedWordAsNameResponse", new String[] {"static"}, new Class<?>[] {String.class});
+    this.assertStructure(structures.get(10), "reset", new String[] {}, new Class<?>[] {});
+    this.assertStructure(structures.get(11), "resetResponse", new String[] {}, new Class<?>[] {});
+    this.assertStructure(structures.get(12), "setTo", new String[] {"value"}, new Class<?>[] {Integer.class});
+    this.assertStructure(structures.get(13), "setToResponse", new String[] {}, new Class<?>[] {});
   }
 
   private List<WSOperation> sortOperations() {
