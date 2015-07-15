46643421a3f91ef2ddc11976e48859d2ddc2cacb
Tijs Rademakers <tijs.rademakers@gmail.com>
2015-07-15 12:02:44 +0200

    Manual merge of https://github.com/Activiti/Activiti/commit/d7a966f0b65c1e0aa14fb75788f04ae569dbaa5a to v6: change to JobRetryCmd did not have to be applied on v6: process definition is not fetched, as the executionId is used to get the associated step in the process definition.

--- modules/activiti-engine/src/main/java/org/activiti/engine/impl/persistence/entity/TimerEntity.java
+++ modules/activiti-engine/src/main/java/org/activiti/engine/impl/persistence/entity/TimerEntity.java
@@ -28,10 +28,13 @@ import org.activiti.engine.impl.calendar.CycleBusinessCalendar;
 import org.activiti.engine.impl.context.Context;
 import org.activiti.engine.impl.el.NoExecutionVariableScope;
 import org.activiti.engine.impl.interceptor.CommandContext;
+import org.activiti.engine.impl.jobexecutor.TimerCatchIntermediateEventJobHandler;
 import org.activiti.engine.impl.jobexecutor.TimerDeclarationImpl;
 import org.activiti.engine.impl.jobexecutor.TimerEventHandler;
+import org.activiti.engine.impl.jobexecutor.TimerExecuteNestedActivityJobHandler;
 import org.activiti.engine.impl.jobexecutor.TimerStartEventJobHandler;
-import org.activiti.engine.impl.util.ProcessDefinitionUtil;
+import org.activiti.engine.impl.pvm.process.ActivityImpl;
+import org.activiti.engine.repository.ProcessDefinition;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -140,7 +143,11 @@ public class TimerEntity extends JobEntity {
 
         BusinessCalendar businessCalendar = Context.getProcessEngineConfiguration().getBusinessCalendarManager().getBusinessCalendar(CycleBusinessCalendar.NAME);
 
-        VariableScope executionEntity = commandContext.getExecutionEntityManager().findExecutionById(this.getExecutionId());
+        VariableScope executionEntity = null;
+        if (executionId != null) {
+          executionEntity = commandContext.getExecutionEntityManager().findExecutionById(executionId);
+        }
+        
         if (executionEntity == null) {
           executionEntity = NoExecutionVariableScope.getSharedInstance();
         }
