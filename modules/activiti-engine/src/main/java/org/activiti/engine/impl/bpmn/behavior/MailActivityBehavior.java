6bb132906d5a9620bbac33932f3901fa58726536
Tim Stephenson <tim@knowprocess.com>
2015-09-17 14:09:20 +0100

    Fix multitenant mail server configuration

--- modules/activiti-engine/src/main/java/org/activiti/engine/impl/bpmn/behavior/MailActivityBehavior.java
+++ modules/activiti-engine/src/main/java/org/activiti/engine/impl/bpmn/behavior/MailActivityBehavior.java
@@ -261,8 +261,8 @@ public class MailActivityBehavior extends AbstractBpmnActivityBehavior {
 
         email.setSmtpPort(mailServerInfo.getMailServerPort());
 
-        email.setSSLOnConnect(processEngineConfiguration.getMailServerUseSSL());
-        email.setStartTLSEnabled(processEngineConfiguration.getMailServerUseTLS());
+        email.setSSLOnConnect(mailServerInfo.isMailServerUseSSL());
+        email.setStartTLSEnabled(mailServerInfo.isMailServerUseTLS());
 
         String user = mailServerInfo.getMailServerUsername();
         String password = mailServerInfo.getMailServerPassword();
