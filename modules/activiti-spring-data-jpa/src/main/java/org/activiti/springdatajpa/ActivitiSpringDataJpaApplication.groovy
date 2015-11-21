72a64041c03dd721dbc75e8fb4007402f3648087
luis <luisbigvargas@gmail.com>
2015-11-21 12:09:47 -0500

    Reverse Engineer Activiti models

---
+++ modules/activiti-spring-data-jpa/src/main/java/org/activiti/springdatajpa/ActivitiSpringDataJpaApplication.groovy
@@ -0,0 +1,12 @@
+package org.activiti.springdatajpa
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class ActivitiSpringDataJpaApplication {
+
+    static void main(String[] args) {
+        SpringApplication.run ActivitiSpringDataJpaApplication, args
+    }
+}
