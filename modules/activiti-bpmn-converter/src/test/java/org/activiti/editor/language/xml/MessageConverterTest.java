3ab0bbda8d0ff4c7e4fc1ab16fd406b3039c19e0
Joram Barrez <joram.barrez@gmail.com>
2015-12-18 10:43:16 +0100

    Porting https://github.com/Activiti/Activiti/commit/74b57bfb203ff52d3a00c4b4e070aedf6d3ad66d to v6

--- modules/activiti-bpmn-converter/src/test/java/org/activiti/editor/language/xml/MessageConverterTest.java
+++ modules/activiti-bpmn-converter/src/test/java/org/activiti/editor/language/xml/MessageConverterTest.java
@@ -8,35 +8,35 @@ import org.activiti.bpmn.model.Message;
 import org.junit.Test;
 
 public class MessageConverterTest extends AbstractConverterTest {
-
+  
   @Test
   public void connvertXMLToModel() throws Exception {
     BpmnModel bpmnModel = readXMLFile();
     validateModel(bpmnModel);
   }
-
+  
   @Test
   public void convertModelToXML() throws Exception {
     BpmnModel bpmnModel = readXMLFile();
     BpmnModel parsedModel = exportAndReadXMLFile(bpmnModel);
     validateModel(parsedModel);
   }
-
+  
   private void validateModel(BpmnModel model) {
-    Message message = model.getMessage("writeReportId");
+    Message message = model.getMessage("writeReport");
     assertNotNull(message);
-    assertEquals("writeReportItem", message.getItemRef());
-    assertEquals("writeReportName", message.getName());
-    assertEquals("writeReportId", message.getId());
+    assertEquals("Examples:writeReportItem", message.getItemRef());
+    assertEquals("newWriteReport", message.getName());
+    assertEquals("writeReport", message.getId());
 
-    Message message2 = model.getMessage("writeReport2Id");
+    Message message2 = model.getMessage("writeReport2");
     assertNotNull(message2);
-    assertEquals("writeReportItem2", message2.getItemRef());
-    assertEquals("writeReport2Name", message2.getName());
-    assertEquals("writeReport2Id", message2.getId());
+    assertEquals("http://foo.bar.com/Examples:writeReportItem2", message2.getItemRef());
+    assertEquals("newWriteReport2", message2.getName());
+    assertEquals("writeReport2", message2.getId());
   }
-
+  
   protected String getResource() {
-    return "message.bpmn";
+        return "message.bpmn";
   }
 }
