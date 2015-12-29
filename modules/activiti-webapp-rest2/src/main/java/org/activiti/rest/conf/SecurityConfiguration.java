6cef92c7049aae28e1c87df15dbc3fe7241245d7
Vedran Pavic <vedran.pavic@gmail.com>
2015-12-29 11:37:28 +0100

    Upgrade Spring dependencies

--- modules/activiti-webapp-rest2/src/main/java/org/activiti/rest/conf/SecurityConfiguration.java
+++ modules/activiti-webapp-rest2/src/main/java/org/activiti/rest/conf/SecurityConfiguration.java
@@ -7,12 +7,10 @@ import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.config.annotation.web.servlet.configuration.EnableWebMvcSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 
 @Configuration
 @EnableWebSecurity
-@EnableWebMvcSecurity
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
   
   @Bean
