b3eb5fb476b7310ab7a2b0ff1262876da2864d06
Joram Barrez <joram.barrez@gmail.com>
2016-02-02 15:57:31 +0100

    Adding test from v6 bug, just to make sure (https://github.com/Activiti/Activiti/commit/67f7fac38239db33453c42c5725e78cfba5f8469)

--- modules/activiti-engine/src/test/java/org/activiti/engine/test/bpmn/gateway/ExclusiveGatewayTest.java
+++ modules/activiti-engine/src/test/java/org/activiti/engine/test/bpmn/gateway/ExclusiveGatewayTest.java
@@ -211,5 +211,16 @@ public class ExclusiveGatewayTest extends PluggableActivitiTestCase {
     }
 
   }
+
+  @Deployment
+  public void testExclusiveDirectlyToEnd() {
+    Map<String, Object> variables = new HashMap<String, Object>();
+    variables.put("input", 1);
+    ProcessInstance startProcessInstanceByKey = runtimeService.startProcessInstanceByKey("exclusiveGateway", variables);
+    long count = historyService.createHistoricActivityInstanceQuery()
+        .processInstanceId(startProcessInstanceByKey.getId()).unfinished()
+        .count();
+    assertEquals(0, count);
+  }
   
 }
