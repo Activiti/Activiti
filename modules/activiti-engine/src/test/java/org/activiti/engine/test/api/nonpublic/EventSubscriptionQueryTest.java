f2737581bbea4ade19176afb687246e8ed5adf03
Valentin Rentschler <valentin.rentschler@procentric.ch>
2016-02-04 16:27:46 +0100

    Fix bug to make it possible to query event subscriptions by id

--- modules/activiti-engine/src/test/java/org/activiti/engine/test/api/nonpublic/EventSubscriptionQueryTest.java
+++ modules/activiti-engine/src/test/java/org/activiti/engine/test/api/nonpublic/EventSubscriptionQueryTest.java
@@ -129,6 +129,38 @@ public class EventSubscriptionQueryTest extends PluggableActivitiTestCase {
 
   }
 
+  public void testQueryByEventSubscriptionId() {
+
+    processEngineConfiguration.getCommandExecutor().execute(new Command<Void>() {
+      public Void execute(CommandContext commandContext) {
+
+        MessageEventSubscriptionEntity messageEventSubscriptionEntity1 = commandContext.getEventSubscriptionEntityManager().createMessageEventSubscription();
+        messageEventSubscriptionEntity1.setEventName("messageName");
+        messageEventSubscriptionEntity1.setActivityId("someActivity");
+        commandContext.getEventSubscriptionEntityManager().insert(messageEventSubscriptionEntity1);
+
+        MessageEventSubscriptionEntity messageEventSubscriptionEntity2 = commandContext.getEventSubscriptionEntityManager().createMessageEventSubscription();
+        messageEventSubscriptionEntity2.setEventName("messageName");
+        messageEventSubscriptionEntity2.setActivityId("someOtherActivity");
+        commandContext.getEventSubscriptionEntityManager().insert(messageEventSubscriptionEntity2);
+
+        return null;
+      }
+    });
+
+    List<EventSubscriptionEntity> list = newEventSubscriptionQuery().activityId("someOtherActivity").list();
+    assertEquals(1, list.size());
+
+    final EventSubscriptionEntity entity = list.get(0);
+
+    list = newEventSubscriptionQuery().eventSubscriptionId(entity.getId()).list();
+
+    assertEquals(1, list.size());
+
+    cleanDb();
+
+  }
+
   @Deployment
   public void testQueryByExecutionId() {
 
