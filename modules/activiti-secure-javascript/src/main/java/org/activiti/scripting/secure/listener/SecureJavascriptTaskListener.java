68503051b1fa1cd5379fa81866f8b4330df0c37e
Joram Barrez <joram.barrez@gmail.com>
2016-06-07 18:03:32 +0200

    ACT-4177 : docs for secure scripting + refactor after review with Tijs: instead of behaviorFactory use a parsehandler (which is cleaner) + cleanups

--- modules/activiti-secure-javascript/src/main/java/org/activiti/scripting/secure/listener/SecureJavascriptTaskListener.java
+++ modules/activiti-secure-javascript/src/main/java/org/activiti/scripting/secure/listener/SecureJavascriptTaskListener.java
@@ -1,4 +1,3 @@
-
 /* Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
@@ -15,7 +14,7 @@ package org.activiti.scripting.secure.listener;
 
 import org.activiti.engine.delegate.DelegateTask;
 import org.activiti.engine.impl.bpmn.listener.ScriptTaskListener;
-import org.activiti.scripting.secure.behavior.SecureJavascriptCapableActivityBehaviorFactory;
+import org.activiti.scripting.secure.behavior.SecureJavascriptTaskParseHandler;
 import org.activiti.scripting.secure.impl.SecureJavascriptUtil;
 
 /**
@@ -26,7 +25,7 @@ public class SecureJavascriptTaskListener extends ScriptTaskListener {
   @Override
   public void notify(DelegateTask delegateTask) {
     validateParameters();
-    if (SecureJavascriptCapableActivityBehaviorFactory.LANGUAGE_JAVASCRIPT.equalsIgnoreCase(language.getValue(delegateTask).toString())) {
+    if (SecureJavascriptTaskParseHandler.LANGUAGE_JAVASCRIPT.equalsIgnoreCase(language.getValue(delegateTask).toString())) {
       Object result = SecureJavascriptUtil.evaluateScript(delegateTask, script.getExpressionText());
       if (resultVariable != null) {
         delegateTask.setVariable(resultVariable.getExpressionText(), result);
