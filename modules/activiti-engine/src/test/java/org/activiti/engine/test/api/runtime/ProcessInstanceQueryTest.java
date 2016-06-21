d3eafb4cd35a17740adc2a9f69741604811b749f
Tijs Rademakers <tijs.rademakers@gmail.com>
2016-06-21 10:39:42 +0200

    Test fixes

--- modules/activiti-engine/src/test/java/org/activiti/engine/test/api/runtime/ProcessInstanceQueryTest.java
+++ modules/activiti-engine/src/test/java/org/activiti/engine/test/api/runtime/ProcessInstanceQueryTest.java
@@ -1901,7 +1901,7 @@ public class ProcessInstanceQueryTest extends PluggableActivitiTestCase {
   @Deployment(resources = { "org/activiti/engine/test/api/oneTaskProcess.bpmn20.xml" })
   public void testQueryStartedAfter() throws Exception {
     Calendar calendar = new GregorianCalendar();
-    calendar.set(Calendar.YEAR, 2200);
+    calendar.set(Calendar.YEAR, 2030);
     calendar.set(Calendar.MONTH, 8);
     calendar.set(Calendar.DAY_OF_MONTH, 30);
     calendar.set(Calendar.HOUR_OF_DAY, 12);
