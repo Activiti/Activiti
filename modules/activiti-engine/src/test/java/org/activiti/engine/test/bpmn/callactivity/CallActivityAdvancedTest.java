a6c8fda1e64497f50e76bb1ada77044d4a77cdaf
Joram Barrez <joram.barrez@gmail.com>
2016-07-01 17:56:38 +0200

    Adding test to verify potential bug in https://forums.activiti.org/content/callactivity-not-setting-start-user-second-execution

--- modules/activiti-engine/src/test/java/org/activiti/engine/test/bpmn/callactivity/CallActivityAdvancedTest.java
+++ modules/activiti-engine/src/test/java/org/activiti/engine/test/bpmn/callactivity/CallActivityAdvancedTest.java
@@ -25,6 +25,7 @@ import org.activiti.engine.history.HistoricActivityInstance;
 import org.activiti.engine.history.HistoricProcessInstance;
 import org.activiti.engine.impl.history.HistoryLevel;
 import org.activiti.engine.impl.test.PluggableActivitiTestCase;
+import org.activiti.engine.impl.util.CollectionUtil;
 import org.activiti.engine.runtime.ProcessInstance;
 import org.activiti.engine.task.Task;
 import org.activiti.engine.task.TaskQuery;
@@ -340,6 +341,36 @@ public class CallActivityAdvancedTest extends PluggableActivitiTestCase {
     taskList = taskService.createTaskQuery().list();
     assertNotNull(taskList);
     assertEquals(0, taskList.size());
+  }
+  
+  @Deployment(resources = {
+      "org/activiti/engine/test/bpmn/callactivity/CallActivity.testStartUserIdSetWhenLooping.bpmn20.xml",
+      "org/activiti/engine/test/bpmn/callactivity/simpleSubProcess.bpmn20.xml"
+  })
+  public void testStartUserIdSetWhenLooping() {
+    identityService.setAuthenticatedUserId("kermit");
+    ProcessInstance processInstance = runtimeService.startProcessInstanceByKey("loopingCallActivity", CollectionUtil.singletonMap("input", 0));
+    for (int i=1; i<4; i++) {
+      Task task = taskService.createTaskQuery().singleResult();
+      assertEquals("Task in subprocess", task.getName());
+      identityService.setAuthenticatedUserId("kermit");
+      taskService.complete(task.getId(), CollectionUtil.singletonMap("input", i));
+    }
+    identityService.setAuthenticatedUserId(null);
+    
+    Task task = taskService.createTaskQuery().singleResult();
+    assertEquals("Final task", task.getName());
+    
+    if (processEngineConfiguration.getHistoryLevel().isAtLeast(HistoryLevel.ACTIVITY)) {
+      List<HistoricProcessInstance> historicProcessInstances = historyService.createHistoricProcessInstanceQuery()
+          .superProcessInstanceId(processInstance.getId()).list();
+      assertEquals(3, historicProcessInstances.size());
+      for (HistoricProcessInstance historicProcessInstance : historicProcessInstances) {
+        assertNotNull(historicProcessInstance.getStartUserId());
+        assertNotNull(historicProcessInstance.getStartTime());
+        assertNotNull(historicProcessInstance.getEndTime());
+      }
+    }
   }
     
 }
