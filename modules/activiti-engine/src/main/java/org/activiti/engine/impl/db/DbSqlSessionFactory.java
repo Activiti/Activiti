0ebbee083af8f9047503b0ad50cf56f1a95504e3
Joram Barrez <joram.barrez@gmail.com>
2016-07-07 12:23:59 +0200

    Fix failing QA: databaseCatalog is empty string by default.

--- modules/activiti-engine/src/main/java/org/activiti/engine/impl/db/DbSqlSessionFactory.java
+++ modules/activiti-engine/src/main/java/org/activiti/engine/impl/db/DbSqlSessionFactory.java
@@ -70,14 +70,14 @@ public class DbSqlSessionFactory implements SessionFactory {
 
   public Session openSession(CommandContext commandContext) {
     DbSqlSession dbSqlSession = new DbSqlSession(this, commandContext.getEntityCache());
-    if (getDatabaseSchema() != null) {
+    if (getDatabaseSchema() != null && getDatabaseSchema().length() > 0) {
       try {
         dbSqlSession.getSqlSession().getConnection().setSchema(getDatabaseSchema());
       } catch (SQLException e) {
         throw new ActivitiException("Could not set database schema on connection", e);
       }
     }
-    if (getDatabaseCatalog() != null) {
+    if (getDatabaseCatalog() != null && getDatabaseCatalog().length() > 0) {
       try {
         dbSqlSession.getSqlSession().getConnection().setCatalog(getDatabaseCatalog());
       } catch (SQLException e) {
