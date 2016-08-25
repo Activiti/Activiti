9b649ac6da4293d736676de895a67aaf87fc5979
Joram Barrez <joram.barrez@gmail.com>
2016-08-25 08:05:04 +0200

    Failing sql server fix part 2: fix typo (mysql -> mssql)

--- modules/activiti-engine/src/main/java/org/activiti/engine/impl/cfg/ProcessEngineConfigurationImpl.java
+++ modules/activiti-engine/src/main/java/org/activiti/engine/impl/cfg/ProcessEngineConfigurationImpl.java
@@ -1209,7 +1209,7 @@ public abstract class ProcessEngineConfigurationImpl extends ProcessEngineConfig
 
       // Special care for MSSQL, as it has a hard limit of 2000 params per statement (incl bulk statement).
       // Especially with executions, with 100 as default, this limit is passed.
-      if (DATABASE_TYPE_MYSQL.equals(databaseType)) {
+      if (DATABASE_TYPE_MSSQL.equals(databaseType)) {
         maxNrOfStatementsInBulkInsert = DEFAULT_MAX_NR_OF_STATEMENTS_BULK_INSERT_SQL_SERVER;
       }
       
