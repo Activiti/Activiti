78cf88e81d4742debefad6426fa25ddb80b67cc1
Tijs Rademakers <tijs.rademakers@gmail.com>
2016-08-25 13:15:58 +0200

    Manual merge of PR #993

--- modules/activiti-bpmn-converter/src/test/java/org/activiti/editor/language/xml/TimerDefinitionConverterTest.java
+++ modules/activiti-bpmn-converter/src/test/java/org/activiti/editor/language/xml/TimerDefinitionConverterTest.java
@@ -1,12 +1,17 @@
 package org.activiti.editor.language.xml;
 
-import org.activiti.bpmn.model.*;
-import org.junit.Test;
-
-import java.util.List;
-
 import static org.hamcrest.CoreMatchers.is;
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertThat;
+
+import org.activiti.bpmn.model.BoundaryEvent;
+import org.activiti.bpmn.model.BpmnModel;
+import org.activiti.bpmn.model.IntermediateCatchEvent;
+import org.activiti.bpmn.model.StartEvent;
+import org.activiti.bpmn.model.TimerEventDefinition;
+import org.junit.Test;
 
 public class TimerDefinitionConverterTest extends AbstractConverterTest {
   
@@ -33,15 +38,20 @@ public class TimerDefinitionConverterTest extends AbstractConverterTest {
     assertNotNull(timer);
     TimerEventDefinition timerEvent = (TimerEventDefinition) timer.getEventDefinitions().get(0);
     assertThat(timerEvent.getCalendarName(), is("custom"));
+    assertEquals("PT5M", timerEvent.getTimeDuration());
 
     StartEvent start = (StartEvent) model.getMainProcess().getFlowElement("theStart");
     assertNotNull(start);
-    TimerEventDefinition startTimerEvent = (TimerEventDefinition) timer.getEventDefinitions().get(0);
+    TimerEventDefinition startTimerEvent = (TimerEventDefinition) start.getEventDefinitions().get(0);
     assertThat(startTimerEvent.getCalendarName(), is("custom"));
+    assertEquals("R2/PT5S", startTimerEvent.getTimeCycle());
+    assertEquals("${EndDate}", startTimerEvent.getEndDate());
 
     BoundaryEvent boundaryTimer = (BoundaryEvent) model.getMainProcess().getFlowElement("boundaryTimer");
     assertNotNull(boundaryTimer);
-    TimerEventDefinition boundaryTimerEvent = (TimerEventDefinition) timer.getEventDefinitions().get(0);
+    TimerEventDefinition boundaryTimerEvent = (TimerEventDefinition) boundaryTimer.getEventDefinitions().get(0);
     assertThat(boundaryTimerEvent.getCalendarName(), is("custom"));
+    assertEquals("PT10S", boundaryTimerEvent.getTimeDuration());
+    assertNull(boundaryTimerEvent.getEndDate());
   }
 }
