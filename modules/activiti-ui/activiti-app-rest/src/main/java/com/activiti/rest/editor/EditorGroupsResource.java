177163d3d0bb2ea76800e8c0896ccc03f8c64376
Joram Barrez <joram.barrez@gmail.com>
2016-08-30 17:29:49 +0200

    Activiti 6 UI : add user id to group page + fix create user popup id/email requirement + group search in bpmn editor fix

--- modules/activiti-ui/activiti-app-rest/src/main/java/com/activiti/rest/editor/EditorGroupsResource.java
+++ modules/activiti-ui/activiti-app-rest/src/main/java/com/activiti/rest/editor/EditorGroupsResource.java
@@ -12,21 +12,45 @@
  */
 package com.activiti.rest.editor;
 
+import java.util.List;
+
+import org.activiti.engine.IdentityService;
+import org.activiti.engine.identity.Group;
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.activiti.constant.GroupTypes;
 import com.activiti.model.common.ResultListDataRepresentation;
 
 /**
  * Rest resource for managing groups, used in the editor app.
  */
 @RestController
-public class EditorGroupsResource extends AbstractEditorGroupsResource {
-	
-    @RequestMapping(value = "/rest/editor-groups", method = RequestMethod.GET)
-    public ResultListDataRepresentation getGroups(@RequestParam(required=false, value="filter") String filter) {
-        return super.getGroups(filter);
+public class EditorGroupsResource {
+
+  @Autowired
+  protected IdentityService identityService;
+
+  @RequestMapping(value = "/rest/editor-groups", method = RequestMethod.GET)
+  public ResultListDataRepresentation getGroups(@RequestParam(required = false, value = "filter") String filter) {
+    String groupNameFilter = filter;
+    if (StringUtils.isEmpty(groupNameFilter)) {
+      groupNameFilter = "%";
+    } else {
+      groupNameFilter = "%" + groupNameFilter + "%";
     }
+    List<Group> matchingGroups = identityService.createGroupQuery()
+        .groupNameLike(groupNameFilter)
+        .groupType(GroupTypes.TYPE_ASSIGNMENT)
+        .list();
+
+    ResultListDataRepresentation result = new ResultListDataRepresentation(matchingGroups);
+    // TODO: get total result count instead of page-count, in case the matching
+    // list's size is equal to the page size
+    return result;
+  }
 }
