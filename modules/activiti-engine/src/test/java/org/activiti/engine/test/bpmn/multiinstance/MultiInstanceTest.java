a813e93531a4ca491ef759114d0ef6d5831a6e37
Joram Barrez <joram.barrez@gmail.com>
2016-09-02 16:25:30 +0200

    Fixes ACT-4212 : Mutating list variable for users in parallel multi instance tasks causes floating executions

--- modules/activiti-engine/src/test/java/org/activiti/engine/test/bpmn/multiinstance/MultiInstanceTest.java
+++ modules/activiti-engine/src/test/java/org/activiti/engine/test/bpmn/multiinstance/MultiInstanceTest.java
@@ -1344,6 +1344,20 @@ public class MultiInstanceTest extends PluggableActivitiTestCase {
     }
   }
   
+  @Deployment
+  public void testChangingCollection() {
+    Map<String, Object> vars = new HashMap<String, Object>();
+    vars.put("multi_users", Arrays.asList("testuser"));
+    ProcessInstance instance = runtimeService.startProcessInstanceByKey("test_multi", vars);
+    assertNotNull(instance);
+    Task task = taskService.createTaskQuery().singleResult();
+    assertEquals("multi", task.getTaskDefinitionKey());
+    vars.put("multi_users", new ArrayList<String>()); // <-- Problem here.
+    taskService.complete(task.getId(), vars);
+    List<ProcessInstance> instances = runtimeService.createProcessInstanceQuery().list();
+    assertEquals(0, instances.size());
+  }
+  
   /// HELPERS //////////////////
   
   protected void resetTestCounts() {
