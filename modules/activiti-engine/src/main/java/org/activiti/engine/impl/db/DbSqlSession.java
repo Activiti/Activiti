3f3520fef13efeaae9e2d72d5209b66c9922ed4f
suttermichael <sutter@gmx.li>
2016-09-15 17:37:28 +0200

    Update DbSqlSession.java

--- modules/activiti-engine/src/main/java/org/activiti/engine/impl/db/DbSqlSession.java
+++ modules/activiti-engine/src/main/java/org/activiti/engine/impl/db/DbSqlSession.java
@@ -1302,7 +1302,7 @@ public class DbSqlSession implements Session {
       Connection connection = sqlSession.getConnection();
       Exception exception = null;
       byte[] bytes = IoUtil.readInputStream(inputStream, resourceName);
-      String ddlStatements = new String(bytes);
+      String ddlStatements = Arrays.toString(bytes);
       String databaseType = dbSqlSessionFactory.getDatabaseType();
       
       // Special DDL handling for certain databases
