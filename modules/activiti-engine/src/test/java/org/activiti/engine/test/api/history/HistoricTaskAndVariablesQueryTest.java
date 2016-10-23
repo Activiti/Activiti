d75e56b56f163feb80c538db26e43d7f38d2e60c
Massimo Mangoni <massimo@mangoni.org>
2016-10-23 16:10:45 +0200

    Optimization for HistoricTask queries for 'or queries' with multiple process variable values. Added some test cases.

--- modules/activiti-engine/src/test/java/org/activiti/engine/test/api/history/HistoricTaskAndVariablesQueryTest.java
+++ modules/activiti-engine/src/test/java/org/activiti/engine/test/api/history/HistoricTaskAndVariablesQueryTest.java
@@ -20,6 +20,7 @@ import java.util.List;
 import java.util.Map;
 
 import org.activiti.engine.history.HistoricTaskInstance;
+import org.activiti.engine.history.HistoricTaskInstanceQuery;
 import org.activiti.engine.impl.history.HistoryLevel;
 import org.activiti.engine.impl.test.PluggableActivitiTestCase;
 import org.activiti.engine.runtime.ProcessInstance;
@@ -350,6 +351,34 @@ public class HistoricTaskAndVariablesQueryTest extends PluggableActivitiTestCase
       assertNotNull(variableMap.get("testVar2"));
       assertEquals(123, variableMap.get("testVar2"));
     }
+  }
+
+  @Deployment
+  public void testOrQueryMultipleVariableValues() {
+      Map<String, Object> startMap = new HashMap<String, Object>();
+      startMap.put("processVar", true);
+      startMap.put("anotherProcessVar", 123);
+      runtimeService.startProcessInstanceByKey("oneTaskProcess", startMap);
+
+      startMap.put("anotherProcessVar", 999);
+      runtimeService.startProcessInstanceByKey("oneTaskProcess", startMap);
+
+      HistoricTaskInstanceQuery query0 = historyService.createHistoricTaskInstanceQuery().includeProcessVariables().or();
+      for (int i = 0; i < 20; i++) {
+          query0 = query0.processVariableValueEquals("anotherProcessVar", i);
+      }
+      query0 = query0.endOr();
+      assertNull(query0.singleResult());
+
+      HistoricTaskInstanceQuery query1 = historyService.createHistoricTaskInstanceQuery().includeProcessVariables().or().processVariableValueEquals("anotherProcessVar", 123);
+      for (int i = 0; i < 20; i++) {
+          query1 = query1.processVariableValueEquals("anotherProcessVar", i);
+      }
+      query1 = query1.endOr();
+      HistoricTaskInstance task = query1.singleResult();
+      assertEquals(2, task.getProcessVariables().size());
+      assertEquals(true, task.getProcessVariables().get("processVar"));
+      assertEquals(123, task.getProcessVariables().get("anotherProcessVar"));
   }
   
   @Deployment
