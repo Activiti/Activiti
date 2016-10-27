ff0037094bf5c6c89735e65504b2db66ac4bcb0b
Bassam Al-Sarori <balsarori@gmail.com>
2016-10-27 10:25:57 +0100

    porting #1059 to v6

--- modules/activiti-rest/src/main/java/org/activiti/rest/service/api/engine/variable/QueryVariable.java
+++ modules/activiti-rest/src/main/java/org/activiti/rest/service/api/engine/variable/QueryVariable.java
@@ -40,6 +40,10 @@ public class QueryVariable {
     return QueryVariableOperation.forFriendlyName(operation);
   }
 
+  public String getOperation() {
+    return operation;
+  }
+
   public void setOperation(String operation) {
     this.operation = operation;
   }
