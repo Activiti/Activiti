2d47dd9701e613f4b8897553fed81c6f4e08c7ce
Vasile Dirla <vasile@dirla.ro>
2017-04-04 17:55:19 +0300

    Check the history level

--- modules/activiti-engine/src/test/java/org/activiti/engine/test/api/history/HistoricTaskAndVariablesQueryTest.java
+++ modules/activiti-engine/src/test/java/org/activiti/engine/test/api/history/HistoricTaskAndVariablesQueryTest.java
@@ -309,6 +309,7 @@ public class HistoricTaskAndVariablesQueryTest extends PluggableActivitiTestCase
 
   @Deployment
   public void testOrQueryMultipleVariableValues() {
+    if (processEngineConfiguration.getHistoryLevel().isAtLeast(HistoryLevel.ACTIVITY)) {
       Map<String, Object> startMap = new HashMap<String, Object>();
       startMap.put("processVar", true);
       startMap.put("anotherProcessVar", 123);
@@ -319,20 +320,21 @@ public class HistoricTaskAndVariablesQueryTest extends PluggableActivitiTestCase
 
       HistoricTaskInstanceQuery query0 = historyService.createHistoricTaskInstanceQuery().includeProcessVariables().or();
       for (int i = 0; i < 20; i++) {
-          query0 = query0.processVariableValueEquals("anotherProcessVar", i);
+        query0 = query0.processVariableValueEquals("anotherProcessVar", i);
       }
       query0 = query0.endOr();
       assertNull(query0.singleResult());
 
       HistoricTaskInstanceQuery query1 = historyService.createHistoricTaskInstanceQuery().includeProcessVariables().or().processVariableValueEquals("anotherProcessVar", 123);
       for (int i = 0; i < 20; i++) {
-          query1 = query1.processVariableValueEquals("anotherProcessVar", i);
+        query1 = query1.processVariableValueEquals("anotherProcessVar", i);
       }
       query1 = query1.endOr();
       HistoricTaskInstance task = query1.singleResult();
       assertEquals(2, task.getProcessVariables().size());
       assertEquals(true, task.getProcessVariables().get("processVar"));
       assertEquals(123, task.getProcessVariables().get("anotherProcessVar"));
+    }
   }
 
   @Deployment
