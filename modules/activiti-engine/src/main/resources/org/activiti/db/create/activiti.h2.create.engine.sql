ff0ebdd81ffb1d4732d905e78513124250aa87aa
Bassam Al-Sarori <balsarori@gmail.com>
2017-06-05 15:14:45 +0100

    update schema version

--- modules/activiti-engine/src/main/resources/org/activiti/db/create/activiti.h2.create.engine.sql
+++ modules/activiti-engine/src/main/resources/org/activiti/db/create/activiti.h2.create.engine.sql
@@ -6,10 +6,10 @@ create table ACT_GE_PROPERTY (
 );
 
 insert into ACT_GE_PROPERTY
-values ('schema.version', '6.0.0.4', 1);
+values ('schema.version', '6.0.1.0', 1);
 
 insert into ACT_GE_PROPERTY
-values ('schema.history', 'create(6.0.0.4)', 1);
+values ('schema.history', 'create(6.0.1.0)', 1);
 
 insert into ACT_GE_PROPERTY
 values ('next.dbid', '1', 1);
