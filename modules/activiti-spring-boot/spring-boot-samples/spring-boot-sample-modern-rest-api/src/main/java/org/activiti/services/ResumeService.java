1dae30fcbc105e8600dfcbc79e90bdc0a8f906ba
Salaboy <salaboy@gmail.com>
2017-06-14 11:47:15 +0100

    adding new apis sample project

---
+++ modules/activiti-spring-boot/spring-boot-samples/spring-boot-sample-modern-rest-api/src/main/java/org/activiti/services/ResumeService.java
@@ -0,0 +1,12 @@
+package org.activiti.services;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class ResumeService {
+
+    public void storeResume() {
+        System.out.println("Storing resume ...");
+    }
+
+}
