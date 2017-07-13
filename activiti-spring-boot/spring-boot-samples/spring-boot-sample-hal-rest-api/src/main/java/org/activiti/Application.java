3cb158507d5ae182d92e62430338972ecf26f989
Salaboy <salaboy@gmail.com>
2017-07-13 16:40:50 +0100

    adding bindings to sample

--- activiti-spring-boot/spring-boot-samples/spring-boot-sample-hal-rest-api/src/main/java/org/activiti/Application.java
+++ activiti-spring-boot/spring-boot-samples/spring-boot-sample-hal-rest-api/src/main/java/org/activiti/Application.java
@@ -2,10 +2,13 @@ package org.activiti;
 
 
 
+import org.activiti.services.audit.producer.app.AuditProducerChannels;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.stream.annotation.EnableBinding;
 
 @SpringBootApplication
+@EnableBinding(AuditProducerChannels.class)
 public class Application {
 
 	public static void main(String[] args) {
