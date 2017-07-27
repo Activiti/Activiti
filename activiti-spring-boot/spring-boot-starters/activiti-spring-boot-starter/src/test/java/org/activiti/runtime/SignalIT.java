7ed2d86a2d529256f5e45d8f01a302d811d78f1c
Giuseppe Malanga <gmalanga@zaizi.com>
2017-07-27 14:36:00 +0100

    Removed unused import.

--- activiti-spring-boot/spring-boot-starters/activiti-spring-boot-starter/src/test/java/org/activiti/runtime/SignalIT.java
+++ activiti-spring-boot/spring-boot-starters/activiti-spring-boot-starter/src/test/java/org/activiti/runtime/SignalIT.java
@@ -20,7 +20,6 @@ import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
 
-import com.sun.mail.iap.Response;
 import org.activiti.definition.ProcessDefinitionIT;
 import org.activiti.keycloak.KeycloakEnabledBaseTestIT;
 import org.activiti.keycloak.ProcessInstanceKeycloakRestTemplate;
