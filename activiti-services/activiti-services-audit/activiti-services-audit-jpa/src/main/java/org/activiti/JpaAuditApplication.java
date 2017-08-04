3cf52aca9d00f7bf10c511a267fdd1929e65a06c
Salaboy <Salaboy@gmail.com>
2017-08-04 09:55:48 +0100

    enable discovery (#1341)

--- activiti-services/activiti-services-audit/activiti-services-audit-jpa/src/main/java/org/activiti/JpaAuditApplication.java
+++ activiti-services/activiti-services-audit/activiti-services-audit-jpa/src/main/java/org/activiti/JpaAuditApplication.java
@@ -23,12 +23,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.sleuth.sampler.AlwaysSampler;
 import org.springframework.cloud.stream.annotation.EnableBinding;
 import org.springframework.cloud.stream.annotation.StreamListener;
 import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 @EnableBinding(AuditConsumerChannels.class)
 public class JpaAuditApplication implements CommandLineRunner {
 
