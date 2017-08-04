15a037d989937b9db702abbab57b81405b4e1472
Salaboy <Salaboy@gmail.com>
2017-08-04 09:56:45 +0100

    enabing auto discovery (#3)

--- spring-boot-sample-activiti/src/main/java/org/activiti/Application.java
+++ spring-boot-sample-activiti/src/main/java/org/activiti/Application.java
@@ -5,12 +5,14 @@ package org.activiti;
 import org.activiti.services.events.ProcessEngineChannels;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.stream.annotation.EnableBinding;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 @EnableBinding(ProcessEngineChannels.class)
 @ComponentScan("org.activiti")
+@EnableDiscoveryClient
 public class Application {
 
 	public static void main(String[] args) {
