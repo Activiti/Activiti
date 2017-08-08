d41128ac1fd8451d14b5b73a8f64f5be42a01e49
Francesco Corti <fcorti@gmail.com>
2017-08-08 17:04:11 +0200

    README files for Emergency Call Center example.

--- emergency-call-center/emergency-department/run.sh
+++ emergency-call-center/emergency-department/run.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 
-./package.sh
-java -jar target/emergency-department-1.0-SNAPSHOT.jar --server.port=8080
+java -jar target/emergency-department-7-201708-EA-SNAPSHOT.jar --server.port=8080
--- emergency-call-center/emergency-department/run_depts_in_background.sh
+++ emergency-call-center/emergency-department/run_depts_in_background.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-./package.sh
-
-java -jar target/emergency-department-1.0-SNAPSHOT.jar --server.port=9090 &
-java -jar target/emergency-department-1.0-SNAPSHOT.jar --server.port=9091 &
-java -jar target/emergency-department-1.0-SNAPSHOT.jar --server.port=9092 &
+java -jar target/emergency-department-7-201708-EA-SNAPSHOT.jar --server.port=9090 &
+java -jar target/emergency-department-7-201708-EA-SNAPSHOT.jar --server.port=9091 &
+java -jar target/emergency-department-7-201708-EA-SNAPSHOT.jar --server.port=9092 &
