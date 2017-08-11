4967eb582c1e595f9d26fbf334091bf204c2a005
Ryan Dawson <ryan.dawson@alfresco.com>
2017-08-11 12:35:46 +0100

    narrow down package

--- activiti-spring-boot/spring-boot-starters/activiti-spring-boot-audit-starter/src/main/java/org/activiti/configuration/SwaggerConfig.java
+++ activiti-spring-boot/spring-boot-starters/activiti-spring-boot-audit-starter/src/main/java/org/activiti/configuration/SwaggerConfig.java
@@ -31,7 +31,7 @@ public class SwaggerConfig {
     @Bean
     public Docket api() {
         return new Docket(DocumentationType.SWAGGER_2).select()
-                                                      .apis(RequestHandlerSelectors.basePackage("org.activiti.services"))
+                                                      .apis(RequestHandlerSelectors.basePackage("org.activiti.services.audit"))
                                                       .paths(PathSelectors.any())
                                                       .build();
     }
