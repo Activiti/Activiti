c15e21827becc1f2cf6fc003d94ab29bd83ea773
daisuke-yoshimoto <yoshimoto@intra-mart.jp>
2017-08-20 06:49:59 +0900

    Improvement of sending event to Audit Service

--- activiti-services/activiti-services-events/src/main/java/org/activiti/services/events/MessageProducerActivitiEventListener.java
+++ activiti-services/activiti-services-events/src/main/java/org/activiti/services/events/MessageProducerActivitiEventListener.java
@@ -1,11 +1,16 @@
 package org.activiti.services.events;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.activiti.engine.delegate.event.ActivitiEvent;
 import org.activiti.engine.delegate.event.ActivitiEventListener;
+import org.activiti.engine.impl.context.Context;
+import org.activiti.engine.impl.interceptor.CommandContext;
+import org.activiti.engine.impl.interceptor.CommandContextCloseListener;
 import org.activiti.services.api.events.ProcessEngineEvent;
 import org.activiti.services.events.converter.EventConverterContext;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.messaging.support.MessageBuilder;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -24,10 +29,34 @@ public class MessageProducerActivitiEventListener implements ActivitiEventListen
 
     @Override
     public void onEvent(ActivitiEvent event) {
+        CommandContext currentCommandContext = Context.getCommandContext();
         ProcessEngineEvent newEvent = converterContext.from(event);
-        if (newEvent != null) {
-            producer.auditProducer().send(MessageBuilder.withPayload(newEvent).build());
+        if (newEvent == null) {
+            return;
         }
+        @SuppressWarnings("unchecked")
+        List<ProcessEngineEvent> events = (List<ProcessEngineEvent>) currentCommandContext
+                                                                                          .getAttribute(MessageProducerCommandContextCloseListener.PROCESS_ENGINE_EVENTS);
+        if (events != null) {
+            events.add(newEvent);
+        } else {
+            events = new ArrayList<ProcessEngineEvent>();
+            events.add(newEvent);
+            currentCommandContext.addAttribute(MessageProducerCommandContextCloseListener.PROCESS_ENGINE_EVENTS,
+                                               events);
+        }
+
+        List<CommandContextCloseListener> listeners = currentCommandContext.getCloseListeners();
+        if (listeners.size() != 0) {
+            for (CommandContextCloseListener listener : listeners) {
+                if (listener instanceof MessageProducerCommandContextCloseListener) {
+                    return;
+                }
+            }
+        }
+        MessageProducerCommandContextCloseListener messageListener = new MessageProducerCommandContextCloseListener(
+                                                                                                                    producer);
+        currentCommandContext.addCloseListener(messageListener);
     }
 
     @Override
