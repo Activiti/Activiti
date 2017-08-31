bc852f195e4decd0c8fc89faa09161823a2de50c
LAPTOP-MTIQEU28\qijunbo <junboqi@foxmail.com>
2017-08-31 13:18:25 +0800

    can not start anyway.

---
+++ modules/activiti-ui/activiti-app/src/main/java/com/sunway/activiti/ActivitiRestApp.java
@@ -0,0 +1,13 @@
+package com.sunway.activiti;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ActivitiRestApp {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ActivitiRestApp.class);
+    }
+ 
+}
