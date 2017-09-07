e48b9caa731b27b56dab693c607bc7501d6d9a14
daisuke-yoshimoto <yoshimoto@intra-mart.jp>
2017-09-07 17:24:30 +0900

    Add test pattern

--- activiti-services/activiti-services-audit/activiti-services-audit-mongo/src/main/java/org/activiti/services/audit/mongo/EventsMongoRepository.java
+++ activiti-services/activiti-services-audit/activiti-services-audit-mongo/src/main/java/org/activiti/services/audit/mongo/EventsMongoRepository.java
@@ -1,25 +1,8 @@
 package org.activiti.services.audit.mongo;
 
-import java.util.List;
-import java.util.Optional;
-
 import org.activiti.services.audit.mongo.events.ProcessEngineEventDocument;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.data.querydsl.QuerydslPredicateExecutor;
 
 public interface EventsMongoRepository extends MongoRepository<ProcessEngineEventDocument, String>, QuerydslPredicateExecutor<ProcessEngineEventDocument> {
-
-    @Override
-    public Optional<ProcessEngineEventDocument> findById(String id);
-
-    @Override
-    public List<ProcessEngineEventDocument> findAll();
-
-    @Override
-    public Page<ProcessEngineEventDocument> findAll(Pageable pageable);
-
-    @Override
-    public void deleteAll();
 }
