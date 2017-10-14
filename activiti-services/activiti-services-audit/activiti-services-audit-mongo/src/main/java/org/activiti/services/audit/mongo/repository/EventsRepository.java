6037e8f9570db70f7f1942a16abfb0f3b00bccee
Salaboy <Salaboy@gmail.com>
2017-10-14 16:28:50 +0100

    moving activiti-services to their own separete repo (#1502)

--- activiti-services/activiti-services-audit/activiti-services-audit-mongo/src/main/java/org/activiti/services/audit/mongo/repository/EventsRepository.java
+++
@@ -1,8 +0,0 @@
-package org.activiti.services.audit.mongo.repository;
-
-import org.activiti.services.audit.mongo.events.ProcessEngineEventDocument;
-import org.springframework.data.mongodb.repository.MongoRepository;
-import org.springframework.data.querydsl.QuerydslPredicateExecutor;
-
-public interface EventsRepository extends MongoRepository<ProcessEngineEventDocument, String>, QuerydslPredicateExecutor<ProcessEngineEventDocument> {
-}
