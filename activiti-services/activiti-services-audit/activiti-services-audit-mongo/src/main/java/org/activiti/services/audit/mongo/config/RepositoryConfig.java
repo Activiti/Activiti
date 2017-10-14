461ee9c1aeb0a59178a09a886460698614fdf7ff
daisuke-yoshimoto <yoshimoto@intra-mart.jp>
2017-10-14 14:29:06 +0900

    Remove controller & assembler

--- activiti-services/activiti-services-audit/activiti-services-audit-mongo/src/main/java/org/activiti/services/audit/mongo/config/RepositoryConfig.java
+++ activiti-services/activiti-services-audit/activiti-services-audit-mongo/src/main/java/org/activiti/services/audit/mongo/config/RepositoryConfig.java
@@ -11,17 +11,26 @@ import org.activiti.services.audit.mongo.events.TaskCreatedEventDocument;
 import org.activiti.services.audit.mongo.events.VariableCreatedEventDocument;
 import org.activiti.services.audit.mongo.events.VariableDeletedEventDocument;
 import org.activiti.services.audit.mongo.events.VariableUpdatedEventDocument;
+import org.activiti.services.audit.mongo.repository.EventsRepository;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
+import org.springframework.data.rest.core.mapping.RepositoryDetectionStrategy.RepositoryDetectionStrategies;
 import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurerAdapter;
+import org.springframework.data.web.config.EnableSpringDataWebSupport;
 
 @Configuration
-@EnableMongoRepositories(basePackages = "org.activiti.services.audit.mongo")
+@EnableSpringDataWebSupport
+@EnableMongoRepositories(basePackageClasses = EventsRepository.class)
 public class RepositoryConfig extends RepositoryRestConfigurerAdapter {
 
     @Override
     public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config) {
+        config.setBasePath("/v1");
+        
+        // Expose only repositories annotated with @RepositoryRestResource
+        config.setRepositoryDetectionStrategy(RepositoryDetectionStrategies.ANNOTATED);
+        
         config.exposeIdsFor(ActivityCompletedEventDocument.class);
         config.exposeIdsFor(ActivityStartedEventDocument.class);
         config.exposeIdsFor(ProcessCompletedEventDocument.class);
--- activiti-services/activiti-services-audit/activiti-services-audit-mongo/src/main/java/org/activiti/services/audit/mongo/repository/EventsRepository.java
+++ activiti-services/activiti-services-audit/activiti-services-audit-mongo/src/main/java/org/activiti/services/audit/mongo/repository/EventsRepository.java
@@ -3,6 +3,8 @@ package org.activiti.services.audit.mongo.repository;
 import org.activiti.services.audit.mongo.events.ProcessEngineEventDocument;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.data.querydsl.QuerydslPredicateExecutor;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+@RepositoryRestResource(collectionResourceRel = "events", path = "events")
 public interface EventsRepository extends MongoRepository<ProcessEngineEventDocument, String>, QuerydslPredicateExecutor<ProcessEngineEventDocument> {
 }
