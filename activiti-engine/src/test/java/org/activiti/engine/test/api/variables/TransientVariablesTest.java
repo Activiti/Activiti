5c77faa194bbd5e6f622f7a3c126fcdac1877e4a
daisuke-yoshimoto <yoshimoto@intra-mart.jp>
2017-12-20 05:15:27 +0900

    Fix for codacy

--- activiti-engine/src/test/java/org/activiti/engine/test/api/variables/TransientVariablesTest.java
+++ activiti-engine/src/test/java/org/activiti/engine/test/api/variables/TransientVariablesTest.java
@@ -54,7 +54,11 @@ public class TransientVariablesTest extends PluggableActivitiTestCase {
   // Test case for ACT-4304
   @Deployment
   public void testTransientVariableDeletion() {
-    runtimeService.startProcessInstanceByKey("transientVarsTest");
+    String processInstanceId = runtimeService.startProcessInstanceByKey("transientVarsTest").getId();
+    long count = historyService.createHistoricVariableInstanceQuery().processInstanceId(processInstanceId).variableName("response").count();
+    assertEquals(0, count);
+    count = historyService.createHistoricVariableInstanceQuery().processInstanceId(processInstanceId).variableName("status").count();
+    assertEquals(0, count);
   }
   
   @Deployment
