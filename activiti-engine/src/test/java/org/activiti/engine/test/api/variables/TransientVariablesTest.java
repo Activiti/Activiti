0874f669bc5bba8e89ffa0042efd174079087da0
Daisuke Yoshimoto <yoshimoto@intra-mart.jp>
2017-12-22 23:38:47 +0900

    Exclude transient variables when cleaning up execution data - ACT-4304 (#1605)

--- activiti-engine/src/test/java/org/activiti/engine/test/api/variables/TransientVariablesTest.java
+++ activiti-engine/src/test/java/org/activiti/engine/test/api/variables/TransientVariablesTest.java
@@ -51,6 +51,16 @@ public class TransientVariablesTest extends PluggableActivitiTestCase {
     assertNull(runtimeService.getVariable(processInstance.getId(), "response"));
   }
   
+  // Test case for ACT-4304
+  @Deployment
+  public void testTransientVariableDeletion() {
+    String processInstanceId = runtimeService.startProcessInstanceByKey("transientVarsTest").getId();
+    long count = historyService.createHistoricVariableInstanceQuery().processInstanceId(processInstanceId).variableName("response").count();
+    assertEquals(0, count);
+    count = historyService.createHistoricVariableInstanceQuery().processInstanceId(processInstanceId).variableName("status").count();
+    assertEquals(0, count);
+  }
+  
   @Deployment
   public void testUseTransientVariableInExclusiveGateway() {
     ProcessInstance processInstance = runtimeService.startProcessInstanceByKey("transientVarsTest");
