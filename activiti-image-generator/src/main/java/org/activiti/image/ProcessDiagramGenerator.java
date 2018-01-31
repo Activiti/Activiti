b4592fb0b1535aebc2c077d11822e9bb2ad9d82f
Lucian Oprea <lucianoprea@users.noreply.github.com>
2018-01-31 18:34:17 +0200

    Update copyright header #1686 (#1694)
    - since a new year started the copyright year was updated to 2018

--- activiti-image-generator/src/main/java/org/activiti/image/ProcessDiagramGenerator.java
+++ activiti-image-generator/src/main/java/org/activiti/image/ProcessDiagramGenerator.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2017 Alfresco, Inc. and/or its affiliates.
+ * Copyright 2018 Alfresco, Inc. and/or its affiliates.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
