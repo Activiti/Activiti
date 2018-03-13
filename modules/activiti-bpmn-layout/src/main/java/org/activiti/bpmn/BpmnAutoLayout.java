ef8d554669c0c4ab23a8245079f2b22684802fb8
Marcello Teodori <marcello.teodori@alfresco.com>
2018-03-13 14:55:13 +0000

    updated jgraphx to fix CVE-2017-18197 vulnerability #1817

--- modules/activiti-bpmn-layout/src/main/java/org/activiti/bpmn/BpmnAutoLayout.java
+++ modules/activiti-bpmn-layout/src/main/java/org/activiti/bpmn/BpmnAutoLayout.java
@@ -267,7 +267,7 @@ public class BpmnAutoLayout {
     boundaryEdgeStyle.put(mxConstants.STYLE_EXIT_Y, 1.0);
     boundaryEdgeStyle.put(mxConstants.STYLE_ENTRY_X, 0.5);
     boundaryEdgeStyle.put(mxConstants.STYLE_ENTRY_Y, 1.0);
-    boundaryEdgeStyle.put(mxConstants.STYLE_EDGE, mxEdgeStyle.orthConnector);
+    boundaryEdgeStyle.put(mxConstants.STYLE_EDGE, mxEdgeStyle.OrthConnector);
     graph.getStylesheet().putCellStyle(STYLE_BOUNDARY_SEQUENCEFLOW, boundaryEdgeStyle);
     
     for (SequenceFlow sequenceFlow : sequenceFlows.values()) {
@@ -303,7 +303,7 @@ public class BpmnAutoLayout {
     boundaryEdgeStyle.put(mxConstants.STYLE_EXIT_Y, 1.0);
     boundaryEdgeStyle.put(mxConstants.STYLE_ENTRY_X, 0.5);
     boundaryEdgeStyle.put(mxConstants.STYLE_ENTRY_Y, 1.0);
-    boundaryEdgeStyle.put(mxConstants.STYLE_EDGE, mxEdgeStyle.orthConnector);
+    boundaryEdgeStyle.put(mxConstants.STYLE_EDGE, mxEdgeStyle.OrthConnector);
     graph.getStylesheet().putCellStyle(STYLE_BOUNDARY_SEQUENCEFLOW, boundaryEdgeStyle);
 
     for (Association association : associations.values()) {
