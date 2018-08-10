25b1fc4f41cdce178e9e272edae9b0adb3ed5372
Elias Ricken de Medeiros <elias.demedeiros@alfresco.com>
2018-08-10 17:54:18 +0100

    Add get process definition

--- activiti-api-web-example/src/main/java/org/activiti/examples/DemoApplicationConfiguration.java
+++ activiti-api-web-example/src/main/java/org/activiti/examples/DemoApplicationConfiguration.java
@@ -25,7 +25,6 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
