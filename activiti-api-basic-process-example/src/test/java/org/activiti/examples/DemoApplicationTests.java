024d2a539944c112f7bfa8b5059005e884f9d472
salaboy <salaboy@gmail.com>
2018-08-11 09:24:16 +0100

    updating test in process

--- activiti-api-basic-process-example/src/test/java/org/activiti/examples/DemoApplicationTests.java
+++ activiti-api-basic-process-example/src/test/java/org/activiti/examples/DemoApplicationTests.java
@@ -1,16 +1,30 @@
 package org.activiti.examples;
 
+import org.activiti.runtime.api.ProcessRuntime;
+import org.activiti.runtime.api.model.ProcessDefinition;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class DemoApplicationTests {
 
-	@Test
-	public void contextLoads() {
-	}
+    @Autowired
+    private ProcessRuntime processRuntime;
+
+    @Autowired
+    private SecurityUtil securityUtil;
+
+    @Test
+    public void contextLoads() {
+        securityUtil.logInAs("system");
+        ProcessDefinition processDefinition = processRuntime.processDefinition("categorizeProcess");
+        assertThat(processDefinition).isNotNull();
+    }
 
 }
