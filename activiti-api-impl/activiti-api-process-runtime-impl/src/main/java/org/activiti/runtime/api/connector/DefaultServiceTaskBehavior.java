d09dde50ef0cc24de9b214df8a0289e67c33de8b
ffazzini <ffrancesco83@gmail.com>
2018-09-05 15:01:05 +0100

    Added mapping for outbound variables

--- activiti-api-impl/activiti-api-process-runtime-impl/src/main/java/org/activiti/runtime/api/connector/DefaultServiceTaskBehavior.java
+++ activiti-api-impl/activiti-api-process-runtime-impl/src/main/java/org/activiti/runtime/api/connector/DefaultServiceTaskBehavior.java
@@ -23,10 +23,13 @@ import org.activiti.engine.delegate.DelegateExecution;
 import org.activiti.engine.impl.bpmn.behavior.AbstractBpmnActivityBehavior;
 import org.activiti.model.connector.ActionDefinition;
 import org.activiti.model.connector.ConnectorDefinition;
+import org.activiti.model.connector.VariableDefinition;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.context.ApplicationContext;
 
 import java.util.List;
+import java.util.Map;
+import java.util.function.Function;
 import java.util.stream.Collectors;
 
 public class DefaultServiceTaskBehavior extends AbstractBpmnActivityBehavior {
@@ -45,7 +48,6 @@ public class DefaultServiceTaskBehavior extends AbstractBpmnActivityBehavior {
     @Override
     public void execute(DelegateExecution execution) {
 
-        // 1) fetch Connectors Definition based on Implementation String (this means that you need split the string into connectorDefId.actionId)
         String implementation = ((ServiceTask) execution.getCurrentFlowElement()).getImplementation();
         String connectorId = StringUtils.substringBefore(implementation, ".");
         String actionId = StringUtils.substringAfter(implementation, ".");
@@ -54,26 +56,21 @@ public class DefaultServiceTaskBehavior extends AbstractBpmnActivityBehavior {
         if (resultingConnectors.size() != 1) {
             throw new RuntimeException("Mismatch connector id mapping");
         }
-        //this is the connector definition
         ConnectorDefinition connectorDefinition = resultingConnectors.get(0);
 
-        // 2)  you get the action based on the actionId
         ActionDefinition actionDefinition = connectorDefinition.getActions().get(actionId);
         if (actionDefinition == null) {
             throw new RuntimeException("Mismatch action name mapping");
         }
 
-        // 3) Using the action Name:  Connector connector = applicationContext.getBean(actionName,
-        //                                                         Connector.class);
         Connector connector = applicationContext.getBean(actionDefinition.getName(), Connector.class);
 
-        // 4) IntegrationContext context = integrationContextBuilder.from(execution, connectorDef);
         IntegrationContext context = integrationContextBuilder.from(execution, actionDefinition);
         IntegrationContext results = connector.execute(context);
 
-        // Check the output mappings same thing as input
-        execution.setVariables(results.getOutBoundVariables());
-
+        Map<String, Object> outBoundVariables = actionDefinition.getOutput().stream().filter(output -> results.getOutBoundVariables().containsKey(output.getName())).collect(Collectors.toMap(VariableDefinition::getName,
+                Function.identity()));
+        execution.setVariables(outBoundVariables);
 
         leave(execution);
     }
