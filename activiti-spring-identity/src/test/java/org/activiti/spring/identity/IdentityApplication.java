07eb0e72f49bc5249f6d15b9843e90e233226c60
Daniel <daniel.pancu@ness.com>
2018-09-11 12:39:01 +0300

    Added extended functionality for UserDetailsManager

--- activiti-spring-identity/src/test/java/org/activiti/spring/identity/IdentityApplication.java
+++ activiti-spring-identity/src/test/java/org/activiti/spring/identity/IdentityApplication.java
@@ -10,7 +10,6 @@ import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 @SpringBootApplication
 public class IdentityApplication {
@@ -21,19 +20,18 @@ public class IdentityApplication {
 
     @Bean
     public UserDetailsService myUserDetailsService() {
-        InMemoryUserDetailsManager inMemoryUserDetailsManager = new InMemoryUserDetailsManager();
+        ExtendedInMemoryUserDetailsManager extendedInMemoryUserDetailsManager = new ExtendedInMemoryUserDetailsManager();
 
         List<GrantedAuthority> userAuthorities = new ArrayList<>();
         userAuthorities.add(new SimpleGrantedAuthority("ROLE_ACTIVITI_USER"));
 
-        inMemoryUserDetailsManager.createUser(new User("user", "password", userAuthorities));
+        extendedInMemoryUserDetailsManager.createUser(new User("user", "password", userAuthorities));
 
         List<GrantedAuthority> adminAuthorities = new ArrayList<>();
         adminAuthorities.add(new SimpleGrantedAuthority("ROLE_ACTIVITI_ADMIN"));
 
-        inMemoryUserDetailsManager.createUser(new User("admin", "password", adminAuthorities));
+        extendedInMemoryUserDetailsManager.createUser(new User("admin", "password", adminAuthorities));
 
-        return inMemoryUserDetailsManager;
+        return extendedInMemoryUserDetailsManager;
     }
-
 }
