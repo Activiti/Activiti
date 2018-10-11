6396fc08054b2ea09ed5c967bc64193a036213bd
Elias Ricken de Medeiros <elias.demedeiros@alfresco.com>
2018-10-11 07:59:38 +0100

    Remove unused imports

--- activiti-spring-boot-starter/src/test/java/org/activiti/spring/boot/process/ProcessRuntimeConnectorTest.java
+++ activiti-spring-boot-starter/src/test/java/org/activiti/spring/boot/process/ProcessRuntimeConnectorTest.java
@@ -6,13 +6,10 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.test.context.support.WithUserDetails;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit4.SpringRunner;
-
-import java.io.IOException;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.NONE)
@@ -31,20 +28,18 @@ public class ProcessRuntimeConnectorTest {
      * It tests two connector actions inside the xml against two different connector json definitions:
      * the first input variable is defined in the first connector action,
      * the second input variable in the second connector action.
-     *
      **/
     @Test
     @WithUserDetails(value = "salaboy", userDetailsServiceBeanName = "myUserDetailsService")
     public void shouldConnectorMatchWithConnectorDefinition() {
         processRuntime.start(ProcessPayloadBuilder.start()
-                .withProcessDefinitionKey(CATEGORIZE_IMAGE_CONNECTORS_PROCESS)
-                .withVariable("input-variable-name-1",
-                        "input-variable-name-1")
-                .withVariable("input-variable-name-2",
-                        "input-variable-name-2")
-                .withVariable("expectedKey",
-                        true)
-                .build());
+                                     .withProcessDefinitionKey(CATEGORIZE_IMAGE_CONNECTORS_PROCESS)
+                                     .withVariable("input-variable-name-1",
+                                                   "input-variable-name-1")
+                                     .withVariable("input-variable-name-2",
+                                                   "input-variable-name-2")
+                                     .withVariable("expectedKey",
+                                                   true)
+                                     .build());
     }
-
 }
--- activiti-spring-boot-starter/src/test/java/org/activiti/spring/boot/process/ProcessRuntimeEmptyConnectorDefinitionTest.java
+++ activiti-spring-boot-starter/src/test/java/org/activiti/spring/boot/process/ProcessRuntimeEmptyConnectorDefinitionTest.java
@@ -6,13 +6,10 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.test.context.support.WithUserDetails;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit4.SpringRunner;
-
-import java.io.IOException;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.NONE)
@@ -30,16 +27,14 @@ public class ProcessRuntimeEmptyConnectorDefinitionTest {
     /**
      * This test points to a directory having no connectors definitions.
      * As resulting behaviour, we have the same when there is no match with connector definitions.
-     *
      **/
     @Test
     @WithUserDetails(value = "salaboy", userDetailsServiceBeanName = "myUserDetailsService")
     public void connectorDefinitionEmptyDir() {
         processRuntime.start(ProcessPayloadBuilder.start()
-                .withProcessDefinitionKey(CATEGORIZE_PROCESS)
-                .withVariable("expectedKey",
-                        true)
-                .build());
+                                     .withProcessDefinitionKey(CATEGORIZE_PROCESS)
+                                     .withVariable("expectedKey",
+                                                   true)
+                                     .build());
     }
-
 }
