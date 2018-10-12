4748bc180e9037ace50dc5a4204f4c9221023afe
ryandawsonuk <ryandawson@cantab.net>
2018-10-12 10:52:01 +0100

    moving modules from activiti/activiti so it these dont have to increment with engine changes

---
+++ activiti-api-process-model-impl/src/main/java/org/activiti/api/runtime/event/impl/BPMNActivityCompletedEventImpl.java
@@ -0,0 +1,19 @@
+package org.activiti.api.runtime.event.impl;
+
+import org.activiti.api.process.model.BPMNActivity;
+import org.activiti.api.process.model.events.BPMNActivityCompletedEvent;
+
+public class BPMNActivityCompletedEventImpl extends BPMNActivityEventImpl implements BPMNActivityCompletedEvent {
+
+    public BPMNActivityCompletedEventImpl() {
+    }
+
+    public BPMNActivityCompletedEventImpl(BPMNActivity entity) {
+        super(entity);
+    }
+
+    @Override
+    public ActivityEvents getEventType() {
+        return ActivityEvents.ACTIVITY_COMPLETED;
+    }
+}
