104dc0020f50ddb82f0a131e8db4bb48e86b27b8
Elias Ricken de Medeiros <elias.demedeiros@alfresco.com>
2018-10-19 12:30:38 +0100

    Import processes from application zip file

---
+++ activiti-spring-application/src/main/java/org/activiti/application/ApplicationContent.java
@@ -0,0 +1,39 @@
+/*
+ * Copyright 2018 Alfresco, Inc. and/or its affiliates.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.activiti.application;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class ApplicationContent {
+
+    private Map<String, List<FileContent>> entries = new HashMap<>();
+
+    public void add(ApplicationEntry entry) {
+        List<FileContent> fileContents = entries.computeIfAbsent(entry.getType(),
+                                                                 k -> new ArrayList<>());
+        fileContents.add(entry.getFileContent());
+    }
+
+    public List<FileContent> getFileContents(String entryType) {
+        return entries.getOrDefault(entryType, Collections.emptyList());
+    }
+
+}
---
+++ activiti-spring-application/src/test/java/org/activiti/application/ApplicationLoaderTest.java
@@ -0,0 +1,67 @@
+/*
+ * Copyright 2018 Alfresco, Inc. and/or its affiliates.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.activiti.application;
+
+import java.io.InputStream;
+import java.util.Collections;
+import java.util.List;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.springframework.core.io.Resource;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.mock;
+import static org.mockito.MockitoAnnotations.initMocks;
+
+public class ApplicationLoaderTest {
+
+    @InjectMocks
+    private ApplicationLoader loader;
+
+    @Mock
+    private ApplicationDiscovery applicationDiscovery;
+
+    @Mock
+    private ApplicationReader applicationReader;
+
+    @Before
+    public void setUp() {
+        initMocks(this);
+    }
+
+    @Test
+    public void shouldLoadApplications() throws Exception {
+        //given
+        Resource applicationResource = mock(Resource.class);
+        given(applicationResource.getInputStream()).willReturn(mock(InputStream.class));
+
+        given(applicationDiscovery.discoverApplications()).willReturn(Collections.singletonList(applicationResource));
+
+        ApplicationContent applicationContent = new ApplicationContent();
+        given(applicationReader.read(applicationResource.getInputStream())).willReturn(applicationContent);
+
+        //when
+        List<ApplicationContent> applicationContents = loader.loadApplications();
+
+        //then
+        assertThat(applicationContents).containsExactly(applicationContent);
+    }
+}
