ee1c0f65d301886f05bc291b1d7afa541388142f
Ryan Dawson <ryandawson@cantab.net>
2018-10-25 20:48:30 +0100

    not a docker repo (#8)
    not a docker repo

--- watch.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-# watch the java files and continously deploy the service
-mvn clean install
-skaffold run -p dev
-reflex -r "\.java$" -- bash -c 'mvn install && skaffold run -p dev'
