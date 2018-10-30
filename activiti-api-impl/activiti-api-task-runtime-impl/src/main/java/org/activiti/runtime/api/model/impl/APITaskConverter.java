c01698281ffb1c575ebfeb57ad5f1047587b1d03
Bassam Al-Sarori <balsarori@gmail.com>
2018-10-30 12:17:05 +0000

    Add form key to process definition and task models

--- activiti-api-impl/activiti-api-task-runtime-impl/src/main/java/org/activiti/runtime/api/model/impl/APITaskConverter.java
+++ activiti-api-impl/activiti-api-task-runtime-impl/src/main/java/org/activiti/runtime/api/model/impl/APITaskConverter.java
@@ -37,6 +37,7 @@ public class APITaskConverter extends ListConverter<org.activiti.engine.task.Tas
         task.setOwner(internalTask.getOwner());
         task.setParentTaskId(internalTask.getParentTaskId());
         task.setPriority(internalTask.getPriority());
+        task.setFormKey(internalTask.getFormKey());
         return task;
     }
 
