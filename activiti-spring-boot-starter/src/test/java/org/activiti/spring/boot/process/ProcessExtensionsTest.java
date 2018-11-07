30da0f40dd4b47ccdd67aabbd19aaf8d370c6034
ryandawsonuk <ryandawson@cantab.net>
2018-11-07 11:44:02 +0000

    check types of variables on proc start

--- activiti-spring-boot-starter/src/test/java/org/activiti/spring/boot/process/ProcessExtensionsTest.java
+++ activiti-spring-boot-starter/src/test/java/org/activiti/spring/boot/process/ProcessExtensionsTest.java
@@ -9,6 +9,7 @@ import org.activiti.engine.ActivitiException;
 import org.activiti.spring.boot.security.util.SecurityUtil;
 import org.activiti.spring.boot.test.util.ProcessCleanUpUtil;
 import org.junit.After;
+import org.activiti.spring.process.ExtensionVariableTypes;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -17,6 +18,7 @@ import org.springframework.security.test.context.support.WithUserDetails;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import java.text.ParseException;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -77,6 +79,46 @@ public class ProcessExtensionsTest {
         processRuntime.delete(ProcessPayloadBuilder.delete(initialVarsProcess));
     }
 
+
+    @Test
+    public void processInstanceHasValidInitialVariables() throws ParseException {
+
+
+        securityUtil.logInAs("salaboy");
+        ProcessRuntimeConfiguration configuration = processRuntime.configuration();
+        assertThat(configuration).isNotNull();
+
+        // start a process with vars then check default and specified vars exist
+        ProcessInstance initialVarsProcess = processRuntime.start(ProcessPayloadBuilder.start()
+                .withProcessDefinitionKey(INITIAL_VARS_PROCESS)
+                .withVariable("extraVar",
+                        true)
+                .withVariable("age",
+                        10)
+                .withVariable("name",
+                        "bob")
+                .withVariable("subscribe",
+                        true)
+                .withVariable("birth", ExtensionVariableTypes.DATE.getDateFormat().parse("2009-11-30"))
+                .withBusinessKey("my business key")
+                .build());
+
+        assertThat(initialVarsProcess).isNotNull();
+        assertThat(initialVarsProcess.getStatus()).isEqualTo(ProcessInstance.ProcessInstanceStatus.RUNNING);
+
+        List<VariableInstance> variableInstances = processRuntime.variables(ProcessPayloadBuilder.variables().withProcessInstance(initialVarsProcess).build());
+
+        assertThat(variableInstances).isNotNull();
+        assertThat(variableInstances).hasSize(5);
+
+        assertThat(variableInstances).extracting("name")
+                .contains("extraVar", "name", "age", "birth","subscribe");
+
+        // cleanup
+        processRuntime.delete(ProcessPayloadBuilder.delete(initialVarsProcess));
+    }
+
+
     @Test
     public void processInstanceFailsWithoutRequiredVariables() {
         securityUtil.logInAs("salaboy");
@@ -90,5 +132,23 @@ public class ProcessExtensionsTest {
                             true)
                     .build());
         }).withMessage("Can't start process '" + INITIAL_VARS_PROCESS + "' without required variables age");
+    }
+
+    @Test
+    public void processInstanceFailsIfVariableTypeIncorrect() {
+
+        securityUtil.logInAs("salaboy");
+        ProcessRuntimeConfiguration configuration = processRuntime.configuration();
+        assertThat(configuration).isNotNull();
+
+        assertThatExceptionOfType(ActivitiException.class).isThrownBy(() -> {
+            processRuntime.start(ProcessPayloadBuilder.start()
+                    .withProcessDefinitionKey(INITIAL_VARS_PROCESS)
+                    .withVariable("age", true)
+                    .withVariable("name",7)
+                    .withVariable("subscribe","ok")
+                    .withVariable("birth","thisisnotadate")
+                    .build());
+        }).withMessage("Can't start process '" + INITIAL_VARS_PROCESS + "' as variables have unexpected types subscribe, name, birth, age");
     }
 }
