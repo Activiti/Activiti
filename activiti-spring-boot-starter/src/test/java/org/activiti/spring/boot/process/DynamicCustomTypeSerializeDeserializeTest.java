f9d65b8135402d5bfbb75fbce6f0012a933b03a2
ryandawsonuk <ryandawson@cantab.net>
2018-11-12 11:38:02 +0000

    poc with custom classes that don't have any annotation

--- activiti-spring-boot-starter/src/test/java/org/activiti/spring/boot/process/DynamicCustomTypeSerializeDeserializeTest.java
+++ activiti-spring-boot-starter/src/test/java/org/activiti/spring/boot/process/DynamicCustomTypeSerializeDeserializeTest.java
@@ -1,8 +1,10 @@
 package org.activiti.spring.boot.process;
 
 import com.fasterxml.jackson.annotation.JsonTypeInfo;
+import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.ObjectNode;
 import org.junit.Test;
 
 import java.io.IOException;
@@ -14,14 +16,16 @@ public class DynamicCustomTypeSerializeDeserializeTest {
 
     private ObjectMapper objectMapper = new ObjectMapper();
 
-    private CustomTypeAnnotated customObj = new CustomTypeAnnotated();
+    private CustomTypeAnnotated customTypeAnnotated = new CustomTypeAnnotated();
+
+    private CustomType customType = new CustomType();
 
     @Test
     public void serializeAndDeserializeCustomTypeWithAnnotation() throws IOException {
 
         assertThat(objectMapper.canSerialize(CustomTypeAnnotated.class)).isTrue();
 
-        final String json = objectMapper.writeValueAsString(customObj);
+        final String json = objectMapper.writeValueAsString(customTypeAnnotated);
 
         final JsonNode jsonNode = objectMapper.readTree(json);
 
@@ -38,6 +42,34 @@ public class DynamicCustomTypeSerializeDeserializeTest {
         final Object o = objectMapper.convertValue(jsonNode, cls);
 
         assertThat(o.getClass()).isEqualTo(CustomTypeAnnotated.class);
+    }
+
+
+    @Test
+    public void serializeAndDeserializeCustomTypeWithoutAnnotation() throws IOException {
+
+        assertThat(objectMapper.canSerialize(CustomType.class)).isTrue();
+
+        final String json = objectMapper.writeValueAsString(customTypeAnnotated);
+
+        ObjectNode jsonNode = (ObjectNode) objectMapper.readTree(json);
+
+        jsonNode.put("type",customType.getClass().getTypeName());
+
+        final String type = jsonNode.get("type").asText();
+        Class<?> cls = null;
+        try {
+            cls = Class.forName(type,false,this.getClass().getClassLoader());
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+
+        //now deserialize the object but it doesn't have a `type` field
+        // or an annotation telling jackson to ignore a type field so we need to tell jackson that's ok
+        objectMapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+        final Object o = objectMapper.convertValue(jsonNode, cls);
+
+        assertThat(o.getClass()).isEqualTo(CustomType.class);
     }
 
     @Test
