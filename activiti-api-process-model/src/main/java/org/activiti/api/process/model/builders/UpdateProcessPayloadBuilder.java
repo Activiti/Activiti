6ad9db140772ab62c83757e145b770d66e7b91dd
CTI777 <illia.goncharov@googlemail.com>
2018-11-26 21:11:13 +0100

    fix: processInstanceId added to UpdateProcessPayload

--- activiti-api-process-model/src/main/java/org/activiti/api/process/model/builders/UpdateProcessPayloadBuilder.java
+++ activiti-api-process-model/src/main/java/org/activiti/api/process/model/builders/UpdateProcessPayloadBuilder.java
@@ -1,9 +1,11 @@
 package org.activiti.api.process.model.builders;
 
+import org.activiti.api.process.model.ProcessInstance;
 import org.activiti.api.process.model.payloads.UpdateProcessPayload;
 
 public class UpdateProcessPayloadBuilder {
 
+    private String processInstanceId;
     private String processInstanceName;
     private String processInstanceDescription;
     private String businessKey;
@@ -23,8 +25,22 @@ public class UpdateProcessPayloadBuilder {
         return this;
     }
 
+    public UpdateProcessPayloadBuilder withProcessInstanceId(String processInstanceId) {
+        this.processInstanceId = processInstanceId;
+        return this;
+    }
+    
+    public UpdateProcessPayloadBuilder withProcessInstance(ProcessInstance processInstance) {
+        this.processInstanceId = processInstance.getId();
+        this.businessKey = processInstance.getBusinessKey();
+        this.processInstanceName = processInstance.getName();
+        return this;
+    } 
+
+
     public UpdateProcessPayload build() {
-        return new UpdateProcessPayload(processInstanceName,
+        return new UpdateProcessPayload(processInstanceId,
+                processInstanceName,
                 processInstanceDescription,
                 businessKey);
     }
