23f744e06a02f3b9ffad0dff85a42b7bcefbfdfb
CTI777 <illia.goncharov@googlemail.com>
2018-12-04 22:08:14 +0100

    fix: getInitializedProcessRuntimeEventListeners

--- activiti-api-impl/activiti-api-process-runtime-impl/src/main/java/org/conf/activiti/runtime/api/ProcessRuntimeAutoConfiguration.java
+++ activiti-api-impl/activiti-api-process-runtime-impl/src/main/java/org/conf/activiti/runtime/api/ProcessRuntimeAutoConfiguration.java
@@ -95,7 +95,7 @@ public class ProcessRuntimeAutoConfiguration {
                 processInstanceConverter,
                 variableInstanceConverter,
                 processRuntimeConfiguration,
-                listeners);
+                getInitializedProcessRuntimeEventListeners(listeners));
     }
 
     @Bean
@@ -268,5 +268,9 @@ public class ProcessRuntimeAutoConfiguration {
         return () -> runtimeService.addEventListener(new SequenceFlowTakenListenerDelegate(getInitializedListeners(eventListeners),
                         new ToSequenceFlowTakenConverter()),
                 ActivitiEventType.SEQUENCEFLOW_TAKEN);
+    }
+    
+    private <T> List<T> getInitializedProcessRuntimeEventListeners(List<T> processRuntimeEventListeners) {
+        return processRuntimeEventListeners != null ? processRuntimeEventListeners : Collections.emptyList();
     }
 }
