947d1d6944391c265554071d6a5a9f088d8c0e55
CTI777 <illia.goncharov@googlemail.com>
2018-12-17 17:02:45 +0100

    feat: a test added for assign method

--- activiti-spring-boot-starter/src/test/java/org/activiti/spring/boot/tasks/TaskRuntimeTaskAssigneeTest.java
+++ activiti-spring-boot-starter/src/test/java/org/activiti/spring/boot/tasks/TaskRuntimeTaskAssigneeTest.java
@@ -1,5 +1,7 @@
 package org.activiti.spring.boot.tasks;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 import org.activiti.api.runtime.shared.query.Page;
 import org.activiti.api.runtime.shared.query.Pageable;
 import org.activiti.api.task.model.Task;
@@ -14,9 +16,6 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
-
-
-import static org.assertj.core.api.Assertions.assertThat;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.NONE)
@@ -88,7 +87,7 @@ public class TaskRuntimeTaskAssigneeTest {
     }
 
 
-        @Test
+    @Test
     public void createStandaloneTaskForGroupAndClaim() {
 
         securityUtil.logInAs("garth");
@@ -111,6 +110,52 @@ public class TaskRuntimeTaskAssigneeTest {
         Task claimedTask = taskRuntime.claim(TaskPayloadBuilder.claim().withTaskId(task.getId()).build());
         assertThat(claimedTask.getAssignee()).isEqualTo("garth");
         assertThat(claimedTask.getStatus()).isEqualTo(Task.TaskStatus.ASSIGNED);
+    }
+    
+    @Test
+    public void createStandaloneTaskForGroupAndAdminAssignUser() {
+
+        securityUtil.logInAs("garth");
+
+        Task standAloneTask = taskRuntime.create(TaskPayloadBuilder.create()
+                .withName("group task")
+                .withCandidateGroup("doctor")
+                .build());
+
+        // the owner should be able to see the created task
+        Page<Task> tasks = taskRuntime.tasks(Pageable.of(0,
+                50));
+
+        assertThat(tasks.getContent()).hasSize(1);
+        
+        Task task = tasks.getContent().get(0);
+        assertThat(task.getAssignee()).isNull();
+        assertThat(task.getStatus()).isEqualTo(Task.TaskStatus.CREATED);
+        
+        
+        securityUtil.logInAs("admin");
+        Task assignedTask = taskAdminRuntime.assign(TaskPayloadBuilder
+                                                  .assign()
+                                                  .withTaskId(task.getId())
+                                                  .withAssignee("garth")
+                                                  .build());
+        assertThat(assignedTask.getAssignee()).isEqualTo("garth");
+        assertThat(assignedTask.getStatus()).isEqualTo(Task.TaskStatus.ASSIGNED);
+        
+        securityUtil.logInAs("garth");
+        tasks = taskRuntime.tasks(Pageable.of(0,
+                                              50));
+
+        assertThat(tasks.getContent()).hasSize(1);
+        task = tasks.getContent().get(0);
+    
+        assertThat(task.getAssignee()).isEqualTo("garth");
+        
+        Task deletedTask = taskRuntime.delete(TaskPayloadBuilder
+                                              .delete()
+                                              .withTaskId(task.getId())
+                                              .withReason("test clean up")
+                                              .build());
     }
 
 
