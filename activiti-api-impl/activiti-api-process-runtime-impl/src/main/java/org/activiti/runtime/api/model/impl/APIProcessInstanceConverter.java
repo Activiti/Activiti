f3543483fbb600e1083f42b138d2c000c226ef2e
Igor Dianov <igor@dianov.com>
2018-12-17 19:46:39 -0800

    fix: add Mybatis superExecution parent entity mapping
    Enable eager fetching of execution's superExecution entity via Mybatis association property mapping

--- activiti-api-impl/activiti-api-process-runtime-impl/src/main/java/org/activiti/runtime/api/model/impl/APIProcessInstanceConverter.java
+++ activiti-api-impl/activiti-api-process-runtime-impl/src/main/java/org/activiti/runtime/api/model/impl/APIProcessInstanceConverter.java
@@ -20,11 +20,6 @@ import java.util.Optional;
 
 import org.activiti.api.process.model.ProcessInstance;
 import org.activiti.api.runtime.model.impl.ProcessInstanceImpl;
-import org.activiti.engine.ProcessEngines;
-import org.activiti.engine.impl.cfg.ProcessEngineConfigurationImpl;
-import org.activiti.engine.impl.context.Context;
-import org.activiti.engine.impl.interceptor.Command;
-import org.activiti.engine.impl.interceptor.CommandContext;
 import org.activiti.engine.impl.persistence.entity.ExecutionEntity;
 
 public class APIProcessInstanceConverter extends ListConverter<org.activiti.engine.runtime.ProcessInstance, ProcessInstance>
@@ -47,27 +42,11 @@ public class APIProcessInstanceConverter extends ListConverter<org.activiti.engi
         //To do: it is not the best way to search parentProcessId by this method, it will require extra queries!
         
         //Set parent ProcessInstance Id
-        if(internalProcessInstance.getSuperExecutionId()!=null && ExecutionEntity.class.isInstance(internalProcessInstance)) {
+        if(internalProcessInstance.getSuperExecutionId()!=null) {
             ExecutionEntity executionEntity = ExecutionEntity.class.cast(internalProcessInstance);
-            
-            if (Context.getCommandContext()==null) {
-                  ProcessEngineConfigurationImpl impl = (ProcessEngineConfigurationImpl)
-                        ProcessEngines.getDefaultProcessEngine().getProcessEngineConfiguration();
-
-                        impl.getCommandExecutor().execute(new Command<Void> () {
-
-                        @Override
-                        public Void execute(CommandContext commandContext) {
-                            Optional.ofNullable(executionEntity.getSuperExecution())
-                                    .ifPresent(superExecution -> processInstance.setParentId(superExecution.getProcessInstanceId()));
-
-                             return null;
-                        }});
-
-            } else {
-                  Optional.ofNullable(executionEntity.getSuperExecution())
-                          .ifPresent(superExecution -> processInstance.setParentId(superExecution.getProcessInstanceId()));
-            }
+           
+            Optional.ofNullable(executionEntity.getSuperExecution())
+                  .ifPresent(superExecution -> processInstance.setParentId(superExecution.getProcessInstanceId()));
         }
         return processInstance;
     }
