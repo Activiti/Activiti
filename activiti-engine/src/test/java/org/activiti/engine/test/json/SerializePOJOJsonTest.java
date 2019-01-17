b30c9021bb5008fca07c2cff3b348b8dfe9ce4b5
daisuke-yoshimoto <yoshimoto@intra-mart.jp>
2019-01-17 16:57:10 +0900

    Reproduction case #2184

---
+++ activiti-engine/src/test/java/org/activiti/engine/test/json/SerializePOJOJsonTest.java
@@ -0,0 +1,62 @@
+package org.activiti.engine.test.json;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.function.Consumer;
+
+import org.activiti.engine.history.HistoricTaskInstance;
+import org.activiti.engine.impl.test.ResourceActivitiTestCase;
+import org.activiti.engine.runtime.ProcessInstance;
+import org.activiti.engine.task.IdentityLink;
+import org.activiti.engine.test.Deployment;
+
+public class SerializePOJOJsonTest extends ResourceActivitiTestCase {
+
+    public SerializePOJOJsonTest() {
+        super("org/activiti/standalone/cfg/variable/custom-serialize-variables-activiti.cfg.xml");
+    }
+
+    @Deployment
+    public void testJsonVarInExpression() throws Exception {
+        Map<String, Object> vars = new HashMap<String, Object>();
+        Map<String, Object> map = new HashMap<String, Object>();
+        map.put("assignee", "salaboy");
+        map.put("category", "test");
+        Map<String, Object> mapInMap = new HashMap<String, Object>();
+        mapInMap.put("user", "salaboy");
+        map.put("mapInMap", mapInMap);
+        vars.put("userMap", map);
+        List<String> list = Arrays.asList("salaboy", "salaboy", "salaboy");
+        vars.put("userCollection", list);
+        ProcessInstance processInstance = runtimeService.startProcessInstanceByKey("testJsonVarInExpression", vars);
+        String taskId = taskService.createTaskQuery().processInstanceId(processInstance.getId()).singleResult().getId();
+        taskService.complete(taskId);
+        taskId = taskService.createTaskQuery().processInstanceId(processInstance.getId()).singleResult().getId();
+        taskService.getIdentityLinksForTask(taskId).stream().forEach(new Consumer<IdentityLink>() {
+            @Override
+            public void accept(IdentityLink i) {
+                if ("candidate".equals(i.getType()) ) {
+                	assertEquals("salaboy", i.getUserId());
+                }
+            }
+        });
+        taskService.complete(taskId);
+        HistoricTaskInstance task = historyService.createHistoricTaskInstanceQuery().taskId(taskId).singleResult();
+        assertEquals("salaboy", task.getAssignee());
+        assertEquals("test", task.getCategory());
+    }
+
+    @Deployment
+    public void testCollectionJsonVarInExpression() throws Exception {
+        Map<String, Object> vars = new HashMap<String, Object>();
+        List<String> list = Arrays.asList("salaboy", "salaboy", "salaboy");
+        vars.put("userCollection", list);
+        ProcessInstance processInstance = runtimeService.startProcessInstanceByKey("testCollectionJsonVarInExpression", vars);
+        String taskId = taskService.createTaskQuery().processInstanceId(processInstance.getId()).singleResult().getId();
+        taskService.complete(taskId);
+        taskId = taskService.createTaskQuery().processInstanceId(processInstance.getId()).singleResult().getId();
+        taskService.complete(taskId);
+    }
+}
