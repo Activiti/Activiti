4672d105e6f30b6a972d3c653d6068fa4900ff8d
yanweihan <1327070400@qq.com>
2019-01-26 16:17:08 +0800

    测试

--- activiti-spring-conformance-tests/activiti-spring-conformance-set5/src/test/java/org/activiti/spring/conformance/set5/ConformanceBasicProcessRuntimeTest.java
+++ activiti-spring-conformance-tests/activiti-spring-conformance-set5/src/test/java/org/activiti/spring/conformance/set5/ConformanceBasicProcessRuntimeTest.java
@@ -1,19 +1,18 @@
 package org.activiti.spring.conformance.set5;
 
 import org.activiti.api.process.model.ProcessDefinition;
-import org.activiti.api.process.model.ProcessDefinitionMeta;
 import org.activiti.api.process.runtime.ProcessRuntime;
 import org.activiti.api.process.runtime.conf.ProcessRuntimeConfiguration;
 import org.activiti.api.process.runtime.events.listener.ProcessRuntimeEventListener;
 import org.activiti.api.runtime.shared.events.VariableEventListener;
 import org.activiti.api.runtime.shared.query.Page;
 import org.activiti.api.runtime.shared.query.Pageable;
+import org.activiti.spring.conformance.util.security.SecurityUtil;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
-import org.activiti.spring.conformance.util.security.SecurityUtil;
 
 import java.util.List;
 
@@ -68,7 +67,7 @@ public class ConformanceBasicProcessRuntimeTest {
         securityUtil.logInAs("user1");
 
         Page<ProcessDefinition> processDefinitionPage = processRuntime.processDefinitions(Pageable.of(0, 50));
-
+        //测试
         List<ProcessDefinition> processDefinitions = processDefinitionPage.getContent();
         assertThat(processDefinitions).extracting(ProcessDefinition::getName).contains(
                 "Basic Call Activity",
@@ -80,8 +79,6 @@ public class ConformanceBasicProcessRuntimeTest {
 
 
     }
-
-
 
 
 }
