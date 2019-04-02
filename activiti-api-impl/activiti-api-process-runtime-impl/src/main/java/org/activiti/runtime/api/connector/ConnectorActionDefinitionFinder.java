280651295df7fd8a2d74979934c987d0671e41b4
CTI777 <illia.goncharov@googlemail.com>
2019-04-02 11:53:42 +0200

    Implementation support for the format: connectorName.actionName

--- activiti-api-impl/activiti-api-process-runtime-impl/src/main/java/org/activiti/runtime/api/connector/ConnectorActionDefinitionFinder.java
+++ activiti-api-impl/activiti-api-process-runtime-impl/src/main/java/org/activiti/runtime/api/connector/ConnectorActionDefinitionFinder.java
@@ -1,6 +1,7 @@
 package org.activiti.runtime.api.connector;
 
 import java.util.List;
+import java.util.Map;
 import java.util.Optional;
 import java.util.stream.Collectors;
 
@@ -20,28 +21,51 @@ public class ConnectorActionDefinitionFinder {
 
         Optional<ActionDefinition> actionDefinitionOptional = Optional.empty();
 
-        String connectorId = StringUtils.substringBefore(implementation,
+        String connectorName = StringUtils.substringBefore(implementation,
                                                          ".");
-        String actionId = StringUtils.substringAfter(implementation,
+        String actionName = StringUtils.substringAfter(implementation,
                                                      ".");
 
-        List<ConnectorDefinition> resultingConnectors = connectorDefinitions.stream().filter(c -> c.getId().equals(connectorId)).collect(Collectors.toList());
+        List<ConnectorDefinition> resultingConnectors = connectorDefinitions.stream()
+        										        .filter(c -> connectorName.equals(c.getName()))
+        										        .collect(Collectors.toList());
+        
         if (resultingConnectors != null && resultingConnectors.size() != 0) {
             if (resultingConnectors.size() != 1) {
-                throw new RuntimeException("Expecting exactly 1 connector definition with id mapping `" + connectorId +
-                                                   "`, but were found " + resultingConnectors.size());
+                throw new RuntimeException("Expecting exactly 1 connector definition with name mapping `" + connectorName +
+                                           "`, but were found " + resultingConnectors.size());
             }
-            ConnectorDefinition connectorDefinition = resultingConnectors.get(0);
-
-            ActionDefinition actionDefinition = connectorDefinition.getActions().get(actionId);
+        
+            ActionDefinition actionDefinition = null;
+            List<ActionDefinition> actionDefinitions = filterByName(resultingConnectors.get(0).getActions(), actionName);
+            
+            if (actionDefinitions != null) {
+            	 if (actionDefinitions.size() != 1) {
+                     throw new RuntimeException("Expecting exactly 1 action definition with name mapping `" + actionName +
+                                                "`, but were found " + actionDefinitions.size());
+                 }
+            	    
+            	 actionDefinition = actionDefinitions.get(0);
+            }
+            
             if (actionDefinition == null) {
-                throw new RuntimeException("No action with id mapping `" + actionId + "` was found in connector `" +
-                                                   connectorId + "`");
+                throw new RuntimeException("No action with name mapping `" + actionName + "` was found in connector `" +
+                		                   connectorName + "`");
             }
 
             actionDefinitionOptional = Optional.of(actionDefinition);
         }
 
         return actionDefinitionOptional;
+    }
+    
+    public static List<ActionDefinition> filterByName(Map<String, ActionDefinition> actionDefinitions, 
+    		                                                 String actionName) {
+    	if (actionDefinitions == null || actionName == null) return null;
+        return actionDefinitions.entrySet()
+        		.stream()
+                .filter(a -> actionName.equals(a.getValue().getName()))
+                .map(a -> a.getValue())
+                .collect(Collectors.toList());
     }
 }
