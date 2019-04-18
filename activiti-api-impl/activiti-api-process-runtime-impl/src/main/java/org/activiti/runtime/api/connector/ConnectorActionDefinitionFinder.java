4aac73d97ddcf568448faa67302477d0ca5101df
Elias Ricken de Medeiros <elias.demedeiros@alfresco.com>
2019-04-18 11:01:04 +0100

    Fix indentation

--- activiti-api-impl/activiti-api-process-runtime-impl/src/main/java/org/activiti/runtime/api/connector/ConnectorActionDefinitionFinder.java
+++ activiti-api-impl/activiti-api-process-runtime-impl/src/main/java/org/activiti/runtime/api/connector/ConnectorActionDefinitionFinder.java
@@ -17,40 +17,41 @@ public class ConnectorActionDefinitionFinder {
         this.connectorDefinitions = connectorDefinitions;
     }
 
-    public Optional<ActionDefinition> find(String implementation){
+    public Optional<ActionDefinition> find(String implementation) {
 
         Optional<ActionDefinition> actionDefinitionOptional = Optional.empty();
 
         String connectorName = StringUtils.substringBefore(implementation,
-                                                         ".");
+                                                           ".");
         String actionName = StringUtils.substringAfter(implementation,
-                                                     ".");
+                                                       ".");
 
         List<ConnectorDefinition> resultingConnectors = connectorDefinitions.stream()
-        										        .filter(c -> connectorName.equals(c.getName()))
-        										        .collect(Collectors.toList());
-        
+                .filter(c -> connectorName.equals(c.getName()))
+                .collect(Collectors.toList());
+
         if (resultingConnectors != null && resultingConnectors.size() != 0) {
             if (resultingConnectors.size() != 1) {
                 throw new RuntimeException("Expecting exactly 1 connector definition with name mapping `" + connectorName +
-                                           "`, but were found " + resultingConnectors.size());
+                                                   "`, but were found " + resultingConnectors.size());
             }
-        
+
             ActionDefinition actionDefinition = null;
-            List<ActionDefinition> actionDefinitions = filterByName(resultingConnectors.get(0).getActions(), actionName);
-            
+            List<ActionDefinition> actionDefinitions = filterByName(resultingConnectors.get(0).getActions(),
+                                                                    actionName);
+
             if (actionDefinitions != null) {
-            	 if (actionDefinitions.size() != 1) {
-                     throw new RuntimeException("Expecting exactly 1 action definition with name mapping `" + actionName +
-                                                "`, but were found " + actionDefinitions.size());
-                 }
-            	    
-            	 actionDefinition = actionDefinitions.get(0);
+                if (actionDefinitions.size() != 1) {
+                    throw new RuntimeException("Expecting exactly 1 action definition with name mapping `" + actionName +
+                                                       "`, but were found " + actionDefinitions.size());
+                }
+
+                actionDefinition = actionDefinitions.get(0);
             }
-            
+
             if (actionDefinition == null) {
                 throw new RuntimeException("No action with name mapping `" + actionName + "` was found in connector `" +
-                		                   connectorName + "`");
+                                                   connectorName + "`");
             }
 
             actionDefinitionOptional = Optional.of(actionDefinition);
@@ -58,12 +59,14 @@ public class ConnectorActionDefinitionFinder {
 
         return actionDefinitionOptional;
     }
-    
-    public static List<ActionDefinition> filterByName(Map<String, ActionDefinition> actionDefinitions, 
-    		                                                 String actionName) {
-    	if (actionDefinitions == null || actionName == null) return null;
+
+    public static List<ActionDefinition> filterByName(Map<String, ActionDefinition> actionDefinitions,
+                                                      String actionName) {
+        if (actionDefinitions == null || actionName == null) {
+            return null;
+        }
         return actionDefinitions.entrySet()
-        		.stream()
+                .stream()
                 .filter(a -> actionName.equals(a.getValue().getName()))
                 .map(a -> a.getValue())
                 .collect(Collectors.toList());
