0eb810885dfca42ed3045c34a0dbbc0845554067
CTI777 <illia.goncharov@googlemail.com>
2019-07-15 12:35:39 +0200

    Refactoring of the test

--- activiti-spring-conformance-tests/activiti-spring-conformance-set4/src/test/java/org/activiti/spring/conformance/set4/BasicInclusiveGatewayTest.java
+++ activiti-spring-conformance-tests/activiti-spring-conformance-set4/src/test/java/org/activiti/spring/conformance/set4/BasicInclusiveGatewayTest.java
@@ -1,26 +1,30 @@
 package org.activiti.spring.conformance.set4;
 
+import static org.activiti.test.matchers.BPMNStartEventMatchers.startEvent;
+import static org.activiti.test.matchers.EndEventMatchers.endEvent;
+import static org.activiti.test.matchers.InclusiveGatewayMatchers.inclusiveGateway;
+import static org.activiti.test.matchers.ProcessInstanceMatchers.processInstance;
+import static org.activiti.test.matchers.ProcessTaskMatchers.taskWithName;
+import static org.activiti.test.matchers.SequenceFlowMatchers.sequenceFlow;
+import static org.activiti.test.matchers.TaskMatchers.task;
+import static org.activiti.test.matchers.TaskMatchers.withAssignee;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Java6Assertions.tuple;
 
-import org.activiti.api.model.shared.event.RuntimeEvent;
-import org.activiti.api.model.shared.event.VariableEvent;
 import org.activiti.api.process.model.ProcessInstance;
 import org.activiti.api.process.model.builders.ProcessPayloadBuilder;
-import org.activiti.api.process.model.events.BPMNActivityEvent;
-import org.activiti.api.process.model.events.BPMNSequenceFlowTakenEvent;
-import org.activiti.api.process.model.events.ProcessRuntimeEvent;
 import org.activiti.api.process.runtime.ProcessAdminRuntime;
 import org.activiti.api.process.runtime.ProcessRuntime;
 import org.activiti.api.runtime.shared.query.Page;
 import org.activiti.api.runtime.shared.query.Pageable;
 import org.activiti.api.task.model.Task;
-import org.activiti.api.task.model.Task.TaskStatus;
 import org.activiti.api.task.model.builders.TaskPayloadBuilder;
-import org.activiti.api.task.model.events.TaskRuntimeEvent;
+import org.activiti.api.task.model.payloads.GetTasksPayload;
 import org.activiti.api.task.runtime.TaskRuntime;
 import org.activiti.spring.conformance.util.RuntimeTestConfiguration;
 import org.activiti.spring.conformance.util.security.SecurityUtil;
+import org.activiti.test.operations.ProcessOperations;
+import org.activiti.test.operations.TaskOperations;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
@@ -46,60 +50,92 @@ public class BasicInclusiveGatewayTest {
 
     @Autowired
     private ProcessAdminRuntime processAdminRuntime;
+    
+    @Autowired
+    private ProcessOperations processOperations;
+    
+    @Autowired
+    private TaskOperations taskOperations;
 
     @Before
     public void cleanUp() {
         clearEvents();
     }
-
+    
     @Test
     public void testProcessExecutionWithInclusiveGateway() {
-
+        //given
         securityUtil.logInAs("user1");
-
-        ProcessInstance processInstance = processRuntime.start(ProcessPayloadBuilder
+        
+        //given
+        ProcessInstance processInstance = processOperations.start(ProcessPayloadBuilder
                 .start()
                 .withProcessDefinitionKey(processKey)
                 .withBusinessKey("my-business-key")
                 .withName("my-process-instance-name")
                 .withVariable("input",1)
-                .build());
+                .build())
 
         //then
-        assertThat(processInstance).isNotNull();
-        assertThat(processInstance.getStatus()).isEqualTo(ProcessInstance.ProcessInstanceStatus.RUNNING);
-        assertThat(processInstance.getBusinessKey()).isEqualTo("my-business-key");
-        assertThat(processInstance.getName()).isEqualTo("my-process-instance-name");
+                .expectFields(processInstance().status(ProcessInstance.ProcessInstanceStatus.RUNNING),
+                              processInstance().name("my-process-instance-name"),
+                              processInstance().businessKey("my-business-key"))
+       
+                .expect(processInstance().hasTask("Start Process",
+                                                  Task.TaskStatus.ASSIGNED,
+                                                  withAssignee("user1")))
+                .expectEvents(processInstance().hasBeenStarted(),
+                              startEvent("theStart").hasBeenStarted(),
+                              startEvent("theStart").hasBeenCompleted(),
+                              sequenceFlow("flow1").hasBeenTaken(),
+                              taskWithName("Start Process").hasBeenCreated(),
+                              taskWithName("Start Process").hasBeenAssigned()
+                )
+                .andReturn();
+        
+    
 
+        
         // I should be able to get the process instance from the Runtime
         ProcessInstance processInstanceById = processRuntime.processInstance(processInstance.getId());
         assertThat(processInstanceById).isEqualTo(processInstance);
-
-        Page<Task> tasks = taskRuntime.tasks(Pageable.of(0, 50));
+        
+        // I should get a task for User1
+        GetTasksPayload processInstanceTasksPayload = TaskPayloadBuilder
+                                                      .tasks()
+                                                      .withProcessInstanceId(processInstance.getId())
+                                                      .build();
+        
+        Page<Task> tasks = taskRuntime.tasks(Pageable.of(0,
+                                                         50),
+                                             processInstanceTasksPayload);
         assertThat(tasks.getTotalItems()).isEqualTo(1);
-
         Task task = tasks.getContent().get(0);
-        assertThat(task.getName()).isEqualTo("Start Process");
-        assertThat(task.getStatus()).isEqualTo(TaskStatus.ASSIGNED);
-        assertThat(task.getAssignee()).isEqualTo("user1");
-
-        assertThat(RuntimeTestConfiguration.collectedEvents)
-                .extracting(RuntimeEvent::getEventType)
-                .containsExactly(
-                        ProcessRuntimeEvent.ProcessEvents.PROCESS_CREATED,
-                        VariableEvent.VariableEvents.VARIABLE_CREATED,
-                        ProcessRuntimeEvent.ProcessEvents.PROCESS_STARTED,
-                        BPMNActivityEvent.ActivityEvents.ACTIVITY_STARTED,
-                        BPMNActivityEvent.ActivityEvents.ACTIVITY_COMPLETED,
-                        BPMNSequenceFlowTakenEvent.SequenceFlowEvents.SEQUENCE_FLOW_TAKEN,
-                        BPMNActivityEvent.ActivityEvents.ACTIVITY_STARTED,
-                        VariableEvent.VariableEvents.VARIABLE_CREATED,
-                        TaskRuntimeEvent.TaskEvents.TASK_CREATED,
-                        TaskRuntimeEvent.TaskEvents.TASK_ASSIGNED);
-
+        
         clearEvents();
+        
+        //given
+        taskOperations.complete(TaskPayloadBuilder
+                                .complete()
+                                .withTaskId(task.getId())
+                                .build())
+        //then
+                .expectEvents(task().hasBeenCompleted(),
+                              sequenceFlow("flow2").hasBeenTaken(),
+                              inclusiveGateway("inclusiveGateway").hasBeenStarted(), 
+                              inclusiveGateway("inclusiveGateway").hasBeenCompleted(), 
+                              sequenceFlow("flow3").hasBeenTaken(),
+                              taskWithName("Send e-mail").hasBeenCreated(),
+                              sequenceFlow("flow4").hasBeenTaken(),
+                              taskWithName("Check account").hasBeenCreated())
+                .expect(processInstance().hasTask("Send e-mail",
+                                                  Task.TaskStatus.ASSIGNED),
+                        processInstance().hasTask("Check account",
+                                                  Task.TaskStatus.ASSIGNED)
+                );
+        
+        
 
-        taskRuntime.complete(TaskPayloadBuilder.complete().withTaskId(task.getId()).build());
         
         //then - two tasks should be available
         tasks = taskRuntime.tasks(Pageable.of(0, 50));
@@ -108,46 +144,21 @@ public class BasicInclusiveGatewayTest {
         Task task1 = tasks.getContent().get(0);
         Task task2 = tasks.getContent().get(1);
         
-      
-        assertThat(tasks.getContent())
-        .extracting(Task::getStatus, Task::getName)
-        .contains(
-                tuple(  TaskStatus.ASSIGNED,
-                        "Send e-mail"),
-                tuple(  TaskStatus.ASSIGNED,
-                        "Check account")
-        );
-        
-        //check some events here, including start / complete events for inclusiveGateway 
-        assertThat(RuntimeTestConfiguration.collectedEvents)
-                .filteredOn(event -> event.getEventType().equals(BPMNActivityEvent.ActivityEvents.ACTIVITY_STARTED) ||
-                                     event.getEventType().equals(BPMNActivityEvent.ActivityEvents.ACTIVITY_COMPLETED))
-                .extracting("eventType",
-                            "entity.activityType",
-                            "entity.elementId")
-                .contains(
-                    tuple(  BPMNActivityEvent.ActivityEvents.ACTIVITY_COMPLETED,
-                            "userTask",
-                            "task0"),
-                    tuple(  BPMNActivityEvent.ActivityEvents.ACTIVITY_STARTED,
-                            "inclusiveGateway",
-                            "inclusiveGateway"
-                            ),
-                    tuple(  BPMNActivityEvent.ActivityEvents.ACTIVITY_COMPLETED,
-                            "inclusiveGateway",
-                            "inclusiveGateway"),
-                    tuple(  BPMNActivityEvent.ActivityEvents.ACTIVITY_STARTED,
-                            "userTask",
-                            "theTask1"
-                            ),
-                    tuple(  BPMNActivityEvent.ActivityEvents.ACTIVITY_STARTED,
-                            "userTask",
-                            "theTask2")
-                    );
-
         clearEvents();
+   
+        //given
+        taskOperations.complete(TaskPayloadBuilder
+                                .complete()
+                                .withTaskId(task1.getId())
+                                .build())
+        //then
+                .expectEvents(task().hasBeenCompleted(),
+                              inclusiveGateway("inclusiveGatewayEnd").hasBeenStarted())
+                .expect(processInstance().hasTask(task2.getName(),
+                                                  task2.getStatus())
+                );
         
-        taskRuntime.complete(TaskPayloadBuilder.complete().withTaskId(task1.getId()).build());
+        
         
         //then - only second task should be available
         tasks = taskRuntime.tasks(Pageable.of(0, 50));
@@ -160,61 +171,24 @@ public class BasicInclusiveGatewayTest {
                         task2.getName())
         );
         
-        //check some events here, including start event for inclusiveGatewayEnd 
-        assertThat(RuntimeTestConfiguration.collectedEvents)
-        .filteredOn(event -> event.getEventType().equals(BPMNActivityEvent.ActivityEvents.ACTIVITY_STARTED) ||
-                             event.getEventType().equals(BPMNActivityEvent.ActivityEvents.ACTIVITY_COMPLETED))
-        .extracting("eventType",
-                    "entity.activityType",
-                    "entity.elementId")
-        .containsExactly(
-            tuple(  BPMNActivityEvent.ActivityEvents.ACTIVITY_COMPLETED,
-                    "userTask",
-                    "theTask1"),
-            tuple(  BPMNActivityEvent.ActivityEvents.ACTIVITY_STARTED,
-                    "inclusiveGateway",
-                    "inclusiveGatewayEnd"
-                    ));
-
         clearEvents();
         
-        
         //complete second task
-        taskRuntime.complete(TaskPayloadBuilder.complete().withTaskId(task2.getId()).build());
+        taskOperations.complete(TaskPayloadBuilder
+                                .complete()
+                                .withTaskId(task2.getId())
+                                .build())
+        //then
+                .expectEvents(task().hasBeenCompleted(),
+                              inclusiveGateway("inclusiveGatewayEnd").hasBeenStarted(),
+                              inclusiveGateway("inclusiveGatewayEnd").hasBeenCompleted(),
+                              endEvent("theEnd").hasBeenStarted(),
+                              endEvent("theEnd").hasBeenCompleted());
+        
         
         //No tasks should be available
         tasks = taskRuntime.tasks(Pageable.of(0, 50));
-        assertThat(tasks.getTotalItems()).isEqualTo(0);
-              
-        //check some events here, including start event for inclusiveGatewayEnd 
-        assertThat(RuntimeTestConfiguration.collectedEvents)
-        .filteredOn(event -> event.getEventType().equals(BPMNActivityEvent.ActivityEvents.ACTIVITY_STARTED) ||
-                             event.getEventType().equals(BPMNActivityEvent.ActivityEvents.ACTIVITY_COMPLETED))
-        .extracting("eventType",
-                    "entity.activityType",
-                    "entity.elementId")
-        .containsExactly(
-            tuple(  BPMNActivityEvent.ActivityEvents.ACTIVITY_COMPLETED,
-                    "userTask",
-                    "theTask2"),
-            tuple(  BPMNActivityEvent.ActivityEvents.ACTIVITY_STARTED,
-                    "inclusiveGateway",
-                    "inclusiveGatewayEnd"
-                    ),
-            tuple(  BPMNActivityEvent.ActivityEvents.ACTIVITY_COMPLETED,
-                    "inclusiveGateway",
-                    "inclusiveGatewayEnd"
-                    ),
-            tuple(  BPMNActivityEvent.ActivityEvents.ACTIVITY_STARTED,
-                    "endEvent",
-                    "theEnd"
-                    ),
-            tuple(  BPMNActivityEvent.ActivityEvents.ACTIVITY_COMPLETED,
-                    "endEvent",
-                    "theEnd"
-                    ));
-        
-        clearEvents();
+        assertThat(tasks.getTotalItems()).isEqualTo(0);              
     }
 
     @After
