acf507e8880f2e3f180f5eb0a106ec7439b4b5fc
Miguel Ruiz <miguel.ruiz.developer@gmail.com>
2019-07-19 11:06:42 +0100

    ACTIVITI-3310 Clean up

--- activiti-spring-boot-starter/src/main/java/org/activiti/spring/boot/DefaultActivityBehaviorFactoryMappingConfigurer.java
+++ activiti-spring-boot-starter/src/main/java/org/activiti/spring/boot/DefaultActivityBehaviorFactoryMappingConfigurer.java
@@ -3,9 +3,7 @@ package org.activiti.spring.boot;
 import org.activiti.runtime.api.impl.MappingAwareActivityBehaviorFactory;
 import org.activiti.runtime.api.impl.VariablesMappingProvider;
 import org.activiti.spring.SpringProcessEngineConfiguration;
-import org.springframework.context.annotation.Configuration;
 
-@Configuration
 public class DefaultActivityBehaviorFactoryMappingConfigurer implements ProcessEngineConfigurationConfigurer {
 
     private VariablesMappingProvider variablesMappingProvider;
