9819769cd69093d0058dd457b1c0442dfc059249
Oleksandr Merkulov <almerico@gmail.com>
2019-07-24 21:26:54 +0300

    first iteration extension remove

--- activiti-engine/src/test/java/org/activiti/engine/test/bpmn/event/error/mapError/BoundaryErrorMapTest.java
+++ activiti-engine/src/test/java/org/activiti/engine/test/bpmn/event/error/mapError/BoundaryErrorMapTest.java
@@ -96,16 +96,4 @@ public class BoundaryErrorMapTest extends PluggableActivitiTestCase {
     runtimeService.startProcessInstanceByKey("subprocssWithSingleExceptionMap", vars);
     assertTrue(FlagDelegate.isVisited());
   }
-
-  @Deployment(resources = { "org/activiti/engine/test/bpmn/event/error/mapError/BoundaryErrorMapTest.testCallProcessSingleDirectMap.bpmn20.xml",
-      "org/activiti/engine/test/bpmn/event/error/mapError/BoundaryErrorMapTest.testCallProcessCalee.bpmn20.xml" })
-  public void testCallProcessSingleDirectMap() {
-    FlagDelegate.reset();
-    Map<String, Object> vars = new HashMap<String, Object>();
-    vars.put("exceptionClass", BoundaryErrorParentException.class.getName());
-
-    runtimeService.startProcessInstanceByKey("callProcssWithSingleExceptionMap", vars);
-    assertTrue(FlagDelegate.isVisited());
-  }
-
 }
