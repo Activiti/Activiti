a3e4fc7444295d83dbd821d26ace71aa213159a9
Illia Goncharov <illia.goncharov@googlemail.com>
2019-10-11 09:10:48 +0200

    DateFormatterProvider autoconfiguration (#122)
    * fix: refactoring made, spring.factories with auto configuration
    
    * test added
    
    * Refactoring

--- activiti-common-util/src/main/java/org/activiti/common/util/conf/ActivitiCoreCommonUtilAutoConfiguration.java
+++ activiti-common-util/src/main/java/org/activiti/common/util/conf/ActivitiCoreCommonUtilAutoConfiguration.java
@@ -20,7 +20,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-public class UtilAutoConfiguration {
+public class ActivitiCoreCommonUtilAutoConfiguration {
 
     @Bean
     @ConditionalOnMissingBean
