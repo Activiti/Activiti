bc2b8012bfcf6b83cc2b8167a29a67b6a68dfd64
CTI777 <illia.goncharov@googlemail.com>
2019-10-28 22:43:26 +0100

    AAE-105 refactoring of test for process / task validators

---
+++ activiti-api-impl/activiti-api-process-runtime-impl/src/test/java/org/activiti/runtime/api/impl/ProcessVariablesPayloadValidatorTest.java
@@ -0,0 +1,201 @@
+/*
+ * Copyright 2017 Alfresco, Inc. and/or its affiliates.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.activiti.runtime.api.impl;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.catchThrowable;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.MockitoAnnotations.initMocks;
+
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.activiti.api.process.model.builders.ProcessPayloadBuilder;
+import org.activiti.common.util.DateFormatterProvider;
+import org.activiti.spring.process.model.Extension;
+import org.activiti.spring.process.model.ProcessExtensionModel;
+import org.activiti.spring.process.model.VariableDefinition;
+import org.activiti.spring.process.variable.VariableValidationService;
+import org.activiti.spring.process.variable.types.DateVariableType;
+import org.activiti.spring.process.variable.types.JavaObjectVariableType;
+import org.activiti.spring.process.variable.types.JsonObjectVariableType;
+import org.activiti.spring.process.variable.types.VariableType;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mock;
+
+public class ProcessVariablesPayloadValidatorTest {
+ 
+    @Mock
+    private Map<String, ProcessExtensionModel> processExtensionModelMap;
+    
+    private DateFormatterProvider dateFormatterProvider = new DateFormatterProvider("yyyy-MM-dd[['T']HH:mm:ss[.SSS'Z']]");
+    private ObjectMapper objectMapper= new ObjectMapper();
+    private VariableNameValidator variableNameValidator= new VariableNameValidator();
+    
+    private Map<String, VariableType> variableTypeMap;
+    
+    private ProcessVariablesPayloadValidator processVariablesValidator;
+    private VariableValidationService variableValidationService;
+
+    @Before
+    public void setUp() {
+        initMocks(this);
+        
+        ProcessExtensionModel processExtensionModel;
+        
+        VariableDefinition variableDefinitionName = new VariableDefinition();
+        variableDefinitionName.setName("name");
+        variableDefinitionName.setType("string");
+
+        VariableDefinition variableDefinitionAge = new VariableDefinition();
+        variableDefinitionAge.setName("age");
+        variableDefinitionAge.setType("integer");
+
+        VariableDefinition variableDefinitionSubscribe = new VariableDefinition();
+        variableDefinitionSubscribe.setName("subscribe");
+        variableDefinitionSubscribe.setType("boolean");
+        
+        VariableDefinition variableDefinitionDate = new VariableDefinition();
+        variableDefinitionDate.setName("mydate");
+        variableDefinitionDate.setType("date");
+
+        Map<String, VariableDefinition> properties = new HashMap<>();
+        properties.put("name", variableDefinitionName);
+        properties.put("age", variableDefinitionAge);
+        properties.put("subscribe", variableDefinitionSubscribe);
+        properties.put("mydate", variableDefinitionDate);
+
+       
+        variableTypeMap = new HashMap<>();
+        variableTypeMap.put("boolean", new JavaObjectVariableType(Boolean.class));
+        variableTypeMap.put("string", new JavaObjectVariableType(String.class));
+        variableTypeMap.put("integer", new JavaObjectVariableType(Integer.class));
+        variableTypeMap.put("json", new JsonObjectVariableType(objectMapper));
+        variableTypeMap.put("file", new JsonObjectVariableType(objectMapper));
+        variableTypeMap.put("date", new DateVariableType(Date.class, dateFormatterProvider));
+        
+        variableValidationService = new VariableValidationService(variableTypeMap); 
+        
+        processVariablesValidator = new ProcessVariablesPayloadValidator(dateFormatterProvider,
+                                                                         processExtensionModelMap,
+                                                                         variableValidationService,
+                                                                         variableNameValidator);
+        
+        Extension extension = new Extension();
+        extension.setProperties(properties);
+
+        processExtensionModel = new ProcessExtensionModel();
+        processExtensionModel.setId("1");
+        processExtensionModel.setExtensions(extension);
+        
+        given(processExtensionModelMap.get(any()))
+                   .willReturn(processExtensionModel);
+    }
+
+    @Test
+    public void should_returnErrorList_when_setVariablesWithWrongType() throws Exception {
+
+        Map<String, Object> variables = new HashMap<>();
+        variables.put("name", "Alice");
+        variables.put("age", "24");
+        variables.put("subscribe", "false");
+
+        Throwable throwable = catchThrowable(() -> processVariablesValidator.checkPayloadVariables(
+                                                                            ProcessPayloadBuilder
+                                                                                .setVariables()
+                                                                                .withVariables(variables)
+                                                                                .build(),
+                                                                            "10"));
+
+        assertThat(throwable).isInstanceOf(IllegalStateException.class);
+        
+        assertThat(throwable.getMessage())
+            .contains("subscribe",
+                      "age");
+    }
+
+    @Test
+    public void should_returnErrorList_when_setVariablesWithNameWrongType() throws Exception {
+
+        Map<String, Object> variables = new HashMap<>();
+        variables.put("name", "Alice");
+        variables.put("gender", "female");
+        variables.put("age", "24");
+        variables.put("subs", true);
+        variables.put("subscribe", true);
+        variables.put("mydate", "2019-08-26T10:20:30.000Z");
+        
+        String expectedTypeErrorMessage = "age";
+
+        Throwable throwable = catchThrowable(() -> processVariablesValidator.checkPayloadVariables(
+                                                                            ProcessPayloadBuilder
+                                                                                .setVariables()
+                                                                                .withVariables(variables)
+                                                                                .build(),
+                                                                            "10"));
+
+        assertThat(throwable).isInstanceOf(IllegalStateException.class); 
+        
+        assertThat(throwable.getMessage())
+            .contains(expectedTypeErrorMessage);
+        
+    }
+  
+    @Test
+    public void should_returnError_when_setVariablesWithWrongDateFormat() throws Exception {
+
+        Map<String, Object> variables = new HashMap<>();
+        variables.put("mydate", "2019-08-26TT10:20:30.000Z");
+
+        Throwable throwable = catchThrowable(() -> processVariablesValidator.checkPayloadVariables(
+                                                                            ProcessPayloadBuilder
+                                                                                .setVariables()
+                                                                                .withVariables(variables)
+                                                                                .build(),
+                                                                            "10"));
+
+        assertThat(throwable).isInstanceOf(IllegalStateException.class);
+        
+    }
+    
+    @Test
+    public void should_returnErrorList_when_setVariableWithWrongCharactersInName() throws Exception {
+
+        Map<String, Object> variables = new HashMap<>();
+        variables.put("name", "Alice");
+        variables.put("gen-der", "female");
+  
+        String expectedTypeErrorMessage = "gen-der";
+
+        Throwable throwable = catchThrowable(() -> processVariablesValidator.checkPayloadVariables(
+                                                                            ProcessPayloadBuilder
+                                                                                .setVariables()
+                                                                                .withVariables(variables)
+                                                                                .build(),
+                                                                            "10"));
+
+        assertThat(throwable).isInstanceOf(IllegalStateException.class); 
+        
+        assertThat(throwable.getMessage())
+            .contains(expectedTypeErrorMessage);
+        
+    }
+}
---
+++ activiti-api-impl/activiti-api-task-runtime-impl/src/test/java/org/activiti/runtime/api/impl/TaskVariablesPayloadValidatorTest.java
@@ -0,0 +1,186 @@
+/*
+ * Copyright 2019 Alfresco, Inc. and/or its affiliates.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.activiti.runtime.api.impl;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.time.Duration;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.activiti.api.task.model.builders.TaskPayloadBuilder;
+import org.activiti.api.task.model.payloads.CreateTaskVariablePayload;
+import org.activiti.api.task.model.payloads.UpdateTaskVariablePayload;
+import org.activiti.common.util.DateFormatterProvider;
+import org.junit.Test;
+
+public class TaskVariablesPayloadValidatorTest {
+
+    private TaskVariablesPayloadValidator taskVariablesPayloadValidator = new TaskVariablesPayloadValidator(new DateFormatterProvider("yyyy-MM-dd[['T']HH:mm:ss[.SSS'Z']]"),
+                                                                                                              new VariableNameValidator());
+
+    @Test
+    public void handleDatesForMap_should_convertStringToDate_whenStringRepresentsADate() {
+        //given
+        HashMap<String, Object> payloadMap = new HashMap<>();
+        payloadMap.put("date", "1970-01-01");
+        payloadMap.put("dateTime", "1970-01-01T01:01:01.001Z");
+        payloadMap.put("notADate", "this is not a date");
+        payloadMap.put("int", 1);
+        payloadMap.put("boolean", true);
+
+
+        //calculate number of milliseconds after 1970-01-01T00:00:00.000Z
+        long time = Duration.ofHours(1).toMillis() + Duration.ofMinutes(1).toMillis() + Duration.ofSeconds(1).toMillis() + 1;
+
+        //when
+        Map<String, Object> handledDates = taskVariablesPayloadValidator.handlePayloadVariables(payloadMap);
+
+        //then
+        assertThat(handledDates).containsEntry("date", new Date(0));
+        assertThat(handledDates).containsEntry("dateTime", new Date(time));
+        assertThat(handledDates).containsEntry("notADate", "this is not a date");
+        assertThat(handledDates).containsEntry("int", 1);
+        assertThat(handledDates).containsEntry("boolean", true);
+    }
+
+    @Test
+    public void handleDateForCreateTaskVariablePayload_should_convertStringToDate_when_stringRepresentsADate() {
+        //given
+        CreateTaskVariablePayload payload = TaskPayloadBuilder.createVariable()
+                .withVariable("date",
+                              "1970-01-01")
+                .build();
+
+        //when
+        CreateTaskVariablePayload handledVariablePayload = taskVariablesPayloadValidator.handleCreateTaskVariablePayload(payload);
+
+        //then
+        assertThat(handledVariablePayload.getValue()).isEqualTo(new Date(0));
+    }
+
+    @Test
+    public void handleDateForCreateTaskVariablePayload_should_convertStringToDate_when_stringRepresentsADateWithTime() {
+        //given
+        CreateTaskVariablePayload payload = TaskPayloadBuilder.createVariable()
+                .withVariable("date",
+                              "1970-01-01T01:01:01.001Z")
+                .build();
+        //calculate number of milliseconds after 1970-01-01T00:00:00.000Z
+        long time = Duration.ofHours(1).toMillis() + Duration.ofMinutes(1).toMillis() + Duration.ofSeconds(1).toMillis() + 1;
+
+        //when
+        CreateTaskVariablePayload handledVariablePayload = taskVariablesPayloadValidator.handleCreateTaskVariablePayload(payload);
+
+        //then
+        assertThat(handledVariablePayload.getValue()).isEqualTo(new Date(time));
+    }
+
+    @Test
+    public void handleDateForCreateTaskVariablePayload_should_doNothing_when_stringIsNotADate() {
+        //given
+        CreateTaskVariablePayload payload = TaskPayloadBuilder.createVariable()
+                .withVariable("notADate",
+                              "this is not a date")
+                .build();
+
+        //when
+        CreateTaskVariablePayload handledVariablePayload = taskVariablesPayloadValidator.handleCreateTaskVariablePayload(payload);
+
+        //then
+        assertThat(handledVariablePayload.getValue()).isEqualTo("this is not a date");
+    }
+
+    @Test
+    public void handleDateForCreateTaskVariablePayload_should_doNothing_when_itIsNotAString() {
+        //given
+        CreateTaskVariablePayload payload = TaskPayloadBuilder.createVariable()
+                .withVariable("notAString",
+                              10)
+                .build();
+
+        //when
+        CreateTaskVariablePayload handledVariablePayload = taskVariablesPayloadValidator.handleCreateTaskVariablePayload(payload);
+
+        //then
+        assertThat(handledVariablePayload.getValue()).isEqualTo(10);
+    }
+
+    @Test
+    public void handleDateForUpdateTaskPayload_should_convertStringToDate_when_stringRepresentsADate() {
+        //given
+        UpdateTaskVariablePayload updateTaskVariablePayload = TaskPayloadBuilder.updateVariable().withVariable("date",
+                                                                                          "1970-01-01").build();
+        //when
+        UpdateTaskVariablePayload handledDatePayload = taskVariablesPayloadValidator.handleUpdateTaskVariablePayload(updateTaskVariablePayload);
+
+        //then
+        assertThat(handledDatePayload.getValue()).isEqualTo(new Date(0));
+    }
+
+    @Test
+    public void handleDateForUpdateTaskPayload_should_convertStringToDate_when_stringRepresentsADateWithTime() {
+        //given
+        UpdateTaskVariablePayload updateTaskVariablePayload = TaskPayloadBuilder
+                .updateVariable()
+                .withVariable("date",
+                              "1970-01-01T01:01:01.001Z")
+                .build();
+        //calculate number of milliseconds after 1970-01-01T00:00:00.000Z
+        long time = Duration.ofHours(1).toMillis() + Duration.ofMinutes(1).toMillis() + Duration.ofSeconds(1).toMillis() + 1;
+
+        //when
+        UpdateTaskVariablePayload handledDatePayload = taskVariablesPayloadValidator.handleUpdateTaskVariablePayload(updateTaskVariablePayload);
+
+        //then
+        assertThat(handledDatePayload.getValue()).isEqualTo(new Date(time));
+    }
+
+    @Test
+    public void handleDateForUpdateTaskPayload_should_doNothing_when_stringIsNotADate() {
+        //given
+        UpdateTaskVariablePayload updateTaskVariablePayload = TaskPayloadBuilder
+                .updateVariable()
+                .withVariable("notADate",
+                              "this is not a date")
+                .build();
+
+        //when
+        UpdateTaskVariablePayload handledDatePayload = taskVariablesPayloadValidator.handleUpdateTaskVariablePayload(updateTaskVariablePayload);
+
+        //then
+        assertThat(handledDatePayload.getValue()).isEqualTo("this is not a date");
+    }
+
+    @Test
+    public void handleDateForUpdateTaskPayload_should_doNothing_when_itIsNotAString() {
+        //given
+        UpdateTaskVariablePayload updateTaskVariablePayload = TaskPayloadBuilder
+                .updateVariable()
+                .withVariable("notAString",
+                              true)
+                .build();
+
+        //when
+        UpdateTaskVariablePayload handledDatePayload = taskVariablesPayloadValidator.handleUpdateTaskVariablePayload(updateTaskVariablePayload);
+
+        //then
+        assertThat(handledDatePayload.getValue()).isEqualTo(true);
+    }
+
+}
