41f1d7f5adc9415b2b976ef0e4934d8af473c42a
robfrank <roberto.franchini@alfresco.com>
2019-11-07 10:32:16 +0100

    AAE-764
    cleanup

--- activiti-spring-process-extensions/src/test/java/org/activiti/spring/process/conf/ProcessExtensionResourceFinderDescriptorAutoConfiguration.java
+++ activiti-spring-process-extensions/src/test/java/org/activiti/spring/process/conf/ProcessExtensionResourceFinderDescriptorAutoConfiguration.java
@@ -22,7 +22,6 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class ProcessExtensionResourceFinderDescriptorAutoConfiguration {
 
-
     @Bean
     @ConditionalOnMissingBean
     public ProcessExtensionResourceFinderDescriptor processExtensionResourceFinderDescriptor(
