c4c445623703fb86083d24dc3f63b2dc841cb88d
dcerbo <davide.cerbo@alfresco.com>
2020-01-21 17:13:31 +0000

    AAE-944 mock test

--- activiti-core/activiti-api-impl/activiti-api-task-runtime-impl/src/test/java/org/activiti/runtime/api/impl/TaskRuntimeImplTest.java
+++ activiti-core/activiti-api-impl/activiti-api-task-runtime-impl/src/test/java/org/activiti/runtime/api/impl/TaskRuntimeImplTest.java
@@ -16,17 +16,27 @@
 
 package org.activiti.runtime.api.impl;
 
+import org.activiti.api.runtime.shared.security.SecurityManager;
 import org.activiti.api.task.model.Task;
 import org.activiti.api.task.model.builders.TaskPayloadBuilder;
 import org.activiti.api.task.model.impl.TaskImpl;
 import org.activiti.api.task.model.payloads.UpdateTaskPayload;
+import org.activiti.engine.TaskService;
+import org.activiti.engine.impl.TaskQueryImpl;
+import org.activiti.engine.task.IdentityLink;
+import org.activiti.engine.task.IdentityLinkType;
+import org.activiti.runtime.api.model.impl.APITaskConverter;
 import org.junit.Before;
 import org.junit.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 
+import static java.util.Arrays.asList;
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.*;
 import static org.mockito.MockitoAnnotations.initMocks;
 
 public class TaskRuntimeImplTest {
@@ -37,13 +47,28 @@ public class TaskRuntimeImplTest {
     @Mock
     private TaskRuntimeHelper taskRuntimeHelper;
 
+    @Mock
+    private APITaskConverter taskConverter;
+
+    @Mock
+    private org.activiti.engine.task.Task engineTaskMock;
+
+    @Mock
+    private SecurityManager securityManager;
+
+    @Mock
+    private TaskService taskService;
+
+    @Mock
+    private IdentityLink identityLink;
+
     @Before
     public void setUp() {
         initMocks(this);
     }
 
     @Test
-    public void updateShouldReturnResultOfHelper() {
+    public void should_returnResultOfHelper_when_updateTask() {
         //given
         UpdateTaskPayload updateTaskPayload = TaskPayloadBuilder
                 .update()
@@ -60,5 +85,49 @@ public class TaskRuntimeImplTest {
 
         //then
         assertThat(retrievedTask).isEqualTo(updatedTask);
+    }
+
+    @Test
+    public void should_returnCandidateUsersAndGroups_when_getTaskById(){
+        String taskId = "taskId";
+        given(taskRuntimeHelper.getInternalTaskWithChecks(any()))
+                .willReturn(engineTaskMock);
+
+        given(taskConverter.from(any(org.activiti.engine.task.Task.class)))
+                .willReturn(new TaskImpl(taskId, "task name", Task.TaskStatus.CREATED));
+
+        given(securityManager.getAuthenticatedUserId())
+                .willReturn("userId");
+
+        TaskQueryImpl taskQuery = spy(new TaskQueryImpl());
+
+        given(taskService.createTaskQuery())
+                .willReturn(taskQuery);
+
+        doReturn(engineTaskMock)
+                .when(taskQuery)
+                .singleResult();
+
+        given(taskService.getIdentityLinksForTask(any()))
+                .willReturn(asList(identityLink));
+
+        given(identityLink.getType())
+                .willReturn(IdentityLinkType.CANDIDATE);
+
+        given(identityLink.getUserId())
+                .willReturn("user");
+
+        given(identityLink.getGroupId())
+                .willReturn("group");
+
+        Task task = taskRuntime.task(taskId);
+        assertThat(task.getCandidateUsers()).isNotNull();
+        assertThat(task.getCandidateUsers()).containsExactly("user");
+        assertThat(task.getCandidateGroups()).isNotNull();
+        assertThat(task.getCandidateGroups()).containsExactly("group");
+
+
+        verify(taskRuntimeHelper).getInternalTaskWithChecks(eq(taskId));
+        verify(taskService, times(2)).getIdentityLinksForTask(eq(taskId));
     }
 }
