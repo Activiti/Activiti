4423997fe4de21aaf5bb22ac8476e20700756d12
Illia Goncharov <illia.goncharov@googlemail.com>
2020-02-14 16:54:21 +0100

    #3145 Fix import of simple Task (#3143)
    * AAE-817 changes for TaskXMLConverter
    
    <task> xml tag will remain after converting
    
    * AAE-817 add incoming / outgoing flow elements for manual task
    
    * AAE-817 add incoming  / outgoing flow elements for userTask
    
    * AAE-817 fix a test
    
    * AAE-817 code style fix
    
    * #3145 fix comments from reviewer

---
+++ activiti-core/activiti-bpmn-converter/src/test/java/org/activiti/editor/language/xml/SimpleUserTaskConverterTest.java
@@ -0,0 +1,48 @@
+package org.activiti.editor.language.xml;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.activiti.bpmn.converter.BpmnXMLConverter;
+import org.activiti.bpmn.model.BpmnModel;
+import org.activiti.bpmn.model.FlowElement;
+import org.activiti.bpmn.model.UserTask;
+import org.junit.Test;
+
+public class SimpleUserTaskConverterTest extends AbstractConverterTest {
+
+    @Test
+    public void convertXMLToModel() throws Exception {
+        BpmnModel bpmnModel = readXMLFile();
+        validateModel(bpmnModel);
+    }
+
+    @Test
+    public void convertModelToXML() throws Exception {
+        BpmnModel bpmnModel = readXMLFile();
+        BpmnModel parsedModel = exportAndReadXMLFile(bpmnModel);
+        validateModel(parsedModel);
+    }
+
+    @Override
+    protected String getResource() {
+        return "simpleUserTaskModel.bpmn";
+    }
+
+    private void validateModel(BpmnModel model) throws Exception {
+        FlowElement flowElement = model.getMainProcess().getFlowElement("UserTask_0ej3luy");
+        assertThat(flowElement).isNotNull();
+        assertThat(flowElement instanceof UserTask).isTrue();
+
+        checkXml(model);
+    }
+
+    private void checkXml(BpmnModel model) throws Exception {
+
+        String xml = new String(new BpmnXMLConverter().convertToXML(model),
+                                "UTF-8");
+
+        assertThat(xml).contains("incoming>SequenceFlow_12e82d4<",
+                                 "outgoing>SequenceFlow_0zx88mt<");
+
+    }
+}
