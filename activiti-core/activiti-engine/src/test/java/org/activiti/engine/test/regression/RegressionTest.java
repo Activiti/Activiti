2af68cbc5f3bc654dc24ff72f3df235724da2a23
Nithin Nambiar <nithinnambiar@yahoo.co.in>
2020-04-30 09:45:13 +0100

    increase timeout of tests

--- activiti-core/activiti-engine/src/test/java/org/activiti/engine/test/regression/RegressionTest.java
+++ activiti-core/activiti-engine/src/test/java/org/activiti/engine/test/regression/RegressionTest.java
@@ -1,9 +1,9 @@
 /* Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -16,7 +16,7 @@ import org.activiti.engine.impl.test.PluggableActivitiTestCase;
 
 /**
  * All tests that do not belong to any other test case, but test a supposedly working feature go here.
- * 
+ *
 
  */
 public class RegressionTest extends PluggableActivitiTestCase {
@@ -30,6 +30,8 @@ public class RegressionTest extends PluggableActivitiTestCase {
         .addClasspathResource("org/activiti/engine/test/regression/act1623-processTwo.bpmn").deploy().getId();
 
     runtimeService.startProcessInstanceByKey("ProcessOne");
+
+    Thread.sleep(2000);
 
     // Clean
     repositoryService.deleteDeployment(deploymentId, true);
