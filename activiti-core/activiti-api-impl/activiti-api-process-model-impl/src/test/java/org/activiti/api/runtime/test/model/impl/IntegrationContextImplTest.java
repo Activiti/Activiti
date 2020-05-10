f1a95761e3f45fd63d296bf03c28e705024aded8
Igor Dianov <igor@dianov.com>
2020-05-10 09:44:14 -0700

    feat: added JUnit5 parametrized tests

--- activiti-core/activiti-api-impl/activiti-api-process-model-impl/src/test/java/org/activiti/api/runtime/test/model/impl/IntegrationContextImplTest.java
+++ activiti-core/activiti-api-impl/activiti-api-process-model-impl/src/test/java/org/activiti/api/runtime/test/model/impl/IntegrationContextImplTest.java
@@ -4,10 +4,12 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 import java.io.IOException;
 import java.math.BigDecimal;
+import java.util.stream.Stream;
 
 import org.activiti.api.process.model.IntegrationContext;
 import org.activiti.api.runtime.model.impl.IntegrationContextImpl;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.MethodSource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -25,6 +27,13 @@ class IntegrationContextImplTest {
     @Autowired
     private ObjectMapper objectMapper;
 
+    private static Object[] testValues = {BigDecimal.valueOf(1000, 2),
+                                          Long.valueOf(100000000000L),
+                                          Integer.valueOf(123),
+                                          String.valueOf("name"),
+                                          Boolean.valueOf(true),
+                                          Double.valueOf(123.123),
+                                          Float.valueOf(123.123F)};
     @SpringBootApplication
     static class Application {
 
@@ -34,42 +43,49 @@ class IntegrationContextImplTest {
         }
     }
 
-    @Test
-    void testIntegrationContextInBoundVariables() throws JsonParseException, JsonMappingException, IOException {
+    private static Stream<Object> testIntegrationContextInBoundVariables() {
+        return Stream.of(testValues);
+    }
+
+    @ParameterizedTest
+    @MethodSource
+    void testIntegrationContextInBoundVariables(Object value) throws JsonParseException, JsonMappingException, IOException {
         // given
         IntegrationContextImpl source = new IntegrationContextImpl();
 
-        source.addInBoundVariable("amount",
-                                  BigDecimal.valueOf(1000, 2));
-
+        source.addInBoundVariable("variable",
+                                  value);
         // when
         IntegrationContext target = exchangeIntegrationContext(source);
 
         // then
-        assertThat(target.getInBoundVariables()).containsEntry("amount",
-                                                               BigDecimal.valueOf(1000, 2));
+        assertThat(target.getInBoundVariables()).containsEntry("variable",
+                                                               value);
     }
 
+    private static Stream<Object> testIntegrationContextOutBoundVariables() {
+        return Stream.of(testValues);
+    }
 
-    @Test
-    void testIntegrationContextOutBoundVariables() throws JsonParseException, JsonMappingException, IOException {
+    @ParameterizedTest
+    @MethodSource
+    public void testIntegrationContextOutBoundVariables(Object value) throws JsonParseException, JsonMappingException, IOException {
         // given
         IntegrationContextImpl source = new IntegrationContextImpl();
 
-        source.addOutBoundVariable("amount",
-                                   BigDecimal.valueOf(1000, 2));
+        source.addOutBoundVariable("variable",
+                                   value);
 
         // when
         IntegrationContext result = exchangeIntegrationContext(source);
 
         // then
-        assertThat(result.getOutBoundVariables()).containsEntry("amount",
-                                                                BigDecimal.valueOf(1000, 2));
+        assertThat(result.getOutBoundVariables()).containsEntry("variable",
+                                                                value);
     }
 
     private IntegrationContext exchangeIntegrationContext(IntegrationContext source) throws IOException {
         return objectMapper.readValue(objectMapper.writeValueAsString(source),
                                       IntegrationContext.class);
     }
-
 }
