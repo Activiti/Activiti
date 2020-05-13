64f6538bfc630ecb250f57b6517c92e75b145e4d
Igor Dianov <igor@dianov.com>
2020-05-13 00:22:10 -0700

    fix: use Instant to convert Dates variables

--- activiti-core/activiti-api-impl/activiti-api-process-model-impl/src/main/java/org/activiti/api/runtime/model/impl/DateToStringConverter.java
+++ activiti-core/activiti-api-impl/activiti-api-process-model-impl/src/main/java/org/activiti/api/runtime/model/impl/DateToStringConverter.java
@@ -16,8 +16,6 @@
 
 package org.activiti.api.runtime.model.impl;
 
-import java.text.DateFormat;
-import java.text.SimpleDateFormat;
 import java.util.Date;
 
 import org.springframework.core.convert.converter.Converter;
@@ -25,19 +23,11 @@ import org.springframework.core.convert.converter.Converter;
 @ProcessVariableTypeConverter
 public class DateToStringConverter implements Converter<Date, String> {
 
-    private String dateFormatString = "yyyy-MM-dd'T'HH:mm:ss.SSSXXX";
-
     public DateToStringConverter() {
-    }
-
-    public DateToStringConverter(String dateFormatString) {
-        this.dateFormatString = dateFormatString;
     }
 
     @Override
     public String convert(Date source) {
-        DateFormat df = new SimpleDateFormat(dateFormatString);
-
-        return df.format(source);
+        return source.toInstant().toString();
     }
 }
