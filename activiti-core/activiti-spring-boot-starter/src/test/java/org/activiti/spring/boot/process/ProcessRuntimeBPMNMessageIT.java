2fe10ed05f1fe6e68d4c648caf20253b4ac00bc9
Miguel Ruiz <miguel.ruiz.developer@gmail.com>
2020-07-28 18:14:03 +0200

    Activiti/Activiti#3368 Clean up

--- activiti-core/activiti-spring-boot-starter/src/test/java/org/activiti/spring/boot/process/ProcessRuntimeBPMNMessageIT.java
+++ activiti-core/activiti-spring-boot-starter/src/test/java/org/activiti/spring/boot/process/ProcessRuntimeBPMNMessageIT.java
@@ -22,7 +22,6 @@ import static org.assertj.core.api.Assertions.tuple;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Map;
 import org.activiti.api.model.shared.event.RuntimeEvent;
 import org.activiti.api.model.shared.model.VariableInstance;
 import org.activiti.api.process.model.ProcessInstance;
