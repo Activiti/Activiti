488c149fa35ea21f05c352bc84224f87f4d02499
Elias Ricken de Medeiros <26007058+erdemedeiros@users.noreply.github.com>
2020-08-10 17:45:40 +0200

    Use thread-safe list
    awaitility uses a different thread so we need a thread safe collection to hold the events

--- activiti-core-common/activiti-core-test/activiti-core-test-local-runtime/src/main/java/org/activiti/test/LocalEventSource.java
+++ activiti-core-common/activiti-core-test/activiti-core-test-local-runtime/src/main/java/org/activiti/test/LocalEventSource.java
@@ -16,14 +16,10 @@
 package org.activiti.test;
 
 import static java.util.Arrays.asList;
-import static java.util.Collections.unmodifiableList;
-import static java.util.stream.Collectors.collectingAndThen;
 
-import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
+import java.util.concurrent.CopyOnWriteArrayList;
 import java.util.stream.Collectors;
-
 import org.activiti.api.model.shared.event.RuntimeEvent;
 import org.activiti.api.process.model.events.BPMNTimerCancelledEvent;
 import org.activiti.api.process.model.events.BPMNTimerFiredEvent;
@@ -33,7 +29,7 @@ import org.activiti.api.task.model.events.TaskRuntimeEvent;
 
 public class LocalEventSource implements EventSource {
 
-    private List<RuntimeEvent<?, ?>> collectedEvents = new ArrayList<>();
+    private List<RuntimeEvent<?, ?>> collectedEvents = new CopyOnWriteArrayList<>();
 
     public void addCollectedEvents(RuntimeEvent<?, ?> event) {
         this.collectedEvents.add(event);
@@ -41,7 +37,7 @@ public class LocalEventSource implements EventSource {
 
     @Override
     public List<RuntimeEvent<?, ?>> getEvents() {
-        return unmodifiableList(collectedEvents);
+        return collectedEvents;
     }
 
     public void clearEvents() {
@@ -53,16 +49,14 @@ public class LocalEventSource implements EventSource {
                 .stream()
                 .filter(eventType::isInstance)
                 .map(eventType::cast)
-                .collect(collectingAndThen(Collectors.toList(),
-                                           Collections::unmodifiableList));
+                .collect(Collectors.toList());
     }
 
-    public List<RuntimeEvent<?, ?>> getEvents(Enum<?> ... eventTypes) {
+    public List<RuntimeEvent<?, ?>> getEvents(Enum<?>... eventTypes) {
         return collectedEvents
                 .stream()
                 .filter(event -> asList(eventTypes).contains(event.getEventType()))
-                .collect(collectingAndThen(Collectors.toList(),
-                                           Collections::unmodifiableList));
+                .collect(Collectors.toList());
 
     }
 
