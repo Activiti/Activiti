b2a1b330023658dededa97118ee7fd0c51742723
Igor Dianov <igor@dianov.com>
2020-09-14 19:35:46 -0700

    Revert "fix(GH-3425): Exception getting ProcessDefinitions with multple versions"
    This reverts commit a3dae80d648595ede7239c78a84860fd7433e48f.

--- activiti-core/activiti-api-impl/activiti-api-process-runtime-impl/src/main/java/org/activiti/runtime/api/impl/ProcessRuntimeImpl.java
+++ activiti-core/activiti-api-impl/activiti-api-process-runtime-impl/src/main/java/org/activiti/runtime/api/impl/ProcessRuntimeImpl.java
@@ -473,7 +473,6 @@ public class ProcessRuntimeImpl implements ProcessRuntime {
         return deploymentConverter.from(
                 repositoryService
                     .createDeploymentQuery()
-                    .latest()
                     .singleResult()
         );
     }
