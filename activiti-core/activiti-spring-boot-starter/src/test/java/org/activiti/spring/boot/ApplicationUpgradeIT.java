da3b3b522aa1ba755f98e8398587b0d2dd2698ac
Elias Ricken de Medeiros <26007058+erdemedeiros@users.noreply.github.com>
2020-10-01 14:53:19 +0200

    Add test reproducing the issue of returning processes deleted in the latest deployment

--- activiti-core/activiti-spring-boot-starter/src/test/java/org/activiti/spring/boot/ApplicationUpgradeIT.java
+++ activiti-core/activiti-spring-boot-starter/src/test/java/org/activiti/spring/boot/ApplicationUpgradeIT.java
@@ -15,13 +15,19 @@
  */
 package org.activiti.spring.boot;
 
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.tuple;
+
 import java.util.ArrayList;
 import java.util.List;
 import org.activiti.api.process.model.ProcessDefinition;
 import org.activiti.api.process.runtime.ProcessRuntime;
+import org.activiti.api.runtime.shared.query.Page;
+import org.activiti.api.runtime.shared.query.Pageable;
 import org.activiti.core.common.project.model.ProjectManifest;
 import org.activiti.engine.RepositoryService;
 import org.activiti.engine.repository.Deployment;
+import org.activiti.engine.repository.DeploymentBuilder;
 import org.activiti.spring.boot.security.util.SecurityUtil;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
@@ -29,14 +35,14 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
-import static org.assertj.core.api.Assertions.assertThat;
-
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.NONE)
 public class ApplicationUpgradeIT {
 
-    private static final String PROCESS_DEFINITION_KEY = "SingleTaskProcess";
+    private static final String SINGLE_TASK_PROCESS_DEFINITION_KEY = "SingleTaskProcess";
     private static final String PROCESS_NAME = "single-task";
     private static final String SINGLE_TASK_PROCESS_DEFINITION_PATH = "processes/SingleTaskProcess.bpmn20.xml";
+    private static final String MULTI_INSTANCE_PROCESS_DEFINITION_PATH = "processes/multi-instance-parallel-all-output-data-ref.bpmn20.xml";
+    private static final String MULTI_INSTANCE_PROCESS_DEFINITION_KEY = "miParallelUserTasksAllOutputCollection";
     private static final String DEPLOYMENT_TYPE_NAME = "SpringAutoDeployment";
 
     @Autowired
@@ -92,19 +98,46 @@ public class ApplicationUpgradeIT {
     public void should_getLatestProcessDefinitionByKey_when_multipleVersions() {
         ProjectManifest projectManifest = new ProjectManifest();
         projectManifest.setVersion("12");
-        deploySingleTaskProcess(projectManifest);
+        deployProcesses(projectManifest, SINGLE_TASK_PROCESS_DEFINITION_PATH);
 
         projectManifest.setVersion("34");
-        deploySingleTaskProcess(projectManifest);
+        Deployment latestDeployment = deployProcesses(projectManifest, SINGLE_TASK_PROCESS_DEFINITION_PATH);
 
         securityUtil.logInAs("user");
 
-        ProcessDefinition result = processRuntime.processDefinition(PROCESS_DEFINITION_KEY);
+        ProcessDefinition result = processRuntime.processDefinition(
+            SINGLE_TASK_PROCESS_DEFINITION_KEY);
 
         assertThat(result).isNotNull();
         assertThat(result.getName()).isEqualTo(PROCESS_NAME);
-        assertThat(result.getId()).contains(PROCESS_DEFINITION_KEY);
-        assertThat(result.getAppVersion()).isEqualTo("2");
+        assertThat(result.getId()).contains(SINGLE_TASK_PROCESS_DEFINITION_KEY);
+        assertThat(result.getAppVersion()).isEqualTo(String.valueOf(latestDeployment.getVersion()));
+
+    }
+
+    @Test
+    public void processDefinitions_should_returnOnlyTheProcessesInTheLatest_when_multipleVersions() {
+        ProjectManifest projectManifest = new ProjectManifest();
+        projectManifest.setVersion("12");
+        deployProcesses(projectManifest, SINGLE_TASK_PROCESS_DEFINITION_PATH,
+            MULTI_INSTANCE_PROCESS_DEFINITION_PATH);
+
+        projectManifest.setVersion("34");
+        Deployment latestDeployment = deployProcesses(projectManifest,
+            MULTI_INSTANCE_PROCESS_DEFINITION_PATH);
+
+        securityUtil.logInAs("user");
+
+        Page<ProcessDefinition> result = processRuntime.processDefinitions(Pageable.of(0, 100));
+        assertThat(result.getContent())
+            .filteredOn(processDefinition -> processDefinition.getKey().equals(
+                SINGLE_TASK_PROCESS_DEFINITION_KEY) || processDefinition.getKey()
+                .equals(MULTI_INSTANCE_PROCESS_DEFINITION_KEY))
+            .extracting(ProcessDefinition::getKey, ProcessDefinition::getVersion,
+                ProcessDefinition::getAppVersion)
+            .containsExactly(
+                tuple(MULTI_INSTANCE_PROCESS_DEFINITION_KEY, latestDeployment.getVersion(),
+                    String.valueOf(latestDeployment.getVersion())));
 
     }
 
@@ -207,15 +240,18 @@ public class ApplicationUpgradeIT {
         assertThat(deployment2.getVersion()).isEqualTo(1);
     }
 
-    private void deploySingleTaskProcess(ProjectManifest projectManifest) {
-        Deployment deployment = repositoryService.createDeployment()
+    private Deployment deployProcesses(ProjectManifest projectManifest, String ... processPaths) {
+        DeploymentBuilder deploymentBuilder = repositoryService.createDeployment()
             .setProjectManifest(projectManifest)
             .enableDuplicateFiltering()
-            .tenantId("tenantId")
-            .name(DEPLOYMENT_TYPE_NAME)
-            .addClasspathResource(SINGLE_TASK_PROCESS_DEFINITION_PATH)
+            .name(DEPLOYMENT_TYPE_NAME);
+        for (String processPath : processPaths) {
+            deploymentBuilder.addClasspathResource(processPath);
+        }
+        Deployment deployment = deploymentBuilder
             .deploy();
         deploymentIds.add(deployment.getId());
+        return deployment;
     }
 
 }
