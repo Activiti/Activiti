464f8e280c1eb3a12ca97f025ab1b1ec6bd695c7
gfertuso <44156864+Giovanni007@users.noreply.github.com>
2020-11-04 13:39:36 +0000

    Activiti/Activiti#3460-Added application deployed event producer (#3471)
    * Activiti/Activiti#3460-Added application deployed event producer
    
    * removed unused import
    
    * minor style fix
    
    * review fixes

---
+++ activiti-core/activiti-spring-boot-starter/src/main/java/org/activiti/spring/ApplicationDeployedEventProducer.java
@@ -0,0 +1,76 @@
+/*
+ * Copyright 2010-2020 Alfresco Software, Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.activiti.spring;
+
+import java.util.ArrayList;
+import java.util.List;
+import org.activiti.api.process.model.Deployment;
+import org.activiti.api.process.model.events.ApplicationDeployedEvent;
+import org.activiti.api.process.runtime.events.listener.ProcessRuntimeEventListener;
+import org.activiti.api.runtime.event.impl.ApplicationDeployedEventImpl;
+import org.activiti.api.runtime.event.impl.ApplicationDeployedEvents;
+import org.activiti.engine.RepositoryService;
+import org.activiti.runtime.api.model.impl.APIDeploymentConverter;
+import org.springframework.context.ApplicationEventPublisher;
+
+public class ApplicationDeployedEventProducer extends AbstractActivitiSmartLifeCycle {
+
+    private RepositoryService repositoryService;
+    private APIDeploymentConverter deploymentConverter;
+    private List<ProcessRuntimeEventListener<ApplicationDeployedEvent>> listeners;
+    private ApplicationEventPublisher eventPublisher;
+    private static final String APPLICATION_DEPLOYMENT_NAME= "SpringAutoDeployment";
+
+    public ApplicationDeployedEventProducer(RepositoryService repositoryService,
+            APIDeploymentConverter deploymentConverter,
+            List<ProcessRuntimeEventListener<ApplicationDeployedEvent>> listeners,
+            ApplicationEventPublisher eventPublisher) {
+        this.repositoryService = repositoryService;
+        this.deploymentConverter = deploymentConverter;
+        this.listeners = listeners;
+        this.eventPublisher = eventPublisher;
+    }
+
+    @Override
+    public void doStart() {
+        List<ApplicationDeployedEvent> applicationDeployedEvents = getApplicationDeployedEvents();
+        for (ProcessRuntimeEventListener<ApplicationDeployedEvent> listener : listeners) {
+            applicationDeployedEvents.forEach(listener::onEvent);
+        }
+        if (!applicationDeployedEvents.isEmpty()) {
+            eventPublisher.publishEvent(new ApplicationDeployedEvents(applicationDeployedEvents));
+        }
+    }
+
+    private List<ApplicationDeployedEvent> getApplicationDeployedEvents() {
+        List<Deployment> deployments = deploymentConverter.from(repositoryService
+                        .createDeploymentQuery()
+                        .deploymentName(APPLICATION_DEPLOYMENT_NAME)
+                        .list());
+
+        List<ApplicationDeployedEvent> applicationDeployedEvents = new ArrayList<>();
+        for (Deployment deployment : deployments) {
+            ApplicationDeployedEventImpl applicationDeployedEvent = new ApplicationDeployedEventImpl(deployment);
+            applicationDeployedEvents.add(applicationDeployedEvent);
+        }
+        return applicationDeployedEvents;
+    }
+
+    @Override
+    public void doStop() {
+        //nothing
+    }
+}
---
+++ activiti-core/activiti-spring-boot-starter/src/test/java/org/activiti/spring/ApplicationDeployedEventProducerTest.java
@@ -0,0 +1,107 @@
+/*
+ * Copyright 2010-2020 Alfresco Software, Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.activiti.spring;
+
+import static java.util.Arrays.asList;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+import static org.mockito.MockitoAnnotations.initMocks;
+
+import java.util.List;
+import org.activiti.api.process.model.events.ApplicationDeployedEvent;
+import org.activiti.api.process.runtime.events.listener.ProcessRuntimeEventListener;
+import org.activiti.api.runtime.event.impl.ApplicationDeployedEvents;
+import org.activiti.engine.RepositoryService;
+import org.activiti.engine.repository.Deployment;
+import org.activiti.engine.repository.DeploymentQuery;
+import org.activiti.runtime.api.model.impl.APIDeploymentConverter;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Mock;
+import org.springframework.context.ApplicationEventPublisher;
+
+public class ApplicationDeployedEventProducerTest { 
+    
+    private ApplicationDeployedEventProducer producer;
+
+    @Mock
+    private RepositoryService repositoryService;
+
+    @Mock
+    private APIDeploymentConverter converter;
+
+    @Mock
+    private ApplicationEventPublisher eventPublisher;
+
+    @Mock
+    private ProcessRuntimeEventListener<ApplicationDeployedEvent> firstListener;
+
+    @Mock
+    private ProcessRuntimeEventListener<ApplicationDeployedEvent> secondListener;
+
+    private static final String APPLICATION_DEPLOYMENT_NAME= "SpringAutoDeployment";
+    
+    @BeforeEach
+    public void setUp() {
+        initMocks(this);
+        producer = new ApplicationDeployedEventProducer(repositoryService,
+                converter,
+                asList(firstListener, secondListener),
+                eventPublisher);
+    }
+
+    @Test
+    public void shouldPublishEventsWhenApplicationIsDeployed() {
+        DeploymentQuery deploymentQuery = mock(DeploymentQuery.class);
+        given(repositoryService.createDeploymentQuery()).willReturn(deploymentQuery);
+
+        List<Deployment> internalDeployment = asList(mock(Deployment.class),
+                mock(Deployment.class));
+        
+        given(deploymentQuery.deploymentName(APPLICATION_DEPLOYMENT_NAME)).willReturn(deploymentQuery);
+        given(deploymentQuery.list()).willReturn(internalDeployment);
+
+        List<org.activiti.api.process.model.Deployment> apiDeployments= asList(
+                buildAPIDeployment("id1", "SpringAutoDeployment"));
+        given(converter.from(internalDeployment)).willReturn(apiDeployments);
+        
+        producer.start();
+        
+        ArgumentCaptor<ApplicationDeployedEvent> captor = ArgumentCaptor.forClass(ApplicationDeployedEvent.class);
+        verify(firstListener).onEvent(captor.capture());
+        verify(secondListener).onEvent(captor.capture());
+
+        List<ApplicationDeployedEvent> allValues = captor.getAllValues();
+        assertThat(allValues)
+                .extracting(ApplicationDeployedEvent::getEntity)
+                .hasSize(2)
+                .containsOnly(apiDeployments.get(0));
+
+        ArgumentCaptor<ApplicationDeployedEvents> captorPublisher = ArgumentCaptor.forClass(ApplicationDeployedEvents.class);
+        verify(eventPublisher).publishEvent(captorPublisher.capture());
+    }
+    
+    private org.activiti.api.process.model.Deployment buildAPIDeployment(String deploymentId, String deploymentName) {
+        org.activiti.api.process.model.Deployment deployment = mock(org.activiti.api.process.model.Deployment.class);
+        given(deployment.getId()).willReturn(deploymentId);
+        given(deployment.getName()).willReturn(deploymentName);
+        return deployment;
+    }
+
+}
