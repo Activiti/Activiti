4c5a89d07774a5f51d62d9df6fba0ee289c9c7b5
Igor Dianov <igor@dianov.com>
2021-02-16 14:16:33 -0800

    fix: add support for default email template

--- activiti-core/activiti-spring-process-extensions/src/test/java/org/activiti/spring/process/ProcessExtensionResourceReaderIT.java
+++ activiti-core/activiti-spring-process-extensions/src/test/java/org/activiti/spring/process/ProcessExtensionResourceReaderIT.java
@@ -15,12 +15,14 @@
  */
 package org.activiti.spring.process;
 
+import static org.activiti.spring.process.model.TemplateDefinition.TemplateType.FILE;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import java.io.InputStream;
 
 import org.activiti.engine.RepositoryService;
 import org.activiti.spring.process.model.ProcessExtensionModel;
+import org.activiti.spring.process.model.TemplateDefinition;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -55,7 +57,10 @@ public class ProcessExtensionResourceReaderIT {
             assertThat(processExtensionModel).isNotNull();
             assertThat(processExtensionModel.getId()).isEqualTo("emailTemplateMapping");
             assertThat(processExtensionModel.getExtensions("processDefinitionId")
-                                            .getTemplates()).hasSize(2);
+                                            .getTemplates()).hasSize(3)
+                                                            .extracting("@")
+                                                            .isEqualTo(new TemplateDefinition(FILE, "classpath:templates/email.html"));
+
         }
     }
 }
