afd48b644aab1393f9cf7adce11f771b838bb3d7
Elias Ricken de Medeiros <26007058+erdemedeiros@users.noreply.github.com>
2021-04-13 06:36:20 +0200

    Allow template definition for assignee and candidate (#3624)
    * Allow template definition for assignee and candidate
    
    Ref https://github.com/Activiti/Activiti/issues/3565
    
    * Fix find methods

--- activiti-core/activiti-spring-process-extensions/src/test/java/org/activiti/spring/process/ProcessExtensionResourceReaderIT.java
+++ activiti-core/activiti-spring-process-extensions/src/test/java/org/activiti/spring/process/ProcessExtensionResourceReaderIT.java
@@ -16,13 +16,15 @@
 package org.activiti.spring.process;
 
 import static org.activiti.spring.process.model.TemplateDefinition.TemplateType.FILE;
+import static org.activiti.spring.process.model.TemplateDefinition.TemplateType.VARIABLE;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import java.io.InputStream;
-
 import org.activiti.engine.RepositoryService;
 import org.activiti.spring.process.model.ProcessExtensionModel;
+import org.activiti.spring.process.model.TaskTemplateDefinition;
 import org.activiti.spring.process.model.TemplateDefinition;
+import org.activiti.spring.process.model.TemplatesDefinition;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -38,29 +40,100 @@ public class ProcessExtensionResourceReaderIT {
     private ProcessExtensionResourceReader reader;
 
     @Test
-    public void shouldReadExtensionFromJsonFile() throws Exception{
-        try(InputStream inputStream = Thread.currentThread().getContextClassLoader().getResourceAsStream("processes/initial-vars-extensions.json")) {
+    public void shouldReadExtensionFromJsonFile() throws Exception {
+        try (InputStream inputStream = Thread.currentThread().getContextClassLoader()
+            .getResourceAsStream("processes/initial-vars-extensions.json")) {
             ProcessExtensionModel processExtensionModel = reader.read(inputStream);
             assertThat(processExtensionModel).isNotNull();
             assertThat(processExtensionModel.getId()).isEqualTo("initialVarsProcess");
-            assertThat(processExtensionModel.getExtensions("Process_initialVarsProcess").getProperties()).containsKey("d440ff7b-0ac8-4a97-b163-51a6ec49faa1");
+            assertThat(
+                processExtensionModel.getExtensions("Process_initialVarsProcess").getProperties())
+                .containsKey("d440ff7b-0ac8-4a97-b163-51a6ec49faa1");
         }
     }
 
     @Test
-    public void shouldReadTemplateExtensionFromJsonFile() throws Exception{
+    public void shouldReadTemplateExtensionFromJsonFile() throws Exception {
         try (InputStream inputStream = Thread.currentThread()
-                                             .getContextClassLoader()
-                                             .getResourceAsStream("processes/template-mapping-extensions.json")) {
+            .getContextClassLoader()
+            .getResourceAsStream("processes/template-mapping-extensions.json")) {
+            //when
             ProcessExtensionModel processExtensionModel = reader.read(inputStream);
 
+            //then
             assertThat(processExtensionModel).isNotNull();
             assertThat(processExtensionModel.getId()).isEqualTo("emailTemplateMapping");
-            assertThat(processExtensionModel.getExtensions("processDefinitionId")
-                                            .getTemplates()).hasSize(3)
-                                                            .extracting("@")
-                                                            .isEqualTo(new TemplateDefinition(FILE, "classpath:templates/email.html"));
 
+            TemplatesDefinition templates = processExtensionModel
+                .getExtensions("processDefinitionId")
+                .getTemplates();
+            TaskTemplateDefinition defaultTemplate = templates.getDefaultTemplate();
+            assertThat(defaultTemplate).isNotNull();
+            assertThat(defaultTemplate.getAssignee())
+                .isNotNull()
+                .extracting(
+                    TemplateDefinition::getType,
+                    TemplateDefinition::getValue)
+                .containsExactly(
+                    FILE,
+                    "classpath:templates/email.html"
+                );
+            assertThat(defaultTemplate.getCandidate())
+                .isNotNull()
+                .extracting(
+                    TemplateDefinition::getType,
+                    TemplateDefinition::getValue
+                ).containsExactly(
+                VARIABLE,
+                "myCandidateTemplateVariable"
+            );
+
+            assertThat(templates.getTasks())
+                .containsOnlyKeys("myTaskId1", "myTaskId2", "myTaskId3");
+
+            assertThat(templates.getTasks().get("myTaskId1").getAssignee())
+                .isNotNull()
+                .extracting(
+                    TemplateDefinition::getType,
+                    TemplateDefinition::getValue)
+                .containsExactly(
+                    FILE,
+                    "https://github.com/leemunroe/responsive-html-email-template/blob/master/email.html"
+                );
+
+            assertThat(templates.getTasks().get("myTaskId1").getCandidate())
+                .isNotNull()
+                .extracting(
+                    TemplateDefinition::getType,
+                    TemplateDefinition::getValue)
+                .containsExactly(
+                    FILE,
+                    "https://github.com/leemunroe/responsive-html-email-template/blob/master/email-inlined.html"
+                );
+
+            assertThat(templates.getTasks().get("myTaskId2").getAssignee())
+                .isNotNull()
+                .extracting(
+                    TemplateDefinition::getType,
+                    TemplateDefinition::getValue)
+                .containsExactly(
+                    VARIABLE,
+                    "myAssigneeTemplateVariable"
+                );
+            assertThat(templates.getTasks().get("myTaskId2").getCandidate())
+                .isNull();
+
+            assertThat(templates.getTasks().get("myTaskId3").getAssignee())
+                .isNull();
+            assertThat(templates.getTasks().get("myTaskId3").getCandidate())
+                .isNotNull()
+                .extracting(
+                    TemplateDefinition::getType,
+                    TemplateDefinition::getValue)
+                .containsExactly(
+                    VARIABLE,
+                    "myCandidateTemplateVariable"
+                );
         }
     }
 }
