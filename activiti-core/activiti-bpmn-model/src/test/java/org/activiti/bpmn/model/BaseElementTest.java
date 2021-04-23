94009786c3686fa0768198f27e5a8e6cda7301fc
fmcejudo <fmcejudo@gmail.com>
2021-04-23 12:06:51 +0200

    Fixing headers

--- activiti-core/activiti-bpmn-model/src/test/java/org/activiti/bpmn/model/BaseElementTest.java
+++ activiti-core/activiti-bpmn-model/src/test/java/org/activiti/bpmn/model/BaseElementTest.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2019 Alfresco, Inc. and/or its affiliates.
+ * Copyright 2010-2020 Alfresco Software, Ltd. 
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
