e46d06972617b56e7d98b40b026fc22700a2cdce
Igor Dianov <igdianov@users.noreply.github.com>
2022-01-18 00:22:41 -0800

    fix: add support for ISO date/time format with offset in string to date converter (#3802)
    * fix: add support for ISO date/time format in string to date converter
    
    * fix: better test name

---
+++ activiti-core/activiti-api-impl/activiti-api-process-model-impl/src/test/java/org/activiti/api/runtime/model/impl/StringToDateConverterTest.java
@@ -0,0 +1,54 @@
+/*
+ * Copyright 2010-2020 Alfresco Software, Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.activiti.api.runtime.model.impl;
+
+import org.junit.jupiter.api.Test;
+
+import java.time.Instant;
+import java.util.Date;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class StringToDateConverterTest {
+
+    private StringToDateConverter subject = new StringToDateConverter();
+
+    @Test
+    void convertISODateTimeUTC() {
+        //given
+        String source = "2022-01-17T00:00:00.000Z";
+
+        //when
+        Date result = subject.convert(source);
+
+        //then
+        assertThat(result).isEqualTo(Instant.parse(source));
+    }
+
+    @Test
+    void convertISODateTimeOffset() {
+        //given
+        String source = "2022-01-17T00:00:00.000-00:00";
+
+        //when
+        Date result = subject.convert(source);
+
+        //then
+        assertThat(result).isEqualTo(Date.from(Instant.parse("2022-01-17T00:00:00.000Z")));
+    }
+
+}
