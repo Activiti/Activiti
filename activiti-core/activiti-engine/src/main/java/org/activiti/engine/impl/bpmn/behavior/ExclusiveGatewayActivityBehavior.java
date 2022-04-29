5405c6258934a6d4f0847f6cf1e77b8362e5574c
Gian Carlo Pace <giancarlo.pace@hyland.com>
2022-04-29 14:13:05 +0200

    chore: modified the project.year in source files

--- activiti-core/activiti-engine/src/main/java/org/activiti/engine/impl/bpmn/behavior/ExclusiveGatewayActivityBehavior.java
+++ activiti-core/activiti-engine/src/main/java/org/activiti/engine/impl/bpmn/behavior/ExclusiveGatewayActivityBehavior.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2010-2020 Alfresco Software, Ltd.
+ * Copyright 2010-2022 Alfresco Software, Ltd.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
