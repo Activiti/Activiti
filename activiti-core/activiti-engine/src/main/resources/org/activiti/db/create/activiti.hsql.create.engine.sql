8a430b0ab3af9d977858fab9f2fed4a49dda24fd
pandaapo <1052156701@qq.com>
2022-09-04 17:56:58 +0800

    Fix the issue #3940:wrong VERSION field in ProcessEngine interface in 7.3.0 release.

--- activiti-core/activiti-engine/src/main/resources/org/activiti/db/create/activiti.hsql.create.engine.sql
+++ activiti-core/activiti-engine/src/main/resources/org/activiti/db/create/activiti.hsql.create.engine.sql
@@ -5,11 +5,16 @@ create table ACT_GE_PROPERTY (
     primary key (NAME_)
 );
 
-insert into ACT_GE_PROPERTY
-values ('schema.version', '7.1.0-M6', 1);
+create procedure init_act_ge_property()
+modifies sql data
+begin atomic
+  declare act_version varchar(300);--
+  set act_version = '';--
+  insert into ACT_GE_PROPERTY values ('schema.version', act_version, 1);--
+  insert into ACT_GE_PROPERTY values ('schema.history', concat('create(', act_version, ')'), 1);--
+end;
 
-insert into ACT_GE_PROPERTY
-values ('schema.history', 'create(7.1.0-M6)', 1);
+call init_act_ge_property();
 
 insert into ACT_GE_PROPERTY
 values ('next.dbid', '1', 1);
