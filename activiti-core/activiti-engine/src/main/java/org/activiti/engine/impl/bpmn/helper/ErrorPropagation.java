a5819ddfdb40d1d7ce0501e993525f58c0464ea3
Igor Dianov <igdianov@users.noreply.github.com>
2023-02-24 01:18:03 -0800

    MNT-23416 fix boundary_event_sub_subprocess error propagtion regression (#4260)
    * MNT-23416 fix boundary_event_sub_subprocess error propagtion regression
    
    * cleanup
    
    * remove unused imports in ErrorPropagation
    
    * remove redundant throw ActivitiException from executeCatch in ErrorPropagation
    
    * improve logic for error propagation catch execution
    
    * update Codaxy warnings

--- activiti-core/activiti-engine/src/main/java/org/activiti/engine/impl/bpmn/helper/ErrorPropagation.java
+++ activiti-core/activiti-engine/src/main/java/org/activiti/engine/impl/bpmn/helper/ErrorPropagation.java
@@ -50,18 +50,27 @@ public class ErrorPropagation {
   }
 
   public static void propagateError(String errorRef, DelegateExecution execution) {
-    Map<String, List<Event>> eventMap = findCatchingEventsForProcess(execution.getProcessDefinitionId(), errorRef);
+    boolean isCatchExecutedForProcess = false;
+    boolean isCatchExecutedForCallActivity = false;
 
     try {
-        executeCatch(eventMap, execution, errorRef);
+        Map<String, List<Event>> eventMap = findCatchingEventsForProcess(execution.getProcessDefinitionId(), errorRef);
+
+        if (!eventMap.isEmpty()) {
+            isCatchExecutedForProcess = executeCatch(eventMap, execution, errorRef);
+        }
+
+        if (isCallActivity(execution)) {
+            isCatchExecutedForCallActivity = findCatchingEventsAndExecuteCatchForCallActivity(errorRef, execution);
+        }
     } finally {
-        if (eventMap.size() == 0) {
+        if (!isCatchExecutedForProcess && !isCatchExecutedForCallActivity) {
             throw new BpmnError(errorRef, "No catching boundary event found for error with errorCode '" + errorRef + "', neither in same process nor in parent process");
         }
     }
   }
 
-  protected static void executeCatch(Map<String, List<Event>> eventMap, DelegateExecution delegateExecution, String errorId) {
+  protected static boolean executeCatch(Map<String, List<Event>> eventMap, DelegateExecution delegateExecution, String errorId) {
     Event matchingEvent = null;
     ExecutionEntity currentExecution = (ExecutionEntity) delegateExecution;
     ExecutionEntity parentExecution = null;
@@ -117,13 +126,11 @@ public class ErrorPropagation {
 
     if (matchingEvent != null && parentExecution != null) {
       executeEventHandler(matchingEvent, parentExecution, currentExecution, errorId);
+      return true;
+    } else {
+      return false;
     }
-    else if (isCallActivity(delegateExecution)) {
-      eventMap.putAll(findCatchingEventsAndExecuteCatchForCallActivity(errorId, delegateExecution));
-    }
-    else {
-      throw new ActivitiException("No matching parent execution for error code " + errorId + " found");
-    }
+
   }
 
     private static boolean isCallActivity(DelegateExecution delegateExecution) {
@@ -131,7 +138,7 @@ public class ErrorPropagation {
             .equals(delegateExecution.getRootProcessInstanceId());
     }
 
-    protected static Map<String, List<Event>> findCatchingEventsAndExecuteCatchForCallActivity(String errorRef, DelegateExecution execution) {
+    protected static boolean findCatchingEventsAndExecuteCatchForCallActivity(String errorRef, DelegateExecution execution) {
       ExecutionEntityManager executionEntityManager = Context.getCommandContext().getExecutionEntityManager();
       ExecutionEntity processInstanceExecution = executionEntityManager.findById(execution.getProcessInstanceId());
 
@@ -149,7 +156,7 @@ public class ErrorPropagation {
                   for (String processInstanceId : toDeleteProcessInstanceIds) {
                       deleteProcessInstanceEntity(errorRef, execution, executionEntityManager, processInstanceId);
                   }
-                  executeCatch(eventMap, parentExecution, errorRef);
+                  return executeCatch(eventMap, parentExecution, errorRef);
               } else {
                   toDeleteProcessInstanceIds.add(parentExecution.getProcessInstanceId());
                   ExecutionEntity superExecution = parentExecution.getSuperExecution();
@@ -162,7 +169,7 @@ public class ErrorPropagation {
           }
       }
 
-      return eventMap;
+      return false;
   }
 
     private static void deleteProcessInstanceEntity(String errorRef, DelegateExecution execution,
