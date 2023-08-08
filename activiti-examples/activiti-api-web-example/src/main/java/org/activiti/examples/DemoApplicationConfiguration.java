4b6c87d2511f2798913a9da73870ae2abcc1d18c
RahulKumar1606 <rahul.kumar@hyland.com>
2023-08-08 13:44:48 +0530

    [AAE-15708] Fix deprecated warning

--- activiti-examples/activiti-api-web-example/src/main/java/org/activiti/examples/DemoApplicationConfiguration.java
+++ activiti-examples/activiti-api-web-example/src/main/java/org/activiti/examples/DemoApplicationConfiguration.java
@@ -67,7 +67,7 @@ public class DemoApplicationConfiguration {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         return http
-            .csrf().disable()
+            .csrf(csrf -> csrf.disable())
             .authorizeHttpRequests(auth -> auth.anyRequest().authenticated())
             .httpBasic(withDefaults())
             .build();
