c531cacc908c8fa3b0652465fb20926a9be3cbd5
Abhinay <abhin.kumar@samsung.com>
2023-08-22 11:59:58 +0530

    Refactored tests

--- activiti-core/activiti-engine/src/test/java/org/activiti/engine/impl/util/ProcessInstanceHelperTest.java
+++ activiti-core/activiti-engine/src/test/java/org/activiti/engine/impl/util/ProcessInstanceHelperTest.java
@@ -5,6 +5,7 @@ import org.activiti.bpmn.model.BpmnModel;
 import org.activiti.bpmn.model.FlowElement;
 import org.activiti.bpmn.model.Process;
 import org.activiti.engine.impl.persistence.entity.ProcessDefinitionEntityImpl;
+import org.activiti.engine.impl.util.json.XML;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
@@ -12,6 +13,7 @@ import org.mockito.MockedStatic;
 import javax.xml.stream.XMLInputFactory;
 import javax.xml.stream.XMLStreamException;
 import javax.xml.stream.XMLStreamReader;
+import java.io.IOException;
 import java.io.InputStream;
 
 import static org.mockito.ArgumentMatchers.any;
@@ -27,6 +29,10 @@ public class ProcessInstanceHelperTest {
 
     private BpmnXMLConverter converter;
 
+    private InputStream fin;
+
+    private XMLStreamReader reader;
+
 
     @Before
     public void setup() throws XMLStreamException {
@@ -41,76 +47,109 @@ public class ProcessInstanceHelperTest {
     }
 
     @Test
-    public void testInitialFlowByMessageForEvent1() throws XMLStreamException {
+    public void testInitialFlowByMessageForEvent1() throws XMLStreamException, IOException {
         String MESSAGE_NAME = "event1";
         String allMessagesExistPath = "/org/activiti/engine/test/impl/util/StartProcessInstanceByMessage.testInitialFlowElementByMessage.bpmn20.xml";
-        InputStream fin = this.getClass().getResourceAsStream(allMessagesExistPath);
-        XMLStreamReader reader = XMLInputFactory.newInstance().createXMLStreamReader(fin);
-        BpmnModel bpmnModel = converter.convertToBpmnModel(reader);
-        Process process = bpmnModel.getProcessById("Process_1");
-        mockedStatic.when(() -> ProcessDefinitionUtil.getBpmnModel(any())).thenReturn(bpmnModel);
-        FlowElement initialFlowElementByMessage = processInstancehelper.getInitialFlowElementByMessage(process, new ProcessDefinitionEntityImpl(), MESSAGE_NAME);
-        assertThat(initialFlowElementByMessage).isNotNull();
-        assertThat(initialFlowElementByMessage.getName()).isEqualTo("start1");
-        reader.close();
+        try{
+            fin = this.getClass().getResourceAsStream(allMessagesExistPath);
+            reader = XMLInputFactory.newInstance().createXMLStreamReader(fin);
+            BpmnModel bpmnModel = converter.convertToBpmnModel(reader);
+            Process process = bpmnModel.getProcessById("Process_1");
+            mockedStatic.when(() -> ProcessDefinitionUtil.getBpmnModel(any())).thenReturn(bpmnModel);
+            FlowElement initialFlowElementByMessage = processInstancehelper.getInitialFlowElementByMessage(process, new ProcessDefinitionEntityImpl(), MESSAGE_NAME);
+            assertThat(initialFlowElementByMessage).isNotNull();
+            assertThat(initialFlowElementByMessage.getName()).isEqualTo("start1");
+        }catch(XMLStreamException e){
+            throw e;
+        }finally {
+            fin.close();
+            reader.close();
+        }
     }
 
     @Test
-    public void testInitialFlowByMessageForEvent2() throws XMLStreamException {
+    public void testInitialFlowByMessageForEvent2() throws XMLStreamException, IOException {
         String MESSAGE_NAME = "event2";
         String allMessagesExistPath = "/org/activiti/engine/test/impl/util/StartProcessInstanceByMessage.testInitialFlowElementByMessage.bpmn20.xml";
-        InputStream fin = this.getClass().getResourceAsStream(allMessagesExistPath);
-        XMLStreamReader reader = XMLInputFactory.newInstance().createXMLStreamReader(fin);
-        BpmnModel bpmnModel = converter.convertToBpmnModel(reader);
-        Process process = bpmnModel.getProcessById("Process_1");
-        mockedStatic.when(() -> ProcessDefinitionUtil.getBpmnModel(any())).thenReturn(bpmnModel);
-        FlowElement initialFlowElementByMessage = processInstancehelper.getInitialFlowElementByMessage(process, new ProcessDefinitionEntityImpl(), MESSAGE_NAME);
-        assertThat(initialFlowElementByMessage).isNotNull();
-        assertThat(initialFlowElementByMessage.getName()).isEqualTo("start2");
-        System.out.println(initialFlowElementByMessage.getName());
+        try {
+            fin = this.getClass().getResourceAsStream(allMessagesExistPath);
+            reader = XMLInputFactory.newInstance().createXMLStreamReader(fin);
+            BpmnModel bpmnModel = converter.convertToBpmnModel(reader);
+            Process process = bpmnModel.getProcessById("Process_1");
+            mockedStatic.when(() -> ProcessDefinitionUtil.getBpmnModel(any())).thenReturn(bpmnModel);
+            FlowElement initialFlowElementByMessage = processInstancehelper.getInitialFlowElementByMessage(process, new ProcessDefinitionEntityImpl(), MESSAGE_NAME);
+            assertThat(initialFlowElementByMessage).isNotNull();
+            assertThat(initialFlowElementByMessage.getName()).isEqualTo("start2");
+        }catch(XMLStreamException e){
+            throw e;
+        }finally {
+            fin.close();
+            reader.close();
+        }
     }
 
     // When messageName passed does not match any messageRef
     @Test
-    public void testInitialFlowByMessageForMessageNameNotPresentInMessageRefs() throws XMLStreamException {
+    public void testInitialFlowByMessageForMessageNameNotPresentInMessageRefs() throws XMLStreamException, IOException {
         String allMessagesExistPath = "/org/activiti/engine/test/impl/util/StartProcessInstanceByMessage.testInitialFlowElementByMessage.bpmn20.xml";
         String MESSAGE_NAME = "eventinvalid";
-        InputStream fin = this.getClass().getResourceAsStream(allMessagesExistPath);
-        XMLStreamReader reader = XMLInputFactory.newInstance().createXMLStreamReader(fin);
-        BpmnModel bpmnModel = converter.convertToBpmnModel(reader);
-        Process process = bpmnModel.getProcessById("Process_1");
-        mockedStatic.when(() -> ProcessDefinitionUtil.getBpmnModel(any())).thenReturn(bpmnModel);
-        FlowElement initialFlowElementByMessage = processInstancehelper.getInitialFlowElementByMessage(process, new ProcessDefinitionEntityImpl(), MESSAGE_NAME);
-        assertThat(initialFlowElementByMessage).isNotNull();
-        assertThat(initialFlowElementByMessage.getName()).isEqualTo("start1");
+        try {
+            fin = this.getClass().getResourceAsStream(allMessagesExistPath);
+            reader = XMLInputFactory.newInstance().createXMLStreamReader(fin);
+            BpmnModel bpmnModel = converter.convertToBpmnModel(reader);
+            Process process = bpmnModel.getProcessById("Process_1");
+            mockedStatic.when(() -> ProcessDefinitionUtil.getBpmnModel(any())).thenReturn(bpmnModel);
+            FlowElement initialFlowElementByMessage = processInstancehelper.getInitialFlowElementByMessage(process, new ProcessDefinitionEntityImpl(), MESSAGE_NAME);
+            assertThat(initialFlowElementByMessage).isNotNull();
+            assertThat(initialFlowElementByMessage.getName()).isEqualTo("start1");
+        }catch(XMLStreamException e){
+            throw e;
+        }finally {
+            fin.close();
+            reader.close();
+        }
     }
 
     @Test
-    public void testInitialFlowByMessageForNoMessageExists() throws XMLStreamException {
+    public void testInitialFlowByMessageForNoMessageExists() throws XMLStreamException, IOException {
         String noMessagesExistPath = "/org/activiti/engine/test/impl/util/StartProcessInstanceByMessage.testNoMessagePresent.bpmn20.xml";
         String MESSAGE_NAME = "eventinvalid";
-        InputStream fin = this.getClass().getResourceAsStream(noMessagesExistPath);
-        XMLStreamReader reader = XMLInputFactory.newInstance().createXMLStreamReader(fin);
-        BpmnModel bpmnModel = converter.convertToBpmnModel(reader);
-        Process process = bpmnModel.getProcessById("Process_No_Message");
-        mockedStatic.when(() -> ProcessDefinitionUtil.getBpmnModel(any())).thenReturn(bpmnModel);
-        FlowElement initialFlowElementByMessage = processInstancehelper.getInitialFlowElementByMessage(process, new ProcessDefinitionEntityImpl(), MESSAGE_NAME);
-        assertThat(initialFlowElementByMessage).isNull();
+        try {
+            fin = this.getClass().getResourceAsStream(noMessagesExistPath);
+            reader = XMLInputFactory.newInstance().createXMLStreamReader(fin);
+            BpmnModel bpmnModel = converter.convertToBpmnModel(reader);
+            Process process = bpmnModel.getProcessById("Process_No_Message");
+            mockedStatic.when(() -> ProcessDefinitionUtil.getBpmnModel(any())).thenReturn(bpmnModel);
+            FlowElement initialFlowElementByMessage = processInstancehelper.getInitialFlowElementByMessage(process, new ProcessDefinitionEntityImpl(), MESSAGE_NAME);
+            assertThat(initialFlowElementByMessage).isNull();
+        }catch(XMLStreamException e){
+            throw e;
+        }finally {
+            fin.close();
+            reader.close();
+        }
     }
 
 
     @Test
-    public void testInitialFlowByMessageForOnlyOneMessageExist() throws XMLStreamException {
+    public void testInitialFlowByMessageForOnlyOneMessageExist() throws XMLStreamException, IOException {
         String noMessagesExistPath = "/org/activiti/engine/test/impl/util/StartProcessInstanceByMessage.testOnlyOneMessageExist.bpmn20.xml";
         String MESSAGE_NAME = "eventinvalid";
-        InputStream fin = this.getClass().getResourceAsStream(noMessagesExistPath);
-        XMLStreamReader reader = XMLInputFactory.newInstance().createXMLStreamReader(fin);
-        BpmnModel bpmnModel = converter.convertToBpmnModel(reader);
-        Process process = bpmnModel.getProcessById("Process_One_Message");
-        mockedStatic.when(() -> ProcessDefinitionUtil.getBpmnModel(any())).thenReturn(bpmnModel);
-        FlowElement initialFlowElementByMessage = processInstancehelper.getInitialFlowElementByMessage(process, new ProcessDefinitionEntityImpl(), MESSAGE_NAME);
-        assertThat(initialFlowElementByMessage).isNotNull();
-        assertThat(initialFlowElementByMessage.getName()).isEqualTo("start2");
+        try {
+            fin = this.getClass().getResourceAsStream(noMessagesExistPath);
+            reader = XMLInputFactory.newInstance().createXMLStreamReader(fin);
+            BpmnModel bpmnModel = converter.convertToBpmnModel(reader);
+            Process process = bpmnModel.getProcessById("Process_One_Message");
+            mockedStatic.when(() -> ProcessDefinitionUtil.getBpmnModel(any())).thenReturn(bpmnModel);
+            FlowElement initialFlowElementByMessage = processInstancehelper.getInitialFlowElementByMessage(process, new ProcessDefinitionEntityImpl(), MESSAGE_NAME);
+            assertThat(initialFlowElementByMessage).isNotNull();
+            assertThat(initialFlowElementByMessage.getName()).isEqualTo("start2");
+        }catch(XMLStreamException e){
+            throw e;
+        }finally {
+            fin.close();
+            reader.close();
+        }
     }
 
 
