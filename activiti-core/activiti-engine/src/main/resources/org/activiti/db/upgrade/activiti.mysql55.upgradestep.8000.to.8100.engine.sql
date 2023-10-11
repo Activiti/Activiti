edb92846051eab997eab8c02085aeddc7d005ec7
Igor Dianov <igor@dianov.com>
2023-10-11 08:54:49 -0700

    Add DbSchemaUpdateTest to latest version

--- activiti-core/activiti-engine/src/main/resources/org/activiti/db/upgrade/activiti.mysql55.upgradestep.8000.to.8100.engine.sql
+++
@@ -1,4 +0,0 @@
-update ACT_GE_PROPERTY set VALUE_ = '8.1.0' where NAME_ = 'schema.version';
-
-alter table ACT_RU_IDENTITY_LINK add column DETAILS_ LONGBLOB;
-alter table ACT_HI_IDENTITYLINK add column DETAILS_ LONGBLOB;
