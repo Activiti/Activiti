aaf62472c35cbbcab5c6292f2650231919151cb1
Pablo Martinez Garcia <pmartinezga@gmail.com>
2023-12-12 10:59:42 +0100

    [AAE-16482] Add single and multiple content

--- activiti-core/activiti-spring-process-extensions/src/main/java/org/activiti/spring/process/conf/ProcessExtensionsAutoConfiguration.java
+++ activiti-core/activiti-spring-process-extensions/src/main/java/org/activiti/spring/process/conf/ProcessExtensionsAutoConfiguration.java
@@ -90,6 +90,8 @@ public class ProcessExtensionsAutoConfiguration {
         variableTypeMap.put("file", new JsonObjectVariableType(objectMapper));
         variableTypeMap.put("folder", new JsonObjectVariableType(objectMapper));
         variableTypeMap.put("content", new JsonObjectVariableType(objectMapper));
+        variableTypeMap.put("single-content", new JsonObjectVariableType(objectMapper));
+        variableTypeMap.put("multiple-content", new JsonObjectVariableType(objectMapper));
         variableTypeMap.put("date", new DateVariableType(Date.class, dateFormatterProvider));
         variableTypeMap.put("datetime", new DateVariableType(Date.class, dateFormatterProvider));
         variableTypeMap.put("array", new JsonObjectVariableType(objectMapper));
