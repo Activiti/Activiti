c14cc6c5d7ef19e2b43b21fdb2d99bb6dc8f8431
Akhilesh Pamidimarthi <akhilesh.pamidimarthi@hyland.com>
2024-05-28 16:19:23 +0530

    Fixed PR comments

--- activiti-core/activiti-process-validation/src/main/java/org/activiti/validation/validator/impl/FlowElementValidator.java
+++ activiti-core/activiti-process-validation/src/main/java/org/activiti/validation/validator/impl/FlowElementValidator.java
@@ -37,74 +37,74 @@ import org.apache.commons.lang3.StringUtils;
  */
 public class FlowElementValidator extends ProcessLevelValidator {
 
-	protected static final int ID_MAX_LENGTH = 255;
+    protected static final int ID_MAX_LENGTH = 255;
 
-	@Override
-	protected void executeValidation(BpmnModel bpmnModel, Process process, List<ValidationError> errors) {
-		for (FlowElement flowElement : process.getFlowElements()) {
+    @Override
+    protected void executeValidation(BpmnModel bpmnModel, Process process, List<ValidationError> errors) {
+        for (FlowElement flowElement : process.getFlowElements()) {
 
-			if (flowElement instanceof Activity) {
-				Activity activity = (Activity) flowElement;
-				handleConstraints(process, activity, errors);
-				handleMultiInstanceLoopCharacteristics(process, activity, errors);
-				handleDataAssociations(process, activity, errors);
-			}
+            if (flowElement instanceof Activity) {
+                Activity activity = (Activity) flowElement;
+                handleConstraints(process, activity, errors);
+                handleMultiInstanceLoopCharacteristics(process, activity, errors);
+                handleDataAssociations(process, activity, errors);
+            }
 
-		}
+        }
 
-	}
+    }
 
-	protected void handleConstraints(Process process, Activity activity, List<ValidationError> errors) {
-		if (activity.getId() != null && activity.getId().length() > ID_MAX_LENGTH) {
-			addError(errors, Problems.FLOW_ELEMENT_ID_TOO_LONG, process, activity,
-					"The id of a flow element must not contain more than " + ID_MAX_LENGTH + " characters");
-		}
-	}
+    protected void handleConstraints(Process process, Activity activity, List<ValidationError> errors) {
+        if (activity.getId() != null && activity.getId().length() > ID_MAX_LENGTH) {
+            addError(errors, Problems.FLOW_ELEMENT_ID_TOO_LONG, process, activity,
+                "The id of a flow element must not contain more than " + ID_MAX_LENGTH + " characters");
+        }
+    }
 
-	protected void handleMultiInstanceLoopCharacteristics(Process process, Activity activity, List<ValidationError> errors) {
-		MultiInstanceLoopCharacteristics multiInstanceLoopCharacteristics = activity.getLoopCharacteristics();
-		if (multiInstanceLoopCharacteristics != null) {
+    protected void handleMultiInstanceLoopCharacteristics(Process process, Activity activity, List<ValidationError> errors) {
+        MultiInstanceLoopCharacteristics multiInstanceLoopCharacteristics = activity.getLoopCharacteristics();
+        if (multiInstanceLoopCharacteristics != null) {
 
-			if (StringUtils.isEmpty(multiInstanceLoopCharacteristics.getLoopCardinality())
-	    		&& StringUtils.isEmpty(multiInstanceLoopCharacteristics.getInputDataItem())) {
+            if (StringUtils.isEmpty(multiInstanceLoopCharacteristics.getLoopCardinality())
+                && StringUtils.isEmpty(multiInstanceLoopCharacteristics.getInputDataItem())) {
 
-			  addError(errors, Problems.MULTI_INSTANCE_MISSING_COLLECTION, process, activity,
-	    			"Either loopCardinality or loopDataInputRef/activiti:collection must been set");
-	    }
+                addError(errors, Problems.MULTI_INSTANCE_MISSING_COLLECTION, process, activity,
+                    "Either loopCardinality or loopDataInputRef/activiti:collection must been set");
+            }
             if(!StringUtils.isEmpty(multiInstanceLoopCharacteristics.getLoopCardinality())){
                 try {
                     int loopCardinality=  Integer.parseInt(multiInstanceLoopCharacteristics.getLoopCardinality());
                     if(loopCardinality >200){
-                        addError(errors, Problems.MULTI_INSTANCE_HIGHER_CARDINALITY, process, activity,
+                        addWarning(errors, Problems.MULTI_INSTANCE_HIGHER_CARDINALITY, process, activity,
                             "Because you set a parallel cardinality greater than 200 for a task, the operation could be slow or does not terminate successfully");
                     }
                 } catch (NumberFormatException e) {
-
-                }
+                    System.out.println("LoopCardinality should be a number");
+                    }
 
 
             }
 
-		}
-	}
+        }
+    }
 
-	protected void handleDataAssociations(Process process, Activity activity, List<ValidationError> errors) {
-	  if (activity.getDataInputAssociations() != null) {
-	  	for (DataAssociation dataAssociation : activity.getDataInputAssociations()) {
-	  		if (StringUtils.isEmpty(dataAssociation.getTargetRef())) {
-	  			 addError(errors, Problems.DATA_ASSOCIATION_MISSING_TARGETREF, process, activity,
-	  					 "Targetref is required on a data association");
-	  	    }
-	  	}
-	  }
-	  if (activity.getDataOutputAssociations() != null) {
-	  	for (DataAssociation dataAssociation : activity.getDataOutputAssociations()) {
-	  		if (StringUtils.isEmpty(dataAssociation.getTargetRef())) {
-	  			 addError(errors, Problems.DATA_ASSOCIATION_MISSING_TARGETREF, process, activity,
-	  					 "Targetref is required on a data association");
-	  	    }
-	  	}
-	  }
+    protected void handleDataAssociations(Process process, Activity activity, List<ValidationError> errors) {
+        if (activity.getDataInputAssociations() != null) {
+            for (DataAssociation dataAssociation : activity.getDataInputAssociations()) {
+                if (StringUtils.isEmpty(dataAssociation.getTargetRef())) {
+                    addError(errors, Problems.DATA_ASSOCIATION_MISSING_TARGETREF, process, activity,
+                        "Targetref is required on a data association");
+                }
+            }
+        }
+        if (activity.getDataOutputAssociations() != null) {
+            for (DataAssociation dataAssociation : activity.getDataOutputAssociations()) {
+                if (StringUtils.isEmpty(dataAssociation.getTargetRef())) {
+                    addError(errors, Problems.DATA_ASSOCIATION_MISSING_TARGETREF, process, activity,
+                        "Targetref is required on a data association");
+                }
+            }
+        }
   }
 
 }
