29cba150cfa13fd285476831cf75947bf570a7db
jesty <davidecerbo@gmail.com>
2024-07-29 10:27:18 +0200

    declase as a bean

--- activiti-core/activiti-spring-boot-starter/src/main/java/org/activiti/spring/boot/ShutdownListener.java
+++ activiti-core/activiti-spring-boot-starter/src/main/java/org/activiti/spring/boot/ShutdownListener.java
@@ -20,16 +20,10 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.context.ApplicationListener;
 import org.springframework.context.event.ContextClosedEvent;
-import org.springframework.stereotype.Component;
 
-@Component
 public class ShutdownListener implements ApplicationListener<ContextClosedEvent> {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(ShutdownListener.class);
-
-    ShutdownListener() {
-        System.out.println("Shutdown listener");
-    }
 
     @Override
     public void onApplicationEvent(ContextClosedEvent event) {
