0f3adff5a7795c92ec86f6e9ddf001b9151f42a0
Vitor Moreira <vitor.moreira-v@alfresco.com>
2024-08-05 19:55:28 +0100

    MNT-24335: increased timeout for job execution

--- activiti-core/activiti-engine/src/test/java/org/activiti/engine/test/bpmn/event/timer/StartTimerEventRepeatWithEndExpressionTest.java
+++ activiti-core/activiti-engine/src/test/java/org/activiti/engine/test/bpmn/event/timer/StartTimerEventRepeatWithEndExpressionTest.java
@@ -98,7 +98,7 @@ public class StartTimerEventRepeatWithEndExpressionTest extends PluggableActivit
     // advance the clock to 11 dec -> the system will execute the pending
     // job and will create a new one
     moveByMinutes(60 * 24);
-    waitForJobExecutorToProcessAllJobsAndExecutableTimerJobs(2000L, 200);
+    waitForJobExecutorToProcessAllJobsAndExecutableTimerJobs(20000L, 2000);
 
     // there must be a pending job because the endDate is not reached yet");
     assertThat(managementService.createTimerJobQuery().count()).isEqualTo(1);
