dc86ca1b9600388b5e44182f917981f737d7fff4
revatilimaye <revati.limaye@hyland.com>
2024-12-09 22:06:17 +0530

    AAE-28995 add svg path for arrow

--- activiti-core/activiti-image-generator/src/main/java/org/activiti/image/impl/icon/LinkCatchIconType.java
+++ activiti-core/activiti-image-generator/src/main/java/org/activiti/image/impl/icon/LinkCatchIconType.java
@@ -1,13 +1,76 @@
 package org.activiti.image.impl.icon;
 
-public class LinkCatchIconType extends TaskIconType{
+import org.activiti.image.impl.ProcessDiagramSVGGraphics2D;
+import org.apache.batik.svggen.SVGGraphics2D;
+import org.w3c.dom.Element;
+
+public class LinkCatchIconType extends IconType{
     @Override
-    public String getStyleValue() {
-        return "";
+    public String getFillValue() {
+        return "none";
+    }
+
+    @Override
+    public String getStrokeValue() {
+        return "#585858";
     }
 
     @Override
     public String getDValue() {
-        return "";
+        return "M 0 0 L 0 0 M 4 4 L 8 4 L 8 3 L 10 5 L 8 7 L 8 6 L 4 6 L 4 4 z";
+    }
+
+    public void drawIcon(final int imageX,
+                         final int imageY,
+                         final int iconPadding,
+                         final ProcessDiagramSVGGraphics2D svgGenerator) {
+        Element gTag = svgGenerator.getDOMFactory().createElementNS(null,
+            SVGGraphics2D.SVG_G_TAG);
+        gTag.setAttributeNS(null,
+            "transform",
+            "translate(" + (imageX - 7) + "," + (imageY - 7) + ")");
+
+        Element pathTag = svgGenerator.getDOMFactory().createElementNS(null,
+            SVGGraphics2D.SVG_PATH_TAG);
+        pathTag.setAttributeNS(null,
+            "d",
+            this.getDValue());
+        pathTag.setAttributeNS(null,
+            "style",
+            this.getStyleValue());
+        pathTag.setAttributeNS(null,
+            "fill",
+            this.getFillValue());
+        pathTag.setAttributeNS(null,
+            "stroke",
+            this.getStrokeValue());
+
+        gTag.appendChild(pathTag);
+        svgGenerator.getExtendDOMGroupManager().addElement(gTag);
+    }
+
+    @Override
+    public String getAnchorValue() {
+        return null;
+    }
+
+    @Override
+    public String getStyleValue() {
+        return "fill:none;stroke-width:1.4;stroke-miterlimit:4;stroke-dasharray:none";
+    }
+
+    @Override
+    public Integer getWidth() {
+        return 17;
+    }
+
+    @Override
+    public Integer getHeight() {
+        return 15;
+    }
+
+    @Override
+    public String getStrokeWidth() {
+        return null;
     }
 }
--- activiti-core/activiti-image-generator/src/main/java/org/activiti/image/impl/icon/LinkThrowIconType.java
+++ activiti-core/activiti-image-generator/src/main/java/org/activiti/image/impl/icon/LinkThrowIconType.java
@@ -1,13 +1,77 @@
 package org.activiti.image.impl.icon;
 
-public class LinkThrowIconType extends TaskIconType{
+import org.activiti.image.impl.ProcessDiagramSVGGraphics2D;
+import org.apache.batik.svggen.SVGGraphics2D;
+import org.w3c.dom.Element;
+
+public class LinkThrowIconType extends IconType{
+    @Override
+    public Integer getWidth() {
+        return 17;
+    }
+
+    @Override
+    public Integer getHeight() {
+        return 15;
+    }
+
+    @Override
+    public String getAnchorValue() {
+        return null;
+    }
+
+    @Override
+    public String getFillValue() {
+        return "#585858";
+    }
+
     @Override
     public String getStyleValue() {
-        return "";
+        return "stroke-width:1.4;stroke-miterlimit:4;stroke-dasharray:none";
     }
 
     @Override
     public String getDValue() {
-        return "";
+        return "M 0 0 L 0 0 M 4 4 L 8 4 L 8 3 L 10 5 L 8 7 L 8 6 L 4 6 L 4 4 z";
+    }
+
+    @Override
+    public void drawIcon(int imageX,
+                         int imageY,
+                         int iconPadding,
+                         ProcessDiagramSVGGraphics2D svgGenerator) {
+        Element gTag = svgGenerator.getDOMFactory().createElementNS(null,
+            SVGGraphics2D.SVG_G_TAG);
+        gTag.setAttributeNS(null,
+            "transform",
+            "translate(" + (imageX - 7) + "," + (imageY - 7) + ")");
+
+        Element pathTag = svgGenerator.getDOMFactory().createElementNS(null,
+            SVGGraphics2D.SVG_PATH_TAG);
+        pathTag.setAttributeNS(null,
+            "d",
+            this.getDValue());
+        pathTag.setAttributeNS(null,
+            "style",
+            this.getStyleValue());
+        pathTag.setAttributeNS(null,
+            "fill",
+            this.getFillValue());
+        pathTag.setAttributeNS(null,
+            "stroke",
+            this.getStrokeValue());
+
+        gTag.appendChild(pathTag);
+        svgGenerator.getExtendDOMGroupManager().addElement(gTag);
+    }
+
+    @Override
+    public String getStrokeValue() {
+        return "#585858";
+    }
+
+    @Override
+    public String getStrokeWidth() {
+        return null;
     }
 }
