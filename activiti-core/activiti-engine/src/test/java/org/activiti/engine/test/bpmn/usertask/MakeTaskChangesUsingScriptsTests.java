912d286cbf8632dfad5b8ae23d03a15638edaea8
Vitor Moreira <vitor.moreira-v@alfresco.com>
2025-02-04 13:07:41 +0000

    MNT-24839: reformatting tests for better understanding

--- activiti-core/activiti-engine/src/test/java/org/activiti/engine/test/bpmn/usertask/MakeTaskChangesUsingScriptsTests.java
+++ activiti-core/activiti-engine/src/test/java/org/activiti/engine/test/bpmn/usertask/MakeTaskChangesUsingScriptsTests.java
@@ -26,9 +26,6 @@ import java.util.Date;
 import static org.assertj.core.api.Assertions.assertThat;
 
 
-/**
-
- */
 public class MakeTaskChangesUsingScriptsTests extends PluggableActivitiTestCase {
 
   @Deployment(resources={"org/activiti/engine/test/bpmn/usertask/MakeTaskChangesUsingScripts.bpmn20.xml"})
@@ -42,39 +39,51 @@ public class MakeTaskChangesUsingScriptsTests extends PluggableActivitiTestCase 
     HistoricTaskInstance historicTask = historyService.createHistoricTaskInstanceQuery().processInstanceId(processInstanceId).singleResult();
 
     // THEN: runtime-task should have the correct values and should be in-sync with historical-task
-    assertThat(task.getName()).isEqualTo(historicTask.getName()).isEqualTo("UserTask.name1");
-    assertThat(task.getDescription()).isEqualTo(historicTask.getDescription()).isEqualTo("UserTask.description1");
-    assertThat(task.getCategory()).isEqualTo(historicTask.getCategory()).isEqualTo("UserTask.category1");
-    assertThat(task.getFormKey()).isEqualTo(historicTask.getFormKey()).isEqualTo("UserTask.formKey1");
-    assertThat(task.getAssignee()).isEqualTo(historicTask.getAssignee()).isEqualTo("UserTask.assignee1");
-    assertThat(task.getOwner()).isEqualTo(historicTask.getOwner()).isEqualTo("UserTask.owner1");
+    assertThat(task.getName()).isEqualTo(historicTask.getName()).isEqualTo("name from script");
+    assertThat(task.getDescription()).isEqualTo(historicTask.getDescription()).isEqualTo("description from script");
+    assertThat(task.getCategory()).isEqualTo(historicTask.getCategory()).isEqualTo("category from script");
+    assertThat(task.getFormKey()).isEqualTo(historicTask.getFormKey()).isEqualTo("formKey from script");
+    assertThat(task.getAssignee()).isEqualTo(historicTask.getAssignee()).isEqualTo("assignee from script");
+    assertThat(task.getOwner()).isEqualTo(historicTask.getOwner()).isEqualTo("owner from script");
     assertThat(task.getPriority()).isEqualTo(historicTask.getPriority()).isEqualTo(1);
     assertThat(task.getDueDate()).isEqualTo(historicTask.getDueDate()).isEqualTo(new Date(1));
-
-    // WHEN: changing the values of the task and saving the task
-    task.setName("UserTask.name2");
-    task.setOwner("UserTask.owner2");
-    task.setDescription("UserTask.description2");
-    task.setCategory("UserTask.category2");
-    task.setFormKey("UserTask.formKey2");
-    task.setAssignee("UserTask.assignee2");
-    task.setPriority(2);
-    task.setDueDate(new Date(2));
-    taskService.saveTask(task);
-
-    // WHEN: fetching the information from the current task (both runtime and historical)
-    task = taskService.createTaskQuery().processInstanceId(processInstanceId).singleResult();
-    historicTask = historyService.createHistoricTaskInstanceQuery().singleResult();
-
-    // THEN: runtime-task should have the correct values and should be in-sync with historical-task
-    assertThat(task.getName()).isEqualTo(historicTask.getName()).isEqualTo("UserTask.name2");
-    assertThat(task.getDescription()).isEqualTo(historicTask.getDescription()).isEqualTo("UserTask.description2");
-    assertThat(task.getCategory()).isEqualTo(historicTask.getCategory()).isEqualTo("UserTask.category2");
-    assertThat(task.getFormKey()).isEqualTo(historicTask.getFormKey()).isEqualTo("UserTask.formKey2");
-    assertThat(task.getAssignee()).isEqualTo(historicTask.getAssignee()).isEqualTo("UserTask.assignee2");
-    assertThat(task.getOwner()).isEqualTo(historicTask.getOwner()).isEqualTo("UserTask.owner2");
-    assertThat(task.getPriority()).isEqualTo(historicTask.getPriority()).isEqualTo(2);
-    assertThat(task.getDueDate()).isEqualTo(historicTask.getDueDate()).isEqualTo(new Date(2));
   }
+
+    @Deployment(resources={"org/activiti/engine/test/bpmn/usertask/MakeTaskChangesUsingScripts.bpmn20.xml"})
+    public void testTaskChangesUsingTaskListenersOnSave() {
+      // GIVEN: a process with an usertask, with a TaskListener to set values on the task using javascript
+      ProcessInstance processInstance = runtimeService.startProcessInstanceByKey("dynamicTaskChanges");
+      String processInstanceId = processInstance.getId();
+
+      // WHEN: fetching the information from the current task (both runtime and historical)
+      Task task = taskService.createTaskQuery().processInstanceId(processInstanceId).singleResult();
+      HistoricTaskInstance historicTask = historyService.createHistoricTaskInstanceQuery().processInstanceId(processInstanceId).singleResult();
+
+      // WHEN: changing the values of the task and saving the task
+      task.setName("name from save");
+      task.setOwner("owner from save");
+      task.setDescription("description from save");
+      task.setCategory("category from save");
+      task.setFormKey("formKey from save");
+      task.setAssignee("assignee from save");
+      task.setPriority(2);
+      task.setDueDate(new Date(2));
+      taskService.saveTask(task);
+
+      // WHEN: fetching the information from the current task (both runtime and historical)
+      task = taskService.createTaskQuery().processInstanceId(processInstanceId).singleResult();
+      historicTask = historyService.createHistoricTaskInstanceQuery().singleResult();
+
+      // THEN: runtime-task should have the values from script and should be in-sync with historical-task
+      // THEN: saveTask will execute the assignment taskListener, which will revert the values to the script ones
+      assertThat(task.getName()).isEqualTo(historicTask.getName()).isEqualTo("name from script");
+      assertThat(task.getDescription()).isEqualTo(historicTask.getDescription()).isEqualTo("description from script");
+      assertThat(task.getCategory()).isEqualTo(historicTask.getCategory()).isEqualTo("category from script");
+      assertThat(task.getFormKey()).isEqualTo(historicTask.getFormKey()).isEqualTo("formKey from script");
+      assertThat(task.getAssignee()).isEqualTo(historicTask.getAssignee()).isEqualTo("assignee from script");
+      assertThat(task.getOwner()).isEqualTo(historicTask.getOwner()).isEqualTo("owner from script");
+      assertThat(task.getPriority()).isEqualTo(historicTask.getPriority()).isEqualTo(1);
+      assertThat(task.getDueDate()).isEqualTo(historicTask.getDueDate()).isEqualTo(new Date(1));
+    }
 
 }
