5643eafc7a28bc48c95cc891ceadb8083d8ba058
Vitor Moreira <vitor.moreira-v@alfresco.com>
2025-06-06 12:06:54 +0100

    MNT-24996: several fixes

--- activiti-core/activiti-engine/src/main/resources/org/activiti/db/drop/activiti.oracle.drop.engine.sql
+++ activiti-core/activiti-engine/src/main/resources/org/activiti/db/drop/activiti.oracle.drop.engine.sql
@@ -35,6 +35,7 @@ drop index ACT_IDX_EXEC_BUSKEY;
 drop index ACT_IDX_TASK_CREATE;
 drop index ACT_IDX_IDENT_LNK_USER;
 drop index ACT_IDX_IDENT_LNK_GROUP;
+drop index ACT_IDX_IDENT_LNK_TASK;
 drop index ACT_IDX_VARIABLE_TASK_ID;
 
 alter table ACT_GE_BYTEARRAY
