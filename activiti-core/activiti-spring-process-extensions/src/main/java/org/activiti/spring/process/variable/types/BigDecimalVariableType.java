172e8459ab05f29e67927dd50b3d411e909cada1
Aleksandra Onych <aleksandra.onych@hyland.com>
2025-07-23 10:19:19 +0200

    [AAE] Test dispatch ActivitiErrorReceived event

--- activiti-core/activiti-spring-process-extensions/src/main/java/org/activiti/spring/process/variable/types/BigDecimalVariableType.java
+++ activiti-core/activiti-spring-process-extensions/src/main/java/org/activiti/spring/process/variable/types/BigDecimalVariableType.java
@@ -16,10 +16,11 @@
 package org.activiti.spring.process.variable.types;
 
 import org.activiti.engine.ActivitiException;
-import org.activiti.engine.delegate.event.ActivitiErrorEvent;
 import org.activiti.engine.delegate.event.ActivitiEvent;
 import org.activiti.engine.delegate.event.ActivitiEventType;
 import org.activiti.engine.delegate.event.impl.ActivitiEventBuilder;
+import org.activiti.engine.delegate.event.impl.ActivitiEventDispatcherImpl;
+import org.activiti.engine.impl.cfg.ProcessEngineConfigurationImpl;
 import org.activiti.engine.impl.context.Context;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -45,7 +46,17 @@ public class BigDecimalVariableType extends VariableType {
             }
             return BigDecimal.valueOf(((Number) value).doubleValue());
         } catch (ClassCastException | NumberFormatException e) {
-            if (Context.getProcessEngineConfiguration().getEventDispatcher().isEnabled()) {
+            if (Context.getProcessEngineConfiguration() != null) {
+                if (Context.getProcessEngineConfiguration().getEventDispatcher() == null) {
+                    ProcessEngineConfigurationImpl processEngineConfiguration = Context.getProcessEngineConfiguration();
+                    processEngineConfiguration.setEventDispatcher(new ActivitiEventDispatcherImpl());
+                    processEngineConfiguration.getEventDispatcher().setEnabled(true);
+                } else {
+                    if (!Context.getProcessEngineConfiguration().getEventDispatcher().isEnabled()) {
+                        Context.getProcessEngineConfiguration().getEventDispatcher().setEnabled(true);
+                    }
+                }
+
                 ActivitiEvent errorEvent = ActivitiEventBuilder.createErrorEvent(
                     ActivitiEventType.ACTIVITY_ERROR_RECEIVED,
                     "1",
@@ -57,6 +68,7 @@ public class BigDecimalVariableType extends VariableType {
                 );
                 Context.getProcessEngineConfiguration().getEventDispatcher().dispatchEvent(errorEvent);
             }
+
             throw new ActivitiException("Error parsing bigdecimal value from " + value + ": " + e.getMessage(), e);
         }
     }
