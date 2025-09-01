3c2bd456587fccd20a1291eacfec33cf3391b048
arpanhyland <47354862+arpanhyland@users.noreply.github.com>
2025-09-01 15:39:20 +0530

    ACTIVITI-5522 preview release for 8.1.4 (#5116)
    * preview release with database fix
    
    * Removed duplicate index
    
    * Remove creation and deletion of index ACT_IDX_IDENT_LNK_TASK for oracle as another index ACT_IDX_TSKASS_TASK already present for the same column
    Added if exists before dropping the index ACT_IDX_IDENT_LNK_TASK & ACT_IDX_RE_PROCDEF_DEPLOYMENT_ID
    
    * Added code -> drop index if exists else do nothing for mysql & oracle
    
    * Solution for EOF error
    
    * modified exception handling if no index found
    
    * changing statements to lowercase
    
    ---------
    
    Co-authored-by: Shreasi Khan <shreasi.khan@hyland.com>

--- activiti-core/activiti-engine/src/main/resources/org/activiti/db/upgrade/activiti.oracle.upgradestep.813.to.814.engine.sql
+++ activiti-core/activiti-engine/src/main/resources/org/activiti/db/upgrade/activiti.oracle.upgradestep.813.to.814.engine.sql
@@ -1,4 +1,10 @@
-create index ACT_IDX_IDENT_LNK_TASK on ACT_RU_IDENTITYLINK(TASK_ID_ ASC);
+begin
+execute immediate 'DROP INDEX ACT_IDX_RE_PROCDEF_DEPLOYMENT_ID';
+exception
+   when others then
+      null;
+end;
+/
 
 create index ACT_IDX_RE_PROCDEF_DEPLOYMENT_ID on ACT_RE_PROCDEF(DEPLOYMENT_ID_ ASC, ID_ ASC);
 
