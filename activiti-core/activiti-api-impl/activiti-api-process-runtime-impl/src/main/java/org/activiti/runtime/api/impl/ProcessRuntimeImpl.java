0be072e468b3efffc47903317f34a3fd0a8e8d94
Federico D'Ulizia <federico.dulizia@hyland.com>
2025-10-06 11:13:09 +0200

    AAE-38145: add support for including non-user-startable processes in process definition query (#5164)

--- activiti-core/activiti-api-impl/activiti-api-process-runtime-impl/src/main/java/org/activiti/runtime/api/impl/ProcessRuntimeImpl.java
+++ activiti-core/activiti-api-impl/activiti-api-process-runtime-impl/src/main/java/org/activiti/runtime/api/impl/ProcessRuntimeImpl.java
@@ -76,6 +76,7 @@ import org.springframework.transaction.annotation.Transactional;
 public class ProcessRuntimeImpl implements ProcessRuntime {
 
     private static final String EVERYONE_GROUP = "*";
+    private static final String NO_USER_STARTABLE_PROCESSES = "noUserStartableProcesses";
 
     private final RepositoryService repositoryService;
 
@@ -160,6 +161,11 @@ public class ProcessRuntimeImpl implements ProcessRuntime {
             .startableByGroups(getCurrentUserGroupsIncludingEveryOneGroup());
     }
 
+    private ProcessDefinitionQuery createProcessDefinitionQueryIncludingNoUserStartableProcesses() {
+        return repositoryService
+            .createProcessDefinitionQuery();
+    }
+
     private Optional<org.activiti.engine.repository.ProcessDefinition> findLatestProcessDefinition(
         ProcessDefinitionQuery processDefinitionQuery
     ) {
@@ -233,9 +239,19 @@ public class ProcessRuntimeImpl implements ProcessRuntime {
             getProcessDefinitionsPayload.setProcessDefinitionKeys(securityKeysInPayload.getProcessDefinitionKeys());
         }
 
-        ProcessDefinitionQuery processDefinitionQuery = createProcessDefinitionQueryWithAccessCheck()
-            .latestVersion()
-            .deploymentIds(latestDeploymentIds());
+        ProcessDefinitionQuery processDefinitionQuery;
+
+
+        if (include.contains(NO_USER_STARTABLE_PROCESSES)){
+            processDefinitionQuery = createProcessDefinitionQueryIncludingNoUserStartableProcesses()
+                .latestVersion()
+                .deploymentIds(latestDeploymentIds());
+        } else {
+            processDefinitionQuery = createProcessDefinitionQueryWithAccessCheck()
+                .latestVersion()
+                .deploymentIds(latestDeploymentIds());
+        }
+
 
         if (getProcessDefinitionsPayload.hasDefinitionKeys()) {
             processDefinitionQuery.processDefinitionKeys(getProcessDefinitionsPayload.getProcessDefinitionKeys());
