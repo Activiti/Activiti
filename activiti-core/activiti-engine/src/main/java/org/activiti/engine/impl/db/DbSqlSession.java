128deb8d18ece2797d85bb0b25c845007bf724ac
Vítor Moreira <vitor.moreira-v@alfresco.com>
2025-12-03 09:42:30 +0100

    MNT-24996: improvement on task and process list (#4987) (#5062)

--- activiti-core/activiti-engine/src/main/java/org/activiti/engine/impl/db/DbSqlSession.java
+++ activiti-core/activiti-engine/src/main/java/org/activiti/engine/impl/db/DbSqlSession.java
@@ -142,6 +142,7 @@ public class DbSqlSession implements Session {
 
         /* Current */
         ACTIVITI_VERSIONS.add(new ActivitiVersion("8.0.0"));
+        ACTIVITI_VERSIONS.add(new ActivitiVersion("8.1.0"));
         ACTIVITI_VERSIONS.add(new ActivitiVersion(ProcessEngine.VERSION));
     }
 
