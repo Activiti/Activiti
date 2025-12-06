5d9f2854dda55ec42696bf2e29f6d2b7672b4b06
Igor Dianov <igor@dianov.com>
2025-12-05 19:46:09 -0800

    Use explicit Activiti JUEL expression factory implementation

--- activiti-core/activiti-engine/src/main/java/org/activiti/engine/impl/el/ExpressionManager.java
+++ activiti-core/activiti-engine/src/main/java/org/activiti/engine/impl/el/ExpressionManager.java
@@ -32,6 +32,7 @@ import org.activiti.core.el.CustomFunctionProvider;
 import org.activiti.core.el.ELContextBuilder;
 import org.activiti.core.el.ELResolverReflectionBlockerDecorator;
 import org.activiti.core.el.ReadOnlyMapELResolver;
+import org.activiti.core.el.juel.ExpressionFactoryImpl;
 import org.activiti.engine.delegate.Expression;
 import org.activiti.engine.delegate.VariableScope;
 import org.activiti.engine.impl.bpmn.data.ItemInstance;
@@ -71,7 +72,7 @@ public class ExpressionManager {
         // Use the ExpressionFactoryImpl in activiti build in version of juel,
         // with parametrised method expressions enabled
         if (initFactory) {
-            expressionFactory = ExpressionFactory.newInstance();
+            expressionFactory = new ExpressionFactoryImpl();
         }
         this.beans = beans;
     }
