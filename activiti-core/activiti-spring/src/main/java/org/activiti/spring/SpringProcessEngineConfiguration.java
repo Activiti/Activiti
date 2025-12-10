6807b519a3be1559af9ddaee0e922ffbe0c8fc35
Jonas Wollweber <87132834+JWollweber@users.noreply.github.com>
2025-12-10 15:20:57 +0100

    AAE-40142 Deactivate Auto-Deployment (#5224)
    * Add NoneAutoDeploymentStrategy
    
    * instantiate strategy
    
    * add UT
    
    * prettier

--- activiti-core/activiti-spring/src/main/java/org/activiti/spring/SpringProcessEngineConfiguration.java
+++ activiti-core/activiti-spring/src/main/java/org/activiti/spring/SpringProcessEngineConfiguration.java
@@ -62,6 +62,7 @@ public class SpringProcessEngineConfiguration
         deploymentStrategies.add(new ResourceParentFolderAutoDeploymentStrategy(applicationUpgradeContextService));
         deploymentStrategies.add(new FailOnNoProcessAutoDeploymentStrategy(applicationUpgradeContextService));
         deploymentStrategies.add(new NeverFailAutoDeploymentStrategy(applicationUpgradeContextService));
+        deploymentStrategies.add(new NoneAutoDeploymentStrategy(applicationUpgradeContextService));
         if (applicationUpgradeContextService != null) {
             this.isRollbackDeployment = applicationUpgradeContextService.isRollbackDeployment();
         }
@@ -107,10 +108,8 @@ public class SpringProcessEngineConfiguration
     @Override
     public void initTransactionContextFactory() {
         if (transactionContextFactory == null && transactionManager != null) {
-            transactionContextFactory = new SpringTransactionContextFactory(
-                transactionManager,
-                transactionSynchronizationAdapterOrder
-            );
+            transactionContextFactory =
+                new SpringTransactionContextFactory(transactionManager, transactionSynchronizationAdapterOrder);
         }
     }
 
---
+++ activiti-core/activiti-spring/src/main/java/org/activiti/spring/autodeployment/NoneAutoDeploymentStrategy.java
@@ -0,0 +1,46 @@
+/*
+ * Copyright 2010-2025 Hyland Software, Inc. and its affiliates.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.activiti.spring.autodeployment;
+
+import org.activiti.core.common.spring.project.ApplicationUpgradeContextService;
+import org.activiti.engine.RepositoryService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.core.io.Resource;
+
+public class NoneAutoDeploymentStrategy extends AbstractAutoDeploymentStrategy {
+
+    protected static final Logger LOGGER = LoggerFactory.getLogger(NoneAutoDeploymentStrategy.class);
+
+    public static final String DEPLOYMENT_MODE = "none";
+
+    public NoneAutoDeploymentStrategy(ApplicationUpgradeContextService applicationUpgradeContextService) {
+        super(applicationUpgradeContextService);
+    }
+
+    @Override
+    protected String getDeploymentMode() {
+        return DEPLOYMENT_MODE;
+    }
+
+    @Override
+    public void deployResources(String deploymentNameHint, Resource[] resources, RepositoryService repositoryService) {
+        LOGGER.info(
+            "Auto-deployment strategy is set to 'none'. No resources will be deployed for deployment '{}'.",
+            deploymentNameHint
+        );
+    }
+}
