a6bbbe403dfdaf9b7cf38c3318c18eddd937e349
Igor Dianov <igor@dianov.com>
2026-04-21 11:32:47 -0700

    add test case with loop variable mappings

--- activiti-core/activiti-spring-boot-starter/src/test/java/org/activiti/spring/boot/process/ProcessRuntimeCallActivityMappingIT.java
+++ activiti-core/activiti-spring-boot-starter/src/test/java/org/activiti/spring/boot/process/ProcessRuntimeCallActivityMappingIT.java
@@ -389,6 +389,43 @@ public class ProcessRuntimeCallActivityMappingIT {
             .isInstanceOf(NotFoundException.class);
     }
 
+    @Test
+    public void shouldMapOutputVariablesFromMultiInstanceCallActivityResultCollectionWithLoopVariables() {
+        securityUtil.logInAs("user");
+
+        ProcessInstance processInstance = processRuntime.start(
+            ProcessPayloadBuilder
+                .start()
+                .withProcessDefinitionKey("multi-instance-parallel-call-activity-result-collection-loop-variables")
+                .build()
+        );
+        assertThat(processInstance).isNotNull();
+
+        List<VariableInstance> procVariables = processRuntime.variables(
+            ProcessPayloadBuilder.variables().withProcessInstanceId(processInstance.getId()).build()
+        );
+
+        assertThat(procVariables)
+            .isNotNull()
+            .singleElement(InstanceOfAssertFactories.type(VariableInstance.class))
+            .satisfies(variable -> assertThat(variable.getName()).isEqualTo("miResult"))
+            .extracting(VariableInstance::getValue)
+            .asInstanceOf(InstanceOfAssertFactories.list(Map.class))
+            .containsExactlyInAnyOrder(
+                Map.of("result", "Result 1", "resultIndex", 1),
+                Map.of("result", "Result 0", "resultIndex", 0)
+            );
+
+        final Task task = getTask(processInstance);
+
+        taskRuntime.claim(TaskPayloadBuilder.claim().withTaskId(task.getId()).withAssignee("user").build());
+
+        taskRuntime.complete(TaskPayloadBuilder.complete().withTaskId(task.getId()).build());
+
+        assertThatThrownBy(() -> processRuntime.processInstance(processInstance.getId()))
+            .isInstanceOf(NotFoundException.class);
+    }
+
     public void completeTask(String taskId, Map<String, Object> variables) {
         Task completeTask = taskRuntime.complete(
             TaskPayloadBuilder.complete().withTaskId(taskId).withVariables(variables).build()
