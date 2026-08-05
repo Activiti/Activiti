4b82b294774ec1532cf09df8c335360f2533ebf4
Darren Thornton <darren.thornton@hyland.com>
2026-08-05 08:45:57 -0500

    Address Sonar another overlooked issue

--- activiti-core/activiti-spring-process-extensions/src/test/java/org/activiti/spring/process/variable/VariableValidationServiceTest.java
+++ activiti-core/activiti-spring-process-extensions/src/test/java/org/activiti/spring/process/variable/VariableValidationServiceTest.java
@@ -33,7 +33,6 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
-import tools.jackson.databind.json.JsonMapper;
 
 @SpringBootTest
 class VariableValidationServiceTest {
